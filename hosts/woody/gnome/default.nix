c63e793dbdde26862c7b5b080c24286cc422a75a
Ryan Clark <ryanc@accentservices.com>
2024-01-02 17:25:31 -0600

    dash-to-panel setup

--- hosts/woody/gnome/default.nix
+++ hosts/woody/gnome/default.nix
@@ -52,20 +52,67 @@
           ];
           disable-user-extensions = false;
           enabled-extensions = [
-            "vitals@corecoding.com"
+            "Vitals@CoreCoding.com"
+            "blur-my-shell@aunetx"
+            "gsconnect@andyholmes.github.io"
+            "dash-to-panel@jderose9.github.com"
+            "BingWallpaper@ineffable-gmail.com"
           ];
         };
-        "org/gnome/desktop/interface".color-scheme = "prefer-dark";
-        "org/gnome/desktop/wm/preferences".button-layout = "appmenu:minimize,maximize,close";
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+          enable-hot-corners = false;
+          clock-show-weekday = true;
+        };
+        "org/gnome/desktop/privacy" = {
+          report-technical-problems = "false";
+        };
+        "org/gnome/desktop/calendar" = {
+          show-weekdate = true;
+        };
+        "org/gnome/desktop/wm/preferences" = {
+          action-right-click-titlebar = "toggle-maximize";
+          action-middle-click-titlebar = "minimize";
+          resize-with-right-button = true;
+          mouse-button-modifier = "<super>";
+          button-layout = "appmenu:minimize,maximize,close";
+        };
+        "org/gnome/shell/extensions/dash-to-panel" = {
+          intellihide = true;
+          panel-positions = ''{"0":"BOTTOM"}'';
+          panel-sizes = ''{"0":55}'';
+          panel-lengths = ''{"0":40}'';
+          panel-anchors = ''{"0":"MIDDLE"}'';
+          panel-element-positions = ''{"0":[{"element":"showAppsButton","visible":true,"position":"stackedTL"},{"element":"activitiesButton","visible":true,"position":"stackedTL"},{"element":"leftBox","visible":true,"position":"stackedTL"},{"element":"taskbar","visible":true,"position":"stackedTL"},{"element":"centerBox","visible":true,"position":"stackedBR"},{"element":"rightBox","visible":true,"position":"stackedBR"},{"element":"dateMenu","visible":false,"position":"stackedBR"},{"element":"systemMenu","visible":false,"position":"stackedBR"},{"element":"desktopButton","visible":true,"position":"stackedBR"}]}'';
+          showdesktop-button-width = 10;
+          animate-appicon-hover = true;
+          dot-style-focused = "DOTS";
+          dot-style-unfocused = "DOTS";
+          trans-use-custom-bg = true;
+          trans-use-custom-opacity = true;
+          trans-use-dynamic-opacity = true;
+          show-favorites = true;
+          show-favorites-all-monitors = false;
+          progress-show-count = true;
+          show-window-previews = true;
+          show-tooltip = true;
+          animate-app-switch = true;
+          animate-window-launch = true;
+          stockgs-keep-dash = true;
+          stockgs-keep-top-panel = true;
+          stockgs-panelbtn-click-only = true;
+          stockgs-force-hotcorner = true;
+          secondarymenu-contains-appmenu = true;
+        };
+
       };
-      # settings.enable-extensions = [
-      #   "Vitals@CoreCoding.com"
-      # ];
     };
     home.packages = with pkgs.gnomeExtensions; [
       vitals
       blur-my-shell
       gsconnect
+      dash-to-panel
+      bing-wallpaper-changer
     ];
   };
 
