aeab1b12d7136b7d6752ac78d0e85d708a74c17b
Ryan Clark <ryanc@accentservices.com>
2024-01-02 20:55:50 -0600

    gnome config updates

--- hosts/woody/gnome/default.nix
+++ hosts/woody/gnome/default.nix
@@ -44,19 +44,28 @@
 
         "org/gnome/shell" = {
           favorite-apps = [
-            "org.gnome.Nautilus.desktop"
             "code.desktop"
-            "chromium-browser.desktop"
             "alacritty.desktop"
-            "org.kde.konsole.desktop"
+            "chromium-browser.desktop"
+            "firefox.desktop"
+            "org.gnome.Nautilus.desktop"
           ];
           disable-user-extensions = false;
           enabled-extensions = [
             "Vitals@CoreCoding.com"
-            "blur-my-shell@aunetx"
-            "gsconnect@andyholmes.github.io"
-            "dash-to-panel@jderose9.github.com"
             "BingWallpaper@ineffable-gmail.com"
+            "trayiconsreloaded@selfmade.pl"
+            "blur-my-shell@aunetx"
+            "drive-menu@gnome-shell-extensions.gcampax.github.com"
+            "dash-to-panel@jderose9.github.com"
+            "just-perfection-desktop@just-perfection"
+            "caffeine@patapon.info"
+            "clipboard-indicator@tudmotu.com"
+            "horizontal-workspace-indicator@tty2.io"
+            "bluetooth-quick-connect@bjarosze.gmail.com"
+            "gsconnect@andyholmes.github.io"
+            "pip-on-top@rafostar.github.com"
+            "forge@jmmaranan.com"
           ];
         };
         "org/gnome/desktop/interface" = {
@@ -112,6 +121,28 @@
           toggle-tiled-left = ["@as []"];               # Tiling
           toggle-tiled-right = ["@as []"];
         };
+        "org/gnome/settings-daemon/plugins/media-keys" = {
+          custom-keybindings = [
+            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
+          ];
+        };
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+          binding = "<super>return";
+          command = "alacritty";
+          name = "open-terminal";
+        };
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+          binding = "<super>t";
+          command = "code";
+          name = "open-editor";
+        };
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+          binding = "<super>e";
+          command = "nautilus";
+          name = "open-file-browser";
+        };
         "org/gnome/shell/extensions/dash-to-panel" = {
           intellihide = true;
           panel-positions = ''{"0":"BOTTOM"}'';
@@ -139,15 +170,79 @@
           stockgs-force-hotcorner = true;
           secondarymenu-contains-appmenu = true;
         };
-
+        "org/gnome/shell/extensions/just-perfection" = {
+          theme = true;
+          activities-button = false;
+          app-menu = false;
+          clock-menu-position = 1;
+          clock-menu-position-offset = 7;
+        };
+        "org/gnome/shell/extensions/caffeine" = {
+          enable-fullscreen = true;
+          restore-state = true;
+          show-indicator = true;
+          show-notification = false;
+        };
+        "org/gnome/shell/extensions/blur-my-shell" = {
+          brightness = 0.9;
+        };
+        "org/gnome/shell/extensions/blur-my-shell/panel" = {
+          customize = true;
+          sigma = 0;
+        };
+        "org/gnome/shell/extensions/blur-my-shell/overview" = {
+          customize = true;
+          sigma = 0;
+        };
+        "org/gnome/shell/extensions/horizontal-workspace-indicator" = {
+          widget-position = "left";
+          widget-orientation = "horizontal";
+          icons-style = "circles";
+        };
+        "org/gnome/shell/extensions/bluetooth-quick-connect" = {
+          show-battery-icon-on = true;
+          show-battery-value-on = true;
+        };
+        "org/gnome/shell/extensions/pip-on-top" = {
+          stick = true;
+        };
+        "org/gnome/shell/extensions/forge" = {
+          window-gap-size = 8;
+          dnd-center-layout = "stacked";
+        };
+        "org/gnome/shell/extensions/forge/keybindings" = { # Set Manually
+          focus-border-toggle = true;
+          float-always-on-top-enabled = true;
+          window-focus-up = ["<super>up"];
+          window-focus-down = ["<super>down"];
+          window-focus-left = ["<super>left"];
+          window-focus-right = ["<super>right"];
+          window-move-up = ["<shift><super>up"];
+          window-move-down = ["<shift><super>down"];
+          window-move-left = ["<shift><super>left"];
+          window-move-right = ["<shift><super>right"];
+          window-swap-last-active = ["@as []"];
+          window-toggle-float = ["<shift><super>f"];
+        };
+        "org/gtk/gtk4/settings/file-chooser".sort-directories-first = true;
       };
     };
     home.packages = with pkgs.gnomeExtensions; [
       vitals
-      blur-my-shell
-      gsconnect
-      dash-to-panel
       bing-wallpaper-changer
+      tray-icons-reloaded
+      blur-my-shell
+      removable-drive-menu
+      dash-to-panel
+      just-perfection
+      caffeine
+      clipboard-indicator
+      workspace-indicator-2
+      bluetooth-quick-connect
+      gsconnect
+      pip-on-top
+      pop-shell
+      forge
     ];
   };
 
