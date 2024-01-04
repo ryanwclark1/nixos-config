9913e1db3a9e8813b6d2c009ab9083e662af2edc
Ryan Clark <ryanc@accentservices.com>
2024-01-04 09:18:06 -0600

    renamed to services.avahi.nssmdns4

--- hosts/common/global/printing.nix
+++ hosts/common/global/printing.nix
@@ -8,7 +8,7 @@
     avahi = {
       enable = true;
       openFirewall = true;
-      nssmdns = true; # Allows software to use Avahi to resolve.
+      nssmdns4 = true; # Allows software to use Avahi to resolve.
       publish = {
         enable = true;
         addresses = true;
