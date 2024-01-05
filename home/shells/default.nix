11f38a506342219c2dbdbd9738cf2c8c5ddc3a4e
Ryan Clark <ryanc@accentservices.com>
2024-01-05 17:04:04 -0600

    updated zellij and alias in shells package

--- home/shells/default.nix
+++ home/shells/default.nix
@@ -7,21 +7,25 @@
 with lib; {
 
   imports = [
+    ./alias.nix
     ./bash.nix
     ./fish.nix
     ./nushell.nix
     ./tmux.nix
+    ./zellij.nix
     ./zsh.nix
   ];
 
   options.shells.enable = mkEnableOption "shells packages";
   config = mkIf config.shells.enable {
 
+    alias.enable = true;
     bash.enable = true;
     fish.enable = true;
     nushell.enable = true;
-    zsh.enable = true;
     tmux.enable = true;
+    zellij.enable = true;
+    zsh.enable = true;
 
   };
 }
