a4762fddf4768693fa0e318ec34968569c040fc0
Ryan Clark <ryanc@accentservices.com>
2024-01-05 14:58:30 -0600

    framework update

--- hosts/common/plasma/default.nix
+++ hosts/common/plasma/default.nix
@@ -5,116 +5,125 @@
   ...
 }:
 
+
+with lib;
+
 {
   # imports = [
   #   ./bismuth.nix
   # ];
-  programs.dconf.enable = true;
-  programs.kdeconnect.enable = true;
 
-  environment.systemPackages = with pkgs; [       # Packages installed
-    xorg.xkill
-    (ark.override {unfreeEnableUnrar = true;})
-    gwenview
-    # kate
-    # neovim-qt
-    plasma5Packages.kdeconnect-kde
+  options.plasma.enable = mkEnableOption "plasma settings";
+  config = mkIf config.plasma.enable {
+    # bismuth.enable = true;
 
-    keepassxc
-    kget
-    kgpg
-    krename
-    hunspell
-    hunspellDicts.ru-ru
-    hyphen
-    okular
-    qalculate-gtk
+    programs.dconf.enable = true;
+    programs.kdeconnect.enable = true;
 
-    kdeplasma-addons
-    spectacle
+    environment.systemPackages = with pkgs; [       # Packages installed
+      xorg.xkill
+      (ark.override {unfreeEnableUnrar = true;})
+      gwenview
+      # kate
+      # neovim-qt
+      plasma5Packages.kdeconnect-kde
 
-    # Required by KInfoCenter
-    clinfo # clinfo
-    glxinfo # eglinfo, glxinfo
-    pciutils # lspci
-    vulkan-tools # vulkaninfo
-    wayland-utils # wayland-info
+      keepassxc
+      kget
+      kgpg
+      krename
+      hunspell
+      hunspellDicts.ru-ru
+      hyphen
+      okular
+      qalculate-gtk
 
-    # Required by Nix
-    gitMinimal
+      kdeplasma-addons
+      spectacle
 
-    plasma5Packages.dolphin-plugins
-    plasma5Packages.ffmpegthumbs
-    plasma5Packages.kdegraphics-thumbnailers
-    plasma5Packages.kglobalaccel
-    plasma5Packages.kio
-    plasma5Packages.kio-extras
+      # Required by KInfoCenter
+      clinfo # clinfo
+      glxinfo # eglinfo, glxinfo
+      pciutils # lspci
+      vulkan-tools # vulkaninfo
+      wayland-utils # wayland-info
 
-    libsForQt5.ark
-    libsForQt5.applet-window-buttons
-    libsForQt5.discover
-    libsForQt5.dolphin-plugins
-    libsForQt5.kaccounts-integration
-    libsForQt5.kaccounts-providers
-    libsForQt5.kio
-    libsForQt5.kio-gdrive
-    libsForQt5.plasma-browser-integration
-    libsForQt5.plasma-integration
-    libsForQt5.qtstyleplugin-kvantum
-    libsForQt5.bismuth
-    lightly-qt
+      # Required by Nix
+      gitMinimal
 
-    # KDE/Plasma: support spellchecking
-    hunspell
-    hunspellDicts.en_US
-    aspell
-    aspellDicts.en
-    aspellDicts.en-computers
-    aspellDicts.en-science
+      plasma5Packages.dolphin-plugins
+      plasma5Packages.ffmpegthumbs
+      plasma5Packages.kdegraphics-thumbnailers
+      plasma5Packages.kglobalaccel
+      plasma5Packages.kio
+      plasma5Packages.kio-extras
 
-    capitaine-cursors
-    la-capitaine-icon-theme
+      libsForQt5.ark
+      libsForQt5.applet-window-buttons
+      libsForQt5.discover
+      libsForQt5.dolphin-plugins
+      libsForQt5.kaccounts-integration
+      libsForQt5.kaccounts-providers
+      libsForQt5.kio
+      libsForQt5.kio-gdrive
+      libsForQt5.plasma-browser-integration
+      libsForQt5.plasma-integration
+      libsForQt5.qtstyleplugin-kvantum
+      libsForQt5.bismuth
+      lightly-qt
 
-    # Settings
-    kgamma5
+      # KDE/Plasma: support spellchecking
+      hunspell
+      hunspellDicts.en_US
+      aspell
+      aspellDicts.en
+      aspellDicts.en-computers
+      aspellDicts.en-science
 
-    filelight
-    kcalc
-#   kgpg
-#   qttools
-#   quazip
-  ];
+      capitaine-cursors
+      la-capitaine-icon-theme
 
-  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
-    elisa
-    oxygen
-  ];
+      # Settings
+      kgamma5
 
-  environment.etc = {
-    "chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json".source = "${pkgs.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
-  };
-
-  environment.variables = {
-    "QT_PLUGIN_PATH" = [ "/run/current-system/sw/"
-                         "${pkgs.qt5.qtbase}/${pkgs.qt5.qtbase.qtPluginPrefix}:${pkgs.qt5.qtimageformats}/${pkgs.qt5.qtbase.qtPluginPrefix}" ];
-    "QT_STYLE_OVERRIDE" = "kvantum";
-  };
-
-
-  # Networking configuration.
-  networking = {
-
-    # Allow connections from certain port ranges (TCP).
-    firewall.allowedTCPPortRanges = [
-      # KDEConnect
-      { from = 1714; to = 1764; }
+      filelight
+      kcalc
+  #   kgpg
+  #   qttools
+  #   quazip
     ];
 
-    # Allow connections from certain port ranges (UDP).
-    firewall.allowedUDPPortRanges = [
-      # KDEConnect
-      { from = 1714; to = 1764; }
+    environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+      elisa
+      oxygen
     ];
 
+    environment.etc = {
+      "chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json".source = "${pkgs.plasma-browser-integration}/etc/chromium/native-messaging-hosts/org.kde.plasma.browser_integration.json";
+    };
+
+    environment.variables = {
+      "QT_PLUGIN_PATH" = [ "/run/current-system/sw/"
+                           "${pkgs.qt5.qtbase}/${pkgs.qt5.qtbase.qtPluginPrefix}:${pkgs.qt5.qtimageformats}/${pkgs.qt5.qtbase.qtPluginPrefix}" ];
+      "QT_STYLE_OVERRIDE" = "kvantum";
+    };
+
+
+    # Networking configuration.
+    networking = {
+
+      # Allow connections from certain port ranges (TCP).
+      firewall.allowedTCPPortRanges = [
+        # KDEConnect
+        { from = 1714; to = 1764; }
+      ];
+
+      # Allow connections from certain port ranges (UDP).
+      firewall.allowedUDPPortRanges = [
+        # KDEConnect
+        { from = 1714; to = 1764; }
+      ];
+
+    };
   };
 }
