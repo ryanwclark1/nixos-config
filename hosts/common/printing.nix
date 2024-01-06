71edff2fb2418d621ed3c7d4b719a1cfe137ce54
Ryan Clark <ryanwclark@yahoo.com>
2024-01-06 15:28:06 -0600

    nssmdns name change

--- hosts/common/printing.nix
+++ hosts/common/printing.nix
@@ -14,8 +14,8 @@ with lib;
       avahi = {
         enable = true;
         openFirewall = true;
-        # nssmdns4 = true; # Allows software to use Avahi to resolve.
-        nssmdns = true; # Allows software to use Avahi to resolve.
+        nssmdns4 = true; # Allows software to use Avahi to resolve.
+        # nssmdns = true; # Allows software to use Avahi to resolve.
         publish = {
           enable = true;
           addresses = true;
