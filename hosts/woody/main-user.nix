4f81907909c9e770fe7a977b8400903587945609
Ryan Clark <ryanc@accentservices.com>
2024-01-07 11:48:16 -0600

    added user to wireshark group

--- hosts/woody/main-user.nix
+++ hosts/woody/main-user.nix
@@ -22,7 +22,7 @@
       # initialPassword = "";
       description = "Administrator User";
       shell = pkgs.zsh;
-      extraGroups = [ "networkmanager" "wheel" "audio" "docker" "video" "transmission"];
+      extraGroups = [ "networkmanager" "wheel" "audio" "docker" "video" "transmission" "wireshark"];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJx3Sk20pLL1b2PPKZey2oTyioODrErq83xG78YpFBoj admin@xxxx"
       ];
@@ -32,5 +32,9 @@
       # ];
     };
     programs.zsh.enable = true;
+    programs.wireshark = {
+      enable = true;
+      package = pkgs.wireshark;
+    };
   };
 }
