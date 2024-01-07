ad930e5f1e682b917eaf2b2d325d6ccbbb9973e3
Ryan Clark <ryanc@accentservices.com>
2024-01-07 12:41:38 -0600

    gpu updates for woody

--- hosts/woody/gpu.nix
+++ hosts/woody/gpu.nix
@@ -8,14 +8,6 @@
   boot.initrd.kernelModules = [ "amdgpu" ];
 
 
-  # You should also install the clinfo package
-  # to verify that OpenCL is correctly setup check darktable
-  hardware.opengl.extraPackages = with pkgs; [
-    rocmPackages.clr.icd
-    rocm-opencl-runtime
-    amdvlk
-  ];
-
   # problems having multiple monitors connected to your GPU,
   # adding `video` parameters for each connector to the kernel command line sometimes helps.
   # To figure out the connector names head /sys/class/drm/*/status
@@ -24,18 +16,20 @@
   #   "video=DP-2:2560x1440@144"
   # ];
 
-  # For 32 bit applications
-  hardware.opengl.extraPackages32 = with pkgs; [
-    driversi686Linux.amdvlk
-  ];
+  # You should also install the clinfo package
+  # to verify that OpenCL is correctly setup check darktable
+  hardware.opengl = {
 
-  # Vulkan is already enabled by default (using Mesa RADV) on 64 bit applications
-  # Settings to control
-  hardware.opengl.driSupport = true; # This is already enabled by default
-  hardware.opengl.driSupport32Bit = true; # For 32 bit applications
-
-
-  # hardware.opengl.extraPackages = with pkgs; [
-  #   amdvlk
-  # ];
+    driSupport = true; # This is already enabled by default
+    driSupport32Bit = true; # For 32 bit applications
+    extraPackages = with pkgs; [
+      rocmPackages.clr.icd
+      rocm-opencl-runtime
+      amdvlk
+      libva-utils
+    ];
+    extraPackages32 = with pkgs; [
+      driversi686Linux.amdvlk
+    ];
+  };
 }
