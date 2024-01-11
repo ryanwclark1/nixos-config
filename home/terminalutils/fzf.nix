010469225cbf202eeec3641f1b397f91d2ce25ac
Ryan Clark <ryanc@accentservices.com>
2024-01-10 18:14:37 -0600

    spice for boxes

--- home/terminalutils/fzf.nix
+++ home/terminalutils/fzf.nix
@@ -14,7 +14,6 @@ with lib; {
     programs.fzf = {
       enable = true;
       changeDirWidgetCommand = "fd --type d";
-      tmux.enableShellIntegration = config.tmux.enable;
       enableBashIntegration = config.bash.enable;
       enableZshIntegration = config.zsh.enable;
       enableFishIntegration = config.fish.enable;
