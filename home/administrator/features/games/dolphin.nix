c85be11e0bad74b835eda2267d092163299536cd
Ryan Clark <ryanwclark@yahoo.com>
2024-01-12 21:03:48 -0600

    Fix bug in login functionality

---
+++ home/administrator/features/games/dolphin.nix
@@ -0,0 +1,11 @@
+{ lib, pkgs, ... }: {
+  home = {
+    packages = [ pkgs.dolphinEmu ];
+    persistence = {
+      "/persist/home/administrator" = {
+        allowOther = true;
+        directories = [ ".dolphin" ];
+      };
+    };
+  };
+}
