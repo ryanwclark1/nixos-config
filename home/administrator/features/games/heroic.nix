4771b340de7051590377e0cb28afa6a4088dfecd
Ryan Clark <ryanwclark@yahoo.com>
2024-01-12 21:03:48 -0600

    Fix bug in login functionality

---
+++ home/administrator/features/games/heroic.nix
@@ -0,0 +1,11 @@
+{ lib, pkgs, ... }: {
+  home = {
+    packages = [ pkgs.heroic ];
+    persistence = {
+      "/persist/home/administrator" = {
+        allowOther = true;
+        directories = [ ".heroic" ];
+      };
+    };
+  };
+}
