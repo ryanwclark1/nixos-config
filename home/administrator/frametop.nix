e977752400720c007383890a089c623dadfa87fa
Ryan Clark <ryanc@accentservices.com>
2024-01-13 18:16:32 -0600

    Update website and frametop configurations

--- home/administrator/frametop.nix
+++ home/administrator/frametop.nix
@@ -1,6 +1,16 @@
-{ lib, ... }:
-{
-  imports = [ ./global ];
-  # Disable impermanence
-  # home.persistence = lib.mkForce { };
+{ inputs, outputs, lib, ... }: let
+  inherit (inputs.nix-colors) colorSchemes;
+in {
+  imports = [
+    ./global
+    ./features/desktop/wireless
+    ./features/desktop/hyprland
+    ./features/pass
+    ./features/productivity
+  ];
+
+  colorscheme = lib.mkDefault colorSchemes.silk-dark;
+  specialisation = {
+    light.configuration.colorscheme = colorSchemes.silk-light;
+  };
 }
