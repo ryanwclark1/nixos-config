a94ddd164b5eef324bbe236ffac74f86de2f90e7
Ryan Clark <ryanc@accentservices.com>
2024-01-13 20:53:05 -0600

    added bootloader to frametop.  Move to HW config

--- hosts/frametop/default.nix
+++ hosts/frametop/default.nix
@@ -27,6 +27,7 @@ networking = {
     kernelPackages = pkgs.linuxKernel.packages.linux_zen;
     binfmt.emulatedSystems = [ "aarch64-linux" "i686-linux" ];
   };
+  boot.loader.systemd-boot.enable = true;
 
   powerManagement.powertop.enable = true;
   programs = {
