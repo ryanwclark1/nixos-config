8a9fa14429d185e4c479ee2375ce78f70fb641ed
Ryan Clark <ryanc@accentservices.com>
2024-01-29 21:47:22 -0600

    revert starship

--- home/features/cli/starship.nix
+++ home/features/cli/starship.nix
@@ -4,7 +4,7 @@
   ...
 }:
 let
-  inherit (config.colorscheme) colors slug;
+  inherit (config.colorscheme) colors;
 in
 # TODO: Add pyenv, nvm, rbenv, rustup, etc. support
 
@@ -14,58 +14,93 @@ in
     enable = true;
     # Configuration written to ~/.config/starship.toml
     settings = {
-      format =  "[](base08)\$os\$username\$hostname\$localip\[](bg:base09 fg:base08)\$directory\[](fg:base09 bg:base0A)\$git_branch\$git_status\[](fg:base0A bg:base0B)\$bun\$c\$cmake\$dart\$golang\$haskell\$java\$kotlin\$kubernetes\$lua\$nodejs\$php\$python\$rust\$swift\$zig[](fg:base0B bg:base07)\$docker_context\[](fg:base07)\$character";
+      format =  "[](color_a1)\$os\$username\$hostname\$localip\[](bg:color_a2 fg:color_a1)\$directory\[](fg:color_a2 bg:color_a3)\$git_branch\$git_status\[](fg:color_a3 bg:color_a4)\$bun\$c\$cmake\$dart\$golang\$haskell\$java\$kotlin\$kubernetes\$lua\$nodejs\$php\$python\$rust\$swift\$zig[](fg:color_a4 bg:color_bg3)\$docker_context\[](fg:color_bg3)\$character";
       add_newline = true;
       scan_timeout = 30;
       command_timeout = 500;
-      palette = "${slug}";
+      palette = "system";
 
       palettes.gruvbox_dark ={
-        base00 = "#fbf1c7";
-        base05 = "#3c3836";
-        base07 = "#665c54";
-        base08 = "#d65d0e";
-        base09 = "#d79921";
-        base0A = "#689d6a";
-        base0B = "#458588";
+        color_fg0 = "#fbf1c7";
+        color_bg1 = "#3c3836";
+        color_bg3 = "#665c54";
+        color_a1 = "#d65d0e";
+        color_a2 = "#d79921";
+        color_a3 = "#689d6a";
+        color_a4 = "#458588";
         color_green = "#98971a";
         color_yellow = "#d79921";
         color_purple = "#b16286";
         color_red = "#cc241d";
-        base0C = "#83a598";
+        color_docker = "#83a598";
       };
 
-      palettes."${slug}" = {
-        base00 = "#${colors.base00}";
-        base01 = "#${colors.base01}";
-        base02 = "#${colors.base02}";
-        base03 = "#${colors.base03}";
-        base04 = "#${colors.base04}";
-        base05 = "#${colors.base05}";
-        base06 = "#${colors.base06}";
-        base07 = "#${colors.base07}";
-        base08 = "#${colors.base08}";
-        base09 = "#${colors.base09}";
-        base0A = "#${colors.base0A}";
-        base0B = "#${colors.base0B}";
-        base0C = "#${colors.base0C}";
-        base0D = "#${colors.base0D}";
-        base0E = "#${colors.base0E}";
-        base0F = "#${colors.base0F}";
+      palettes.system = {
+        color_fg0 = "#${colors.base00}";
+        color_bg1 = "#${colors.base05}";
+        color_bg3 = "#${colors.base07}";
+        color_a1 = "#${colors.base08}";
+        color_a2 = "#${colors.base09}";
+        color_a3 = "#${colors.base0A}";
+        color_a4 = "#${colors.base0B}";
+        color_docker = "#${colors.base0C}";
         color_green = "#98971a";
         color_yellow = "#d79921";
         color_purple = "#b16286";
         color_red = "#cc241d";
       };
 
+      palettes.nord_dark = {
+        color_fg0 = "#d8dee9";
+        color_bg1 = "#2e3440";
+        color_bg3 = "#3b4252";
+        color_a1 = "#bf616a";
+        color_a2 = "#d08770";
+        color_a3 = "#ebcb8b";
+        color_a4 = "#a3be8c";
+        color_green = "#a3be8c";
+        color_yellow = "#ebcb8b";
+        color_purple = "#b48ead";
+        color_red = "#bf616a";
+        color_docker = "#88c0d0";
+      };
 
+      palettes.nord_light = {
+        color_fg0 = "#2e3440";
+        color_bg1 = "#d8dee9";
+        color_bg3 = "#eceff4";
+        color_a1 = "#bf616a";
+        color_a2 = "#d08770";
+        color_a3 = "#ebcb8b";
+        color_a4 = "#a3be8c";
+        color_green = "#a3be8c";
+        color_yellow = "#ebcb8b";
+        color_purple = "#b48ead";
+        color_red = "#bf616a";
+        color_docker = "#88c0d0";
+      };
+
+      palettes.gruvbox_light = {
+        color_fg0 = "#3c3836";
+        color_bg1 = "#fbf1c7";
+        color_bg3 = "#d5c4a1";
+        color_a1 = "#d65d0e";
+        color_a2 = "#d79921";
+        color_a3 = "#689d6a";
+        color_a4 = "#458588";
+        color_green = "#98971a";
+        color_yellow = "#d79921";
+        color_purple = "#b16286";
+        color_red = "#cc241d";
+        color_docker = "#83a598";
+      };
       aws.disabled = true;
       gcloud.disabled = true;
       azure.disabled = true;
 
       os = {
         disabled = false;
-        style = "bg:base08 fg:base00";
+        style = "bg:color_a1 fg:color_fg0";
         symbols = {
           Windows = "󰍲";
           Ubuntu = "󰕈";
@@ -93,15 +128,15 @@ in
       username = {
         show_always = true;
         disabled = false;
-        style_user = "bg:base08 fg:base00";
-        style_root = "bg:base08 fg:base00";
+        style_user = "bg:color_a1 fg:color_fg0";
+        style_root = "bg:color_a1 fg:color_fg0";
         format = "[ $user ]($style)";
       };
 
       localip = {
         disabled = false;
         ssh_only = true;
-        style = "bg:base08 fg:base00";
+        style = "bg:color_a1 fg:color_fg0";
         format = "[ $localipv4 ]($style)";
       };
 
@@ -109,12 +144,12 @@ in
         disabled = false;
         ssh_only = true;
         ssh_symbol = "";
-        style = "bg:base08 fg:base00";
+        style = "bg:color_a1 fg:color_fg0";
         format = "[$ssh_symbol]($style)";
       };
 
       directory = {
-        style = "fg:base00 bg:base09";
+        style = "fg:color_fg0 bg:color_a2";
         format = "[ $path ]($style)";
         truncation_length = 8;
         truncate_to_repo = true;
@@ -160,126 +195,126 @@ in
 
       container = {
         symbol = "";
-        style = "fg:base00 bg:base09";
+        style = "fg:color_fg0 bg:color_a2";
         format = "[$symbol\ [$name]]($style)";
       };
 
       git_branch = {
         symbol = "";
-        style = "bg:base0A";
-        format = "[[ $symbol $branch ](fg:base00 bg:base0A)]($style)";
+        style = "bg:color_a3";
+        format = "[[ $symbol $branch ](fg:color_fg0 bg:color_a3)]($style)";
       };
 
       git_status = {
-        style = "bg:base0A";
-        format = "[[($all_status$ahead_behind )](fg:base00 bg:base0A)]($style)";
+        style = "bg:color_a3";
+        format = "[[($all_status$ahead_behind )](fg:color_fg0 bg:color_a3)]($style)";
       };
 
       bun = {
         symbol = "";
-        style = "bg:base0B";
-        format = "[[ $symbol( $version) ](fg:base00 bg:base0B)]($style)";
+        style = "bg:color_a4";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_a4)]($style)";
       };
 
       c = {
         symbol = " ";
-        style = "bg:base0B";
-        format = "[[ $symbol( $version) ](fg:base00 bg:base0B)]($style)";
+        style = "bg:color_a4";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_a4)]($style)";
       };
 
       cmake = {
         symbol = " ";
-        style = "bg:base0B";
-        format = "[[ $symbol( $version) ](fg:base00 bg:base0B)]($style)";
+        style = "bg:color_a4";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_a4)]($style)";
       };
 
       dart = {
         symbol = " ";
-        style = "bg:base0B";
-        format = "[[ $symbol( $version) ](fg:base00 bg:base0B)]($style)";
+        style = "bg:color_a4";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_a4)]($style)";
         disabled = true;
       };
 
       golang = {
         symbol = "";
-        style = "bg:base0B";
-        format = "[[ $symbol( $version) ](fg:base00 bg:base0B)]($style)";
+        style = "bg:color_a4";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_a4)]($style)";
       };
 
       haskell = {
         symbol = "";
-        style = "bg:base0B";
-        format = "[[ $symbol( $version) ](fg:base00 bg:base0B)]($style)";
+        style = "bg:color_a4";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_a4)]($style)";
       };
 
       java = {
         symbol = " ";
-        style = "bg:base0B";
-        format = "[[ $symbol( $version) ](fg:base00 bg:base0B)]($style)";
+        style = "bg:color_a4";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_a4)]($style)";
       };
 
       kotlin = {
         symbol = "";
-        style = "bg:base0B";
-        format = "[[ $symbol( $version) ](fg:base00 bg:base0B)]($style)";
+        style = "bg:color_a4";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_a4)]($style)";
       };
 
       kubernetes = {
         symbol = "󱃾";
-        style = "bg:base0B";
-        format = "[[ $symbol$context( \$namespace\ ) ](fg:base00 bg:base0B)]($style)";
+        style = "bg:color_a4";
+        format = "[[ $symbol$context( \$namespace\ ) ](fg:color_fg0 bg:color_a4)]($style)";
       };
 
       lua = {
         symbol = "";
-        style = "bg:base0B";
-        format = "[[ $symbol( $version) ](fg:base00 bg:base0B)]";
+        style = "bg:color_a4";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_a4)]";
         disabled = true;
       };
 
       nodejs = {
         symbol = "";
-        style = "bg:base0B";
-        format = "[[ $symbol( $version) ](fg:base00 bg:base0B)]($style)";
+        style = "bg:color_a4";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_a4)]($style)";
       };
 
       php = {
         symbol = "";
-        style = "bg:base0B";
-        format = "[[ $symbol( $version) ](fg:base00 bg:base0B)]($style)";
+        style = "bg:color_a4";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_a4)]($style)";
       };
 
       python = {
         symbol = "";
-        style = "bg:base0B";
+        style = "bg:color_a4";
         version_format = "$raw";
         format = "[ $symbol( $version )(\($virtualenv\) )]($style)";
       };
 
       rust = {
         symbol = "";
-        style = "bg:base0B";
-        format = "[[ $symbol( $version) ](fg:base00 bg:base0B)]($style)";
+        style = "bg:color_a4";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_a4)]($style)";
       };
 
       swift = {
         symbol = "";
-        style = "bg:base0B";
-        format = "[[ $symbol( $version) ](fg:base00 bg:base0B)]($style)";
+        style = "bg:color_a4";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_a4)]($style)";
         disabled = true;
       };
 
       zig = {
         symbol = "";
-        style = "bg:base0B";
-        format = "[[ $symbol( $version) ](fg:base00 bg:base0B)]";
+        style = "bg:color_a4";
+        format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_a4)]";
         disabled = true;
       };
 
       docker_context = {
         symbol = "";
-        style = "bg:base07";
-        format = "[[ $symbol( $context) ](fg:base0C bg:base07)]($style)";
+        style = "bg:color_bg3";
+        format = "[[ $symbol( $context) ](fg:color_docker bg:color_bg3)]($style)";
       };
 
       line_break.disabled = true;
