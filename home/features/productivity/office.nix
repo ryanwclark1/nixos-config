52c9d3b65a1f533e540460333689ab1a1998dc1e
Ryan Clark <ryanc@accentservices.com>
2024-01-30 15:34:31 -0600

    Remove libreoffice-fresh and uncomment libreoffice-qt

--- home/features/productivity/office.nix
+++ home/features/productivity/office.nix
@@ -1,17 +1,11 @@
 {
-  lib,
   pkgs,
-  config,
   ...
 }:
 
-with lib; {
-  options.office.enable = mkEnableOption "office settings";
-
-  config = mkIf config.office.enable {
-    home.packages = with pkgs; [
-      libreoffice-fresh
-      libreoffice-qt
-    ];
-  };
+{
+  home.packages = with pkgs; [
+    # libreoffice-fresh
+    libreoffice-qt
+  ];
 }
