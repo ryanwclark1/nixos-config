61c959d8cb64e393b59e79478db1f5206d8e4f3d
Ryan Clark <ryanc@accentservices.com>
2024-02-06 08:36:41 -0600

    Add bump CLI tool for GitHub Release

--- home/features/cli/gh.nix
+++ home/features/cli/gh.nix
@@ -8,6 +8,7 @@
     enable = true;
     gitCredentialHelper.enable = true;
     extensions = with pkgs; [
+      bump #  CLI tool to draft a GitHub Release for the next semantic version
       gh-markdown-preview
       gh-dash
       gh-eco
