728bc9ecd5b6071182394995b5fbe56cf7dae12e
Ryan Clark <ryanc@accentservices.com>
2024-02-07 10:13:12 -0600

    Remove unused imports and variables in default.nix file

--- home/features/insomnia/default.nix
+++ home/features/insomnia/default.nix
@@ -1,12 +1,10 @@
 # API Client for REST, GraphQL, GRPC and OpenAPI design tool for developers.
 {
   pkgs,
-  lib,
-  config,
   ...
 }:
 
-with lib; {
+{
   home.packages = with pkgs; [
     insomnia
   ];
