9f9db020d849f814629428d5c8a87d9b15653ff5
Ryan Clark <ryanc@accentservices.com>
2024-02-15 13:09:11 -0600

    Update k3s.nix configuration***

--- hosts/common/optional/k3s.nix
+++ hosts/common/optional/k3s.nix
@@ -7,15 +7,17 @@
 
   networking.firewall.allowedTCPPorts = [
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
-    # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
-    # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+    2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+    2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
   ];
   networking.firewall.allowedUDPPorts = [
-    # 8472 # k3s, flannel: required if using multi-node for inter-node networking
+    8472 # k3s, flannel: required if using multi-node for inter-node networking
   ];
   services.k3s.enable = true;
   services.k3s.role = "server";
+  services.k3s.clusterInit = true;
   services.k3s.extraFlags = toString [
+    "--disable=servicelb" # Disable the built-in DNS server
     # "--rootless" # Run k3s as a non-root user
     # "--kubelet-arg=v=4" # Optionally add additional args to k3s
   ];
