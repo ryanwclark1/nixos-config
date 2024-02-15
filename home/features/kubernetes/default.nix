d5e2ca8922ccf0f489435bd5013eda6b68c9834c
Ryan Clark <ryanc@accentservices.com>
2024-02-15 11:03:25 -0600

    Add network-related CLI packages and Kubernetes tool

--- home/features/kubernetes/default.nix
+++ home/features/kubernetes/default.nix
@@ -10,5 +10,6 @@
     minikube
     openlens
     tilt
+    kubeshark
   ];
 }
--- home/features/networking-utils/default.nix
+++ home/features/networking-utils/default.nix
@@ -5,13 +5,12 @@
 
 {
   home.packages = with pkgs; [
-    mtr # A network diagnostic tool
-    iperf3
+
     dnsutils  # `dig` + `nslookup`
     ldns # replacement of `dig`, it provide the command `drill`
     aria2 # A lightweight multi-protocol & multi-source command-line download utility
-    ethtool # A utility for controlling network drivers and hardware
-    socat # replacement of openbsd-netcat
+
+
     nmap # A utility for network discovery and security auditing
     ipcalc  # it is a calculator for the IPv4/v6 addresses
     wireguard-tools
@@ -20,6 +19,6 @@
     rathole # similar to ngrok - NAT traversal and reverse proxy
     ngrep
     wireshark
-    kubeshark
+
   ];
 }
