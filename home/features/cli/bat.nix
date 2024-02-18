d88d244ed5607043e3158131ebaad6ae859d0d89
Ryan Clark <ryanc@accentservices.com>
2024-02-18 15:54:54 -0600

    bat update

--- home/features/cli/bat.nix
+++ home/features/cli/bat.nix
@@ -1,10 +1,22 @@
 {
+  pkgs,
   ...
 }:
 
 {
   programs.bat = {
     enable = true;
-    config.theme = "base16";
+    extraPackages = with pkgs.bat-extras; [ batdiff batman batgrep batwatch ];
+    themes = {
+      nord = {
+        src = pkgs.fetchFromGitHub {
+          owner = "nodetheme";
+          repo = "sublime"; # Bat uses sublime syntax for its themes
+          rev = "91eae63dc83ed501aa133d8f3266c301ab0cbf68";
+          hash = "sha256-PrhDhS1bYL+7AHzytOfNhnLIpi8p6WMv9TPsy/arVew=";
+        };
+        file = "Nord.tmTheme";
+      };
+    };
   };
 }
