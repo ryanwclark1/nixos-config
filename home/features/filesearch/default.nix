6ec81b2096f5e62549ac1d7f37d58bdba78304c8
Ryan Clark <ryanc@accentservices.com>
2024-02-20 13:30:59 -0600

    ripgrep update to include exclude args

--- home/features/filesearch/default.nix
+++ home/features/filesearch/default.nix
@@ -1,13 +1,24 @@
 {
-  lib,
   pkgs,
   ...
 }:
-with lib; {
+
+{
+  programs = {
+    ripgrep = {
+      enable = true;
+      arguments = [
+        "--glob=!.git/*"
+        "--glob=!node_modules/*"
+        "--glob=!.venv/*"
+        "--smart-case"
+      ];
+    };
+  };
+
   home.packages = with pkgs; [
     fd
     # ripgrep-all
-    ripgrep
     sd
   ];
 }
