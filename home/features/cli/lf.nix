ace6453b8af8e1529e4cb578787e6b4704df0d1d
Ryan Clark <ryanc@accentservices.com>
2024-02-20 13:30:33 -0600

    lf overhaul

--- home/features/cli/lf.nix
+++ home/features/cli/lf.nix
@@ -10,6 +10,10 @@
     mediainfo
     poppler_utils #pdftotext and pdfinfo
     hexyl
+    epub2txt2
+    ueberzugpp #for lf image previews
+    xlsx2csv
+    librsvg
   ];
 
   # programs.zathura = {
@@ -21,30 +25,82 @@
   programs.pistol = {
     enable = true;
     associations = [
-      { mime = "image/*"; command = "mediainfo %pistol-filename%"; }
-      { mime = "video/*"; command = "mediainfo %pistol-filename%"; }
-      { mime = "audio/*"; command = "mediainfo %pistol-filename%"; }
-      # { mime = "text/*"; command = "bat %pistol-filename%"; }
-      { mime = "application/pdf"; command = "sh: pdftotext -l 10 -nopgbrk -q -- %pistol-filename% - | fmt -w $(tput cols)"; }
-      { mime = "application/epub+zip"; command = "foliate %pistol-filename%"; }
-      # { mime = "application/zip"; command = "unzip -l %pistol-filename%"; }
-      # { mime = "application/x-tar"; command = "tar tf %pistol-filename%"; }
-      # { mime = "application/x-xz"; command = "tar tf %pistol-filename%"; }
-      # { mime = "application/x-bzip2"; command = "tar tf %pistol-filename%"; }
-      # { mime = "application/x-rar"; command = "unrar l %pistol-filename%"; }
-      # { mime = "application/x-7z-compressed"; command = "7z l %pistol-filename%"; }
-      { mime = "application/x-msdownload"; command = "exiftool %pistol-filename%"; }
-      { mime = "application/x-sharedlib"; command = "exiftool %pistol-filename%"; }
-      { mime = "application/x-executable"; command = "exiftool %pistol-filename%"; }
-      { mime = "application/x-font-ttf"; command = "exiftool %pistol-filename%"; }
-      { mime = "application/x-font-woff"; command = "exiftool %pistol-filename%"; }
-      { mime = "application/x-font-otf"; command = "exiftool %pistol-filename%"; }
-      { mime = "application/vnd.ms-opentype"; command = "exiftool %pistol-filename%"; }
-      { mime = "application/vnd.ms-fontobject"; command = "exiftool %pistol-filename%"; }
-      { mime = "application/vnd.ms-excel"; command = "exiftool %pistol-filename%"; }
-      # { mime = "application/json"; command = "bat %pistol-filename%"; }
-      { mime = "application/*"; command = "hexyl %pistol-filename%"; }
-      { fpath = ".*.md$"; command = "sh: bat --paging=never --color=always %pistol-filename% | head -8"; }
+      {
+        mime = "image/*";
+        command = "${pkgs.exiftool}/bin/exiftool %pistol-filename%";
+      }
+      {
+        mime = "video/*";
+        command = "${pkgs.exiftool}/bin/exiftool %pistol-filename%";
+      }
+      {
+        mime = "audio/*";
+        command = "${pkgs.exiftool}/bin/exiftool %pistol-filename%";
+      }
+      {
+        mime = "application/gzip";
+        command = "${pkgs.atool}/bin/atool -l %pistol-filename%";
+      }
+      {
+        mime = "application/pdf";
+        command = "sh: pdftotext -l 10 -nopgbrk -q -- %pistol-filename% - | fmt -w $(tput cols)";
+      }
+      {
+        mime = "application/epub+zip";
+        command = "epub2txt %pistol-filename%";
+      }
+      {
+        mime = "application/x-msdownload";
+        command = "${pkgs.exiftool}/bin/exiftool %pistol-filename%";
+      }
+      {
+        mime = "application/x-sharedlib";
+        command = "${pkgs.exiftool}/bin/exiftool %pistol-filename%";
+      }
+      {
+        mime = "application/x-executable";
+        command = "${pkgs.exiftool}/bin/exiftool %pistol-filename%";
+      }
+      {
+        mime = "application/x-font-ttf";
+        command = "${pkgs.exiftool}/bin/exiftool %pistol-filename%";
+      }
+      {
+        mime = "application/x-font-woff";
+        command = "${pkgs.exiftool}/bin/exiftool %pistol-filename%";
+      }
+      {
+        mime = "application/x-font-otf";
+        command = "${pkgs.exiftool}/bin/exiftool %pistol-filename%";
+      }
+      {
+        mime = "application/vnd.ms-opentype";
+        command = "${pkgs.exiftool}/bin/exiftool %pistol-filename%";
+      }
+      {
+        mime = "application/vnd.ms-fontobject";
+        command = "${pkgs.exiftool}/bin/exiftool %pistol-filename%";
+      }
+      {
+        mime = "application/vnd.ms-excel";
+        command = "${pkgs.exiftool}/bin/exiftool %pistol-filename%";
+      }
+      {
+        mime = "application/application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+        command = "${pkgs.xlsx2csv}/bin/xlsx2csv %pistol-filename%";
+      }
+      {
+        mime = "application/vnd.ms-excel";
+        command = "${pkgs.xlsx2csv}/bin/xlsx2csv %pistol-filename%";
+      }
+      # {
+      #   mime = "application/*";
+      #   command = "${pkgs.hexyl}/bin/hexyl %pistol-filename%";
+      # }
+      {
+        fpath = ".*.md$";
+        command = "sh: bat --paging=never --color=always %pistol-filename% | head -8";
+      }
     ];
   };
 
@@ -68,90 +124,90 @@
     };
 
     settings = {
-      # color256 = false;
       preview = true;
       hidden = true;
       drawbox = true;
       icons = true;
       ignorecase = true;
+      # color256 = false;
       # shell = "zsh";
       # shellopts = "-c";
     };
 
-    keybindings = {
-      nd = "mkdir";
-      nf = "mkfile";
-      "<c-f>" = "search";
-      "<space>" = "toggle";
-      f = "$EDITOR $(fzf)";
-      "<enter>" = "shell";
-      o = "&mimeopen $f";
-      O = "map O $mimeopen --ask $f";
-    };
+    # keybindings = {
+    #   nd = "mkdir";
+    #   nf = "mkfile";
+    #   "<c-f>" = "search";
+    #   "<space>" = "toggle";
+    #   f = "$EDITOR $(fzf)";
+    #   "<enter>" = "shell";
+    #   o = "&mimeopen $f";
+    #   O = "map O $mimeopen --ask $f";
+    # };
 
-    commands = {
-      f = "$EDITOR $(fzf --preview='pistol {})";
-      fzf_search = ''
-        ''${{
-            res="$( \
-                RG_PREFIX="rg --column --line-number --no-heading --color=always \
-                    --smart-case "
-                FZF_DEFAULT_COMMAND="$RG_PREFIX \'\'" \
-                    fzf --bind "change:reload:$RG_PREFIX {q} || true" \
-                    --ansi --layout=reverse --header 'Search in files' \
-                    | cut -d':' -f1
-            )"
-            [ ! -z "$res" ] && lf -remote "send $id select \"$res\""
-        }}
-      '';
+    # commands = {
+    #   f = "$EDITOR $(fzf --preview='pistol {})";
+    #   fzf_search = ''
+    #     ''${{
+    #         res="$( \
+    #             RG_PREFIX="rg --column --line-number --no-heading --color=always \
+    #                 --smart-case "
+    #             FZF_DEFAULT_COMMAND="$RG_PREFIX \'\'" \
+    #                 fzf --bind "change:reload:$RG_PREFIX {q} || true" \
+    #                 --ansi --layout=reverse --header 'Search in files' \
+    #                 | cut -d':' -f1
+    #         )"
+    #         [ ! -z "$res" ] && lf -remote "send $id select \"$res\""
+    #     }}
+    #   '';
 
-      open = ''
-        ''${{
-            test -L $f && f=$(readlink -f $f)
-            case $(file --mime-type $f -b) in
-                text/*) $EDITOR $fx;;
-                *) for f in $fx; do setsid $OPENER $f > /dev/null 2> /dev/null & done;;
-            esac
-        }}
-      '';
+    #   open = ''
+    #     ''${{
+    #         test -L $f && f=$(readlink -f $f)
+    #         case $(file --mime-type $f -b) in
+    #             text/*) $EDITOR $fx;;
+    #             *) for f in $fx; do setsid $OPENER $f > /dev/null 2> /dev/null & done;;
+    #         esac
+    #     }}
+    #   '';
 
-      mkdir = ''
-        ''${{
-          printf "Directory Name: "
-          read ans
-          mkdir $ans
-        }}
-      '';
+    #   mkdir = ''
+    #     ''${{
+    #       printf "Directory Name: "
+    #       read ans
+    #       mkdir $ans
+    #     }}
+    #   '';
 
-      mkfile = ''
-        ''${{
-          printf "File Name: "
-          read ans
-          $EDITOR $ans
-        }}
-      '';
+    #   mkfile = ''
+    #     ''${{
+    #       printf "File Name: "
+    #       read ans
+    #       $EDITOR $ans
+    #     }}
+    #   '';
 
-      extract = ''
-        ''${{
-            set -f
-            atool -x $f
-        }}
-      '';
+    #   extract = ''
+    #     ''${{
+    #         set -f
+    #         atool -x $f
+    #     }}
+    #   '';
 
-      z = ''
-        %{{
-              	result="$(zoxide query --exclude $PWD $@)"
-              	lf -remote "send $id cd $result"
-        }}
-      '';
+    #   z = ''
+    #     %{{
+    #           	result="$(zoxide query --exclude $PWD $@)"
+    #           	lf -remote "send $id cd $result"
+    #     }}
+    #   '';
 
-      zi = ''
-        ''${{
-        	result="$(zoxide query -i)"
-        	lf -remote "send $id cd $result"
-        }}
-      '';
-    };
+    #   zi = ''
+    #     ''${{
+    #     	result="$(zoxide query -i)"
+    #     	lf -remote "send $id cd $result"
+    #     }}
+    #   '';
+    # };
   };
 
 }
