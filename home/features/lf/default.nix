642c40605e3eaf331a948c002afcf7c81df8f330
Ryan Clark <ryanc@accentservices.com>
2024-02-22 22:41:35 -0600

    Add support for ISO9660 image and octet-stream file types

--- home/features/lf/default.nix
+++ home/features/lf/default.nix
@@ -62,8 +62,11 @@
           application/zip)
               ${pkgs.exiftool}/bin/exiftool -s $EXIF_TAGS "$1"; echo -e "\n"; ${pkgs.atool}/bin/atool --list -- "$1"
             ;;
-          application/gzip|application/x-xz)
+          application/gzip|application/x-xz|application/x-tarapplication/x-tar)
             ${pkgs.exiftool}/bin/exiftool -s $EXIF_TAGS "$1";echo -e "\n"; ${pkgs.atool}/bin/atool --list -- "$1"| sed '\Permission\tUID/GID\t\tSize\tDate\tTime\tFileName\n---------\t-------\t\t----\t----\t----\t--------'
+            ;;
+          application/x-iso9660-image)
+            ${pkgs.exiftool}/bin/exiftool -s $EXIF_TAGS "$1"; echo -e "\n"; ${pkgs.atool}/bin/atool --list -- "$1"
             ;;
           application/pdf)
             ${pkgs.exiftool}/bin/exiftool -s $EXIF_TAGS "$1"; echo -e "\n"; ${pkgs.poppler_utils}/bin/pdftotext -l 4 -nopgbrk -q -- "$1" - | fmt -w $(tput cols)
@@ -104,6 +107,9 @@
           application/vnd.sqlite3)
             # open sqlite3 database and show table names in the terminal
             ${pkgs.exiftool}/bin/exiftool "$1"; echo -e "\n"; sqlite3 "$1" ".tables";echo "$MIMETYPE"; echo "$1"
+            ;;
+          application/octet-stream)
+            ${pkgs.exiftool}/bin/exiftool -s $EXIF_TAGS "$1"; echo -e "\n"; ${pkgs.hexyl}/bin/hexyl "$1"
             ;;
           application/*)
             ${pkgs.exiftool}/bin/exiftool -s $EXIF_TAGS "$1"; echo -e "\n"; echo "$MIMETYPE"; echo "$1"
