9cec3ac1f7a9f8499ad4e84ce2107c372e1e965d
Ryan Clark <ryanc@accentservices.com>
2024-02-24 15:25:47 -0600

    Add VSCode server and configure power management and lid settings***

--- hosts/frametop/services/default.nix
+++ hosts/frametop/services/default.nix
@@ -1,1 +1,39 @@
-{ }
+{
+  inputs,
+  ...
+}:
+
+{
+  import = [
+    inputs.vscode-server.nixosModules.default
+  ];
+
+  services = {
+    fwupd.enable = true;
+    # fprintd = {
+    #   enable = true;
+    # };
+    logind = {
+      # Lid settings
+      lidSwitch = "suspend";
+      lidSwitchExternalPower = "lock";
+    };
+    vscode-server = {
+      enable = true;
+    };
+
+    xserver = {
+      libinput = {
+        enable = true;
+        touchpad = {
+          naturalScrolling = true;
+          scrollMethod = "twofinger";
+        };
+      };
+    };
+  };
+
+  powerManagement.powertop.enable = true;
+
+  # security.pam.services.login.fprintAuth = true;
+}
