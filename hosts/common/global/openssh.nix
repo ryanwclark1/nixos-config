2cd7f1563ad77a779ad0b2532e9032d5833d9efc
Ryan Clark <ryanc@accentservices.com>
2024-02-26 13:34:47 -0600

    Update openssh.nix file with knownHosts configuration

--- hosts/common/global/openssh.nix
+++ hosts/common/global/openssh.nix
@@ -26,25 +26,26 @@ in
     };
 
     hostKeys = [{
-      path = "~/.ssh/ssh_host_ed25519_key";
+      path = "$HOME/.ssh/ssh_host_ed25519_key";
       type = "ed25519";
     }];
   };
 
   programs.ssh = { # Each hosts public key
-    # knownHosts = builtins.mapAttrs
-    #   (name: _: {
-    #     # publicKeyFile = pubKey name;
-    #     extraHostNames =
-    #       (lib.optional (name == hostName) "localhost") ++ # Alias for localhost if it's the same host
-    #       # (lib.optionals (name == gitHost) [ "techcasa.io" ]); # Alias for techcasa.io
-    #   })
-    #   hosts;
+    knownHosts = builtins.mapAttrs
+      (name: _: {
+        # publicKeyFile = pubKey name;
+        extraHostNames =
+          (lib.optional (name == hostName) "localhost");
+          #  ++ # Alias for localhost if it's the same host
+          # (lib.optionals (name == gitHost) [ "techcasa.io" ]); # Alias for techcasa.io
+      })
+      hosts;
   };
 
 # Passwordless sudo when SSH'ing with keys
-  # security.pam.sshAgentAuth = {
-  #   enable = true;
-  #   authorizedKeysFiles = [ "/etc/ssh/authorized_keys.d/%u" ];
-  # };
+  security.pam.sshAgentAuth = {
+    enable = true;
+    authorizedKeysFiles = [ "/etc/ssh/authorized_keys.d/%u" ];
+  };
 }
