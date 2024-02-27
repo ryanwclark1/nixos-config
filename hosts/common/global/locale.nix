cadc4e26150756b63ebebe1316876f498d0f9212
Ryan Clark <ryanc@accentservices.com>
2024-02-26 20:51:17 -0600

    Remove extraLocaleSettings from i18n in locale.nix

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -6,9 +6,6 @@
 {
   i18n = {
     defaultLocale = lib.mkDefault "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_TIME = "en_US.UTF-8";
-    };
     supportedLocales = lib.mkDefault [
       "en_US.UTF-8/UTF-8"
     ];
