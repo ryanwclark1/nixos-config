39814461e9054f0b0a5104f0bab658e0d687c014
Ryan Clark <ryanc@accentservices.com>
2024-02-28 16:40:49 -0600

    Remove unused kernel module and update available kernel modules

--- hosts/woody/hardware-configuration.nix
+++ hosts/woody/hardware-configuration.nix
@@ -9,8 +9,8 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-  # boot.initrd.kernelModules = [ "amdgpu" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" ];
+  boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
