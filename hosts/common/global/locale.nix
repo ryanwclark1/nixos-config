9be86b4f8c421399485700ed6634e41e9b32d142
Ryan Clark <ryanc@accentservices.com>
2024-03-04 15:27:06 -0600

    Remove unsupported locale and gnupg dependency

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -6,9 +6,6 @@
 {
   i18n = {
     defaultLocale = lib.mkDefault "en_US.UTF-8";
-    supportedLocales = lib.mkDefault [
-      "en_US.UTF-8/UTF-8"
-    ];
   };
   location.provider = "geoclue2";
   time.timeZone = lib.mkDefault "America/Chicago";
