da3c1704344f00b3db36244a6b621fca81325d7e
Ryan Clark <ryanc@accentservices.com>
2024-03-04 22:10:35 -0600

    hyprland qt5

--- hosts/common/optional/hyprland/default.nix
+++ hosts/common/optional/hyprland/default.nix
@@ -59,7 +59,7 @@
 
   # DO NOT USE ON OTHER DEs
   qt.style = "kvantum";
-  qt.platformTheme = "qt6ct";
+  qt.platformTheme = "qt5ct";
 
 
   # Use librsvg's gdk-pixbuf loader cache file as it enables gdk-pixbuf to load SVG files (important for icons in GTK apps)
