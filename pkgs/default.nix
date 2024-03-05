e6898cd8f6e645e4a9c919d197bdf5bea7aec746
Ryan Clark <ryanc@accentservices.com>
2024-03-05 14:38:31 -0600

    Revert "Remove unused modules and plugins"
    This reverts commit c2ce90a29d5a83c198807a622e49ba26d73c6ac4.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,16 @@
-{ pkgs ? import <nixpkgs> { } }: {
-  gitkraken = pkgs.callPackage ./gitkraken { };
+{ pkgs ? import <nixpkgs> { } }: rec {
+
+  # Packages with an actual source
+  # shellcolord = pkgs.callPackage ./shellcolord { };
+  # trekscii = pkgs.callPackage ./trekscii { };
+
+  # Personal scripts
+  # nix-inspect = pkgs.callPackage ./nix-inspect { };
+  lyrics = pkgs.python3Packages.callPackage ./lyrics { };
+  xpo = pkgs.callPackage ./xpo { };
+  # tly = pkgs.callPackage ./tly { };
+  hyprslurp = pkgs.callPackage ./hyprslurp { };
+
+  # My wallpaper collection
   # wallpapers = pkgs.callPackage ./wallpapers { };
 }
