212184674a328a31389cea0fa9cb5b0dd4948058
Ryan Clark <ryanc@accentservices.com>
2024-03-05 22:46:43 -0600

    Remove unused GTK and Qt configurations

--- home/features/desktop/common/default.nix
+++ home/features/desktop/common/default.nix
@@ -10,11 +10,9 @@
     ./dragon.nix
     ./firefox.nix
     ./font.nix
-    ./gtk.nix
     ./kdeconnect.nix
     ./pavucontrol.nix
     ./playerctl.nix
-    ./qt.nix
     ./slack.nix
     ./sublime-music.nix
   ];
