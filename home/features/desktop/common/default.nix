4ca200f218a73c836dd9c7ca3847c69333abcda8
Ryan Clark <ryanc@accentservices.com>
2024-03-05 22:12:27 -0600

    Update package imports and add missing dependencies

--- home/features/desktop/common/default.nix
+++ home/features/desktop/common/default.nix
@@ -7,20 +7,18 @@
   imports = [
     ./deluge.nix
     ./discord.nix
-    #   ./dragon.nix
+    ./dragon.nix
     ./firefox.nix
     ./font.nix
-    #   ./gtk.nix
+    ./gtk.nix
     ./kdeconnect.nix
-    #   ./pavucontrol.nix
-    #   ./playerctl.nix
-    #   ./qt.nix
-    #   ./slack.nix
-    #   ./sublime-music.nix
+    ./pavucontrol.nix
+    ./playerctl.nix
+    ./qt.nix
+    ./slack.nix
+    ./sublime-music.nix
   ];
 
-  # Requires an implementation in xdg.portal.extraportals such as xdg-desktop-portal-kde or xdg-desktop-portal-gtk
-  # xdg.portal.enable = true;
 
   nixpkgs.config.permittedInsecurePackages = [
     "electron-19.1.9"
@@ -29,29 +27,21 @@
 
   home.packages = with pkgs; [
     fortune
-    # openssl_3
     dfeet # Dbus debugger
-
     ventoy-full #balena type tool
     etcher
-
-    # misc
     libnotify
     wineWowPackages.wayland
-    # wineWowPackages.stagingFull
     xdg-utils
-    # Wayland, Xorg
     wl-clipboard
     mediainfo
     remmina # XRDP & VNC Client
     bluez-tools # bt-adapter
-
     scrot
     element-desktop
     megatools
     megasync
-    # Duplicate file finder
-    czkawka
+    czkawka # Duplicate file finder
     f1viewer
     tickrs
     # fantomas
