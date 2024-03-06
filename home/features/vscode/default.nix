4701c4d2665f14ef39f4c9810cc9b39356d42420
Ryan Clark <ryanc@accentservices.com>
2024-03-05 19:38:36 -0600

    Update VSCode version to 1.87.0

--- home/features/vscode/default.nix
+++ home/features/vscode/default.nix
@@ -4,6 +4,23 @@
 }:
 
 {
+  nixpkgs.overlays = [
+    (
+      final: prev: {
+        vscode = prev.vscode.overrideAttrs (_: rec {
+          version = "1.87.0";
+          plat = "linux-x64";
+          archive_fmt = "tar.gz";
+          pname = "vscode";
+          src = prev.fetchurl {
+            url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
+            sha256 = "00izdy01d34czxfjn6rv4vg179r7f264bls5fib4caakj9bblalw";
+            name = "VSCode_${version}_${plat}.${archive_fmt}";
+          };
+        });
+      }
+    )
+  ];
 
   home.packages = with pkgs; [
     tailwindcss
