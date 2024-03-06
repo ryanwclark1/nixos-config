55fd3ddb5c4909839913d502a5c001ca66bc90f7
Ryan Clark <ryanc@accentservices.com>
2024-03-06 11:09:29 -0600

    chilli for sddm

--- hosts/common/optional/sddm.nix
+++ hosts/common/optional/sddm.nix
@@ -4,6 +4,21 @@
 }:
 
 {
+
+  environment.systemPackages = with pkgs;
+    [
+      (sddm-chili-theme.override {
+        themeConfig = {
+          background = config.stylix.image;
+          # ScreenWidth = 1920;
+          # ScreenHeight = 1080;
+          blur = true;
+          recursiveBlurLoops = 3;
+          recursiveBlurRadius = 5;
+        };
+      })
+    ];
+
   services = {
     xserver = {
       enable = true;
@@ -12,6 +27,7 @@
         sddm = {
           enable = true;
           wayland.enable = true;
+          theme = "chili";
         };
       };
     };
