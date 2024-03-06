05471662d14c95e79d79a3a034f042d9434b140c
Ryan Clark <ryanc@accentservices.com>
2024-03-06 16:58:16 -0600

    Add Hyprland and hyprland-plugins as inputs***

--- flake.nix
+++ flake.nix
@@ -21,6 +21,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
+    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
