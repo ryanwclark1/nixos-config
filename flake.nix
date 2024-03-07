295a98e345a7f986fc4aad26507a4528c49e1720
Ryan Clark <ryanc@accentservices.com>
2024-03-07 15:22:17 -0600

    Clean up syntax

--- flake.nix
+++ flake.nix
@@ -78,6 +78,5 @@
           extraSpecialArgs = { inherit inputs outputs; };
         };
       };
-
     };
 }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,10 @@
 { pkgs ? import <nixpkgs> { } }: {
 
+  aichat = pkgs.callPackage ./aichat { };
   gitkraken = pkgs.callPackage ./gitkraken { };
   # shellcolord = pkgs.callPackage ./shellcolord { };
 
   nix-inspect = pkgs.callPackage ./nix-inspect { };
   # wallpapers = pkgs.callPackage ./wallpapers { };
+  multiviewer = pkgs.callPackage ./multiviewer { };
 }
