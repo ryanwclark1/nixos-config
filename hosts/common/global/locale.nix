506c7df76e7eed13aac6bc1faab00ec4c566895a
Ryan Clark <ryanc@accentservices.com>
2024-03-07 13:01:50 -0600

    Add geoclue2 service configuration

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -7,6 +7,10 @@
   i18n = {
     defaultLocale = lib.mkDefault "en_US.UTF-8";
   };
+  services.geoclue2 = {
+    enable = true;
+    submitData = false;
+  };
   location.provider = "geoclue2";
   time.timeZone = lib.mkDefault "America/Chicago";
 }
