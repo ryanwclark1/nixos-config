ea558258f34221faef327ae12f0a9d91b383f00c
Ryan Clark <ryanc@accentservices.com>
2024-03-07 12:04:59 -0600

    Remove khard, khal, todoman, vdirsyncer, and mail configurations

--- home/features/productivity/default.nix
+++ home/features/productivity/default.nix
@@ -1,12 +1,6 @@
 {
   imports = [
     ./flameshot.nix
-    # ./khal.nix
-    ./khard.nix
-    ./todoman.nix
-    # ./vdirsyncer.nix
-    ./mail.nix
-    ./neomutt.nix
     ./office.nix
     ./zathura.nix
 
