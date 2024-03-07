f94df71ed54266bafa890c5d28f137c754d90620
Ryan Clark <ryanc@accentservices.com>
2024-03-07 16:01:28 -0600

    Add multiviewer package and desktop item

--- home/features/desktop/common/default.nix
+++ home/features/desktop/common/default.nix
@@ -39,6 +39,7 @@
     f1viewer
     tickrs
     # fantomas
+    # multiviewer
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
