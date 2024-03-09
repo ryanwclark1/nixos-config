779770a1d609da7c185737c16234c628b09663fa
Ryan Clark <ryanc@accentservices.com>
2024-03-09 17:20:24 -0600

    Added SDDM display manager  theme configuration

--- hosts/frametop/default.nix
+++ hosts/frametop/default.nix
@@ -25,7 +25,7 @@
     ../common/optional/wireshark.nix
 
     ../common/optional/hyprland
-    ../common/optional/displaymanager/sddm.nix
+    ../common/optional/displaymanager/sddm
     ../common/optional/plasma
   ];
 
