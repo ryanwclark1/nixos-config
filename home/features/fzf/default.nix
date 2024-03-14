46f992d224b6ee30336293554fd08448c9bce1fe
Ryan Clark <ryanc@accentservices.com>
2024-03-14 13:16:05 -0500

    fzf styling moved to stylix

--- home/features/fzf/default.nix
+++ home/features/fzf/default.nix
@@ -5,28 +5,28 @@
   pkgs,
   ...
 }:
-let
-  inherit (config.colorscheme) palette;
-in
+# let
+#   inherit (config.colorscheme) palette;
+# in
 {
 # source = pkgs.writeShellScript "pv.sh" ''
   programs.fzf = {
     enable = true;
     package = pkgs.fzf;
-    colors = {
-      "fg" = "#${palette.base06}";
-      "bg" = "#${palette.base03}";
-      "hl" = "#${palette.base0B}";
-      "fg+" = "#${palette.base04}";
-      "bg+" = "#${palette.base00}";
-      "hl+" = "#${palette.base04}";
-      "pointer" = "#${palette.base08}";
-      "info" = "#${palette.base03}";
-      "spinner" = "#${palette.base03}";
-      "header" = "#${palette.base03}";
-      "prompt" = "#${palette.base0D}";
-      "marker" = "#${palette.base0A}";
-    };
+    # colors = {
+    #   "fg" = "#${palette.base06}";
+    #   "bg" = "#${palette.base03}";
+    #   "hl" = "#${palette.base0B}";
+    #   "fg+" = "#${palette.base04}";
+    #   "bg+" = "#${palette.base00}";
+    #   "hl+" = "#${palette.base04}";
+    #   "pointer" = "#${palette.base08}";
+    #   "info" = "#${palette.base03}";
+    #   "spinner" = "#${palette.base03}";
+    #   "header" = "#${palette.base03}";
+    #   "prompt" = "#${palette.base0D}";
+    #   "marker" = "#${palette.base0A}";
+    # };
     defaultCommand = "find . -type f ! -path '.git'";
     defaultOptions = [
       "--preview='pistol {}'"
