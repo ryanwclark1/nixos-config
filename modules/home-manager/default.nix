81068b26308b2094102fff9f8e8b258152f7f81d
Ryan Clark <ryanc@accentservices.com>
2024-03-13 22:32:27 -0500

    Remove unused imports

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,7 +1,3 @@
 {
-  fonts = import ./fonts.nix;
-  # monitors = import ./monitors.nix;
-  # pass-secret-service = import ./pass-secret-service.nix;
-  # shellcolor = import ./shellcolor.nix;
-  # wallpaper = import ./wallpaper.nix;
+
 }
