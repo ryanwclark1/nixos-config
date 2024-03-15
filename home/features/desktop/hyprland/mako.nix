7a7c8c5aa339a0075d91f7d3fffb546f2285bca0
Ryan Clark <ryanc@accentservices.com>
2024-03-15 06:31:36 -0500

    Commented out font and color settings in mako.nix file

--- home/features/desktop/hyprland/mako.nix
+++ home/features/desktop/hyprland/mako.nix
@@ -2,9 +2,9 @@
   config,
   ...
 }:
-let
-  inherit (config.colorscheme) palette variant;
-in
+# let
+#   inherit (config.colorscheme) palette variant;
+# in
 {
   services.mako = {
     enable = true;
@@ -13,16 +13,16 @@ in
         "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark"
       else
         "${config.gtk.iconTheme.package}/share/icons/Papirus-Light";
-    font = "${config.fontProfiles.regular.family} 12";
+    # font = "${config.fontProfiles.regular.family} 12";
     padding = "10,20";
     anchor = "top-center";
     width = 400;
     height = 150;
     borderSize = 2;
     defaultTimeout = 12000;
-    backgroundColor = "#${palette.base00}dd";
-    borderColor = "#${palette.base03}dd";
-    textColor = "#${palette.base05}dd";
+    # backgroundColor = "#${palette.base00}dd";
+    # borderColor = "#${palette.base03}dd";
+    # textColor = "#${palette.base05}dd";
     layer = "overlay";
   };
 }
