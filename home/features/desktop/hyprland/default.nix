d9cd67145333ba048d0c31125af77ae954d5e13d
Ryan Clark <ryanc@accentservices.com>
2024-03-15 16:57:06 -0500

    start moving hyprland to nix file not dot files

--- home/features/desktop/hyprland/default.nix
+++ home/features/desktop/hyprland/default.nix
@@ -28,10 +28,10 @@
       source = ./config/swaync;
       recursive = true;
     };
-    file.".config/hypr" = {
-      source = ./config/hyprland;
-      recursive = true;
-    };
+    # file.".config/hypr" = {
+    #   source = ./config/hyprland;
+    #   recursive = true;
+    # };
   };
 
   home.packages = with pkgs; [
