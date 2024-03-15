469d3e878c49c3576d58d27efae90a6d8261561c
Ryan Clark <ryanc@accentservices.com>
2024-03-15 07:14:41 -0500

    rofi overhaul

--- home/features/desktop/hyprland/rofi.nix
+++ home/features/desktop/hyprland/rofi.nix
@@ -5,19 +5,24 @@
 {
   programs.rofi = {
     enable = true;
-
+    package = pkgs.rofi-wayland;
+    plugins = [ pkgs.rofi-calc pkgs.rofi-emoji ];
     extraConfig = {
       bw = 1;
       columns = 2;
       icon-theme = "Papirus-Dark";
     };
-
-    # theme = "gruvbox-dark-soft";
-
-    # font = "Fira Code 12";
-
-    package = pkgs.rofi.override {
-      plugins = [ pkgs.rofi-calc pkgs.rofi-emoji ];
+    extraConfig = {
+      modi = "drun,emoji,calc";
+      show-icons = true;
+      drun-display-format = "{icon} {name}";
+      disable-history = false;
+      hide-scrollbar = true;
+      display-drun = "   Apps ";
+      display-run = "   Run ";
+      display-emoji = "   Emoji ";
+      display-calc = "   Calc ";
+      sidebar-mode = true;
     };
   };
 }
