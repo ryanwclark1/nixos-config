6e5fb3baeb275f4a8b10a96ba88906855f1ae938
Ryan Clark <ryanc@accentservices.com>
2024-03-15 12:42:18 -0500

    update mako remove icons

--- home/features/desktop/hyprland/mako.nix
+++ home/features/desktop/hyprland/mako.nix
@@ -8,12 +8,6 @@
 {
   services.mako = {
     enable = true;
-    iconPath =
-      if variant == "dark" then
-        "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark"
-      else
-        "${config.gtk.iconTheme.package}/share/icons/Papirus-Light";
-    # font = "${config.fontProfiles.regular.family} 12";
     padding = "10,20";
     anchor = "top-center";
     width = 400;
