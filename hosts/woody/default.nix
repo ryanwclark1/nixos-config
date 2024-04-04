99119b5674531b34ad9f343ba23707df253a8285
Ryan Clark <ryanc@accentservices.com>
2024-04-04 10:29:32 -0500

    Move to linux kernel 6.8

--- hosts/woody/default.nix
+++ hosts/woody/default.nix
@@ -43,18 +43,19 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_6_7.override {
-      argsOverride = rec {
-        src = pkgs.fetchurl {
-              url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-              sha256 = "sha256-Rp/0a5hoXfE7VsmEF8ZLp6MPikW680qpnweTXhv2XBg=";
-        };
-        version = "6.7.8";
-        modDirVersion = "6.7.8";
-        };
-    });
+    kernelPackages = pkgs.linux_6_8;
 
-    # initrd.kernelModules = [ "amdgpu" ];
+    # kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_6_7.override {
+    #   argsOverride = rec {
+    #     src = pkgs.fetchurl {
+    #           url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
+    #           sha256 = "sha256-Rp/0a5hoXfE7VsmEF8ZLp6MPikW680qpnweTXhv2XBg=";
+    #     };
+    #     version = "6.7.8";
+    #     modDirVersion = "6.7.8";
+    #     };
+    # });
+
     # binfmt.emulatedSystems = [ "aarch64-linux" "i686-linux" ];
   };
 
