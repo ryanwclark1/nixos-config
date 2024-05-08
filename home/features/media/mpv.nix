473868008e8a73ad756f5eb64bc18a1c5f6be18c
Ryan Clark <ryanc@accentservices.com>
2024-05-08 10:02:38 -0500

    Update mpv package configuration

--- home/features/media/mpv.nix
+++ home/features/media/mpv.nix
@@ -7,7 +7,6 @@
   programs = {
     mpv = {
       enable = true;
-      package = pkgs.wrapMpv (pkgs.mpv-unwrapped.override { vapoursynthSupport = true; }) { youtubeSupport = true; };
       defaultProfiles = [ "high-quality" ];
       scripts = [ pkgs.mpvScripts.mpris ];
     };
