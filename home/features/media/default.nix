ae212d713282c26e8f56d71c7db593bf3b98814c
Ryan Clark <ryanc@accentservices.com>
2024-05-08 09:35:45 -0500

    Update media-related packages and configurations

--- home/features/media/default.nix
+++ home/features/media/default.nix
@@ -7,11 +7,12 @@
     ./darktable.nix
     ./graphical.nix
     ./gstreamer.nix
-    ./mediautils.nix
+    ./mpv.nix
     ./music.nix
     ./obs.nix
     ./pavucontrol.nix
     ./playerctl.nix
+    ./pulseaudio.nix
     ./video.nix
     ./yt-dlp.nix
   ];
