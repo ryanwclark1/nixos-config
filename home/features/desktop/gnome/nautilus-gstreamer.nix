26fb71927b88b6a9d0ec090eec3abd7b9c7d9cbe
Ryan Clark <ryanc@accentservices.com>
2024-05-08 09:04:03 -0500

    Update GST_PLUGIN_SYSTEM_PATH_1_0 in nautilus-gstreamer.nix

--- home/features/desktop/gnome/nautilus-gstreamer.nix
+++ home/features/desktop/gnome/nautilus-gstreamer.nix
@@ -1,19 +1,27 @@
 {
   pkgs,
+  lib,
   ...
 }:
 
 {
-  nixpkgs.overlays = [
-    (self: super: {
-      gnome = super.gnome.overrideScope' (gself: gsuper: {
-        nautilus = gsuper.nautilus.overrideAttrs (nsuper: {
-          buildInputs = nsuper.buildInputs ++ (with pkgs.gst_all_1; [
-            gst-plugins-good
-            gst-plugins-bad
-          ]);
-        });
-      });
-    })
-  ];
+  environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
+  gst-plugins-good
+  gst-plugins-bad
+  gst-plugins-ugly
+  gst-libav
+  ]);
+
+  # nixpkgs.overlays = [
+  #   (self: super: {
+  #     gnome = super.gnome.overrideScope' (gself: gsuper: {
+  #       nautilus = gsuper.nautilus.overrideAttrs (nsuper: {
+  #         buildInputs = nsuper.buildInputs ++ (with pkgs.gst_all_1; [
+  #           gst-plugins-good
+  #           gst-plugins-bad
+  #         ]);
+  #       });
+  #     });
+  #   })
+  # ];
 }
