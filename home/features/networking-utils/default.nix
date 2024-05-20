535be04b749ce87564c43f2178c2c7b61b99efeb
Ryan Clark <ryanc@accentservices.com>
2024-05-20 09:00:42 -0500

    wg-nordvpn and openresolv added.

--- home/features/networking-utils/default.nix
+++ home/features/networking-utils/default.nix
@@ -5,15 +5,17 @@
 
 {
   home.packages = with pkgs; [
-    dnsutils # `dig` + `nslookup`
-    ldns # replacement of `dig`, it provide the command `drill`
     aria2 # A lightweight multi-protocol & multi-source command-line download utility
-    nmap # A utility for network discovery and security auditing
+    dnsutils # `dig` + `nslookup`
     ipcalc # it is a calculator for the IPv4/v6 addresses
-    wireguard-tools
+    ldns # replacement of `dig`, it provide the command `drill`
     libpcap
-    tcpdump
-    rathole # similar to ngrok - NAT traversal and reverse proxy
     ngrep
+    nmap # A utility for network discovery and security auditing
+    openresolv # a resolv.conf management framework
+    rathole # similar to ngrok - NAT traversal and reverse proxy
+    tcpdump
+    wgnord
+    wireguard-tools
   ];
 }
