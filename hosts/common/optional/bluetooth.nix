b846ca9d1ec040a3c6e84eedbe7dbb880427d9b1
Ryan Clark <ryanc@accentservices.com>
2024-06-26 13:31:04 -0500

    bluetooth updates to settings

--- hosts/common/optional/bluetooth.nix
+++ hosts/common/optional/bluetooth.nix
@@ -8,6 +8,17 @@
     bluetooth = {
       enable = true;
       powerOnBoot = true;
+      settings = {
+        General = {
+          Name = "Hello";
+          ControllerMode = "dual";
+          FastConnectable = "true";
+          Experimental = "true";
+        };
+        Policy = {
+          AutoEnable = "true";
+        };
+      };
     };
   };
   # Blueman is a GTK+ Bluetooth Manager
