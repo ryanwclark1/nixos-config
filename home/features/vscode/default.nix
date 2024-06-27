1a8c287c0b9cd3faeaabf958a6e618ad66a7d7c6
Ryan Clark <ryanwclark@yahoo.com>
2024-06-27 17:19:48 -0500

    Update vscode overlay

--- home/features/vscode/default.nix
+++ home/features/vscode/default.nix
@@ -7,28 +7,8 @@
 
 
 {
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "vscode"
-  ];
-  # nixpkgs.config.allowUnfree = true;
 
-  nixpkgs.overlays = [
-    (
-      final: prev: {
-        vscode = prev.vscode.overrideAttrs (_: rec {
-          version = "1.90.2";
-          plat = "linux-x64";
-          archive_fmt = "tar.gz";
-          pname = "vscode";
-          src = prev.fetchurl {
-            url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
-            sha256 = "bc15c4bf497c569af0726c218328c6ffe85a2189f544897be52157a7a27e0c34";
-            name = "VSCode_${version}_${plat}.${archive_fmt}";
-          };
-        });
-      }
-    )
-  ];
+# Overlay for VSCode version
 
   home.packages = with pkgs; [
     tailwindcss
--- overlays/default.nix
+++ overlays/default.nix
@@ -9,13 +9,13 @@
 
   modifications = final: prev: {
     vscode = prev.vscode.overrideAttrs (_: rec {
-      version = "1.89.0";
+      version = "1.90.2";
       plat = "linux-x64";
       archive_fmt = "tar.gz";
       pname = "vscode";
       src = prev.fetchurl {
         url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
-        sha256 = "sha256-vGDY57xMuEJrmJBwQ0ufnAKt1GR16jEDKt5/fva9wUM=";
+        sha256 = "bc15c4bf497c569af0726c218328c6ffe85a2189f544897be52157a7a27e0c34";
         name = "VSCode_${version}_${plat}.${archive_fmt}";
       };
     });
