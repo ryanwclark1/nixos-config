f608aacd8456bfb66237a0e3830d2c62d5ccb5ce
Ryan Clark <ryanwclark@yahoo.com>
2024-06-28 10:20:19 -0500

    Add btop

---
+++ home/features/cli/btop.nix
@@ -0,0 +1,19 @@
+{
+  ...
+}:
+
+{
+  programs.btop = {
+    enable = true;
+
+    settings = {
+      # color_theme = "gruvbox_dark";
+      vim_keys = true;
+      update_ms = 1000;
+      disks_filter = "";
+      # mem_graphs = false;
+      proc_per_core = true;
+      # net_iface = "wg0";
+    };
+  };
+}
