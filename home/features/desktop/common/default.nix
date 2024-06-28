e65c3c652a72f7d434bd05a729cf3708211fb281
Ryan Clark <ryanwclark@yahoo.com>
2024-06-28 12:12:38 -0500

    Updating desktop common features.

--- home/features/desktop/common/default.nix
+++ home/features/desktop/common/default.nix
@@ -8,9 +8,9 @@
     ./deluge.nix
     ./discord.nix
     ./firefox.nix
-    # ./kdeconnect.nix
+    ./kdeconnect.nix
     ./slack.nix
-    # ./sublime-music.nix
+    ./sublime-music.nix
   ];
 
   home.packages = with pkgs; [
@@ -21,17 +21,11 @@
     wl-clipboard
     mediainfo
     bluez-tools # bt-adapter
-
     element-desktop
-    megatools
     megasync
     czkawka # Duplicate file finder
-    f1viewer
     tickrs
     gpu-viewer
-
-    # fantomas
-    # multiviewer
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
