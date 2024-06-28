9a623947677e36186e01fe64f25cf285ba1778b6
Ryan Clark <ryanwclark@yahoo.com>
2024-06-27 20:32:08 -0500

    Updating hyprland test config

--- hosts/common/optional/hyprland/default.nix
+++ hosts/common/optional/hyprland/default.nix
@@ -9,13 +9,13 @@
   # ];
 
   programs = {
-    hyprland = {
-      systemd.setPath.enable = false;
-      xwayland.enable = true;
-      enable = true;
-      package = pkgs.hyprland;
-      portalPackage = pkgs.xdg-desktop-portal-hyprland;
-    };
+    # hyprland = {
+    #   systemd.setPath.enable = false;
+    #   xwayland.enable = true;
+    #   enable = true;
+    #   package = pkgs.hyprland;
+    #   portalPackage = pkgs.xdg-desktop-portal-hyprland;
+    # };
     mtr.enable = true;
     gnupg.agent = {
       enable = true;
@@ -25,19 +25,19 @@
     thunar.enable = true;
   };
 
-  services = {
-    xserver = {
-      enable = true;
-      xkb.layout = "us";
-    };
-    libinput = {
-      enable = true;
-      touchpad = {
-        naturalScrolling = true;
-        scrollMethod = "twofinger";
-      };
-    };
-  };
+  # services = {
+    # xserver = {
+    #   enable = true;
+    #   xkb.layout = "us";
+    # };
+    # libinput = {
+    #   enable = true;
+    #   touchpad = {
+    #     naturalScrolling = true;
+    #     scrollMethod = "twofinger";
+    #   };
+    # };
+  # };
 
   xdg = {
     portal = {
