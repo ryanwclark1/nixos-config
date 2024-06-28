c1639d40aa5e8b9e302f9c338b849c61645679f6
Ryan Clark <ryanwclark@yahoo.com>
2024-06-28 10:02:26 -0500

    Update flameshot

--- home/features/productivity/flameshot.nix
+++ home/features/productivity/flameshot.nix
@@ -1,10 +1,26 @@
 {
-  pkgs,
   ...
 }:
 
 {
-  home.packages = with pkgs; [
-    flameshot
-  ];
+  services.flameshot = {
+    enable = true;
+
+    settings = {
+      General = {
+        drawColor = "#ff0000";
+        drawFontSize = 23;
+        drawThickness = 3;
+
+        savePath = "/home/administrator/Downloads";
+        savePathFixed = false;
+
+        disabledTrayIcon = true;
+        showDesktopNotification = false;
+        showHelp = false;
+
+        uiColor = "#ffffff";
+      };
+    };
+  };
 }
