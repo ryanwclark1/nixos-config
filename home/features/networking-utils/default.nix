5ad9975a22dad41667826580d9fbc406c0f1dfca
Ryan Clark <ryanwclark@yahoo.com>
2024-06-28 11:01:27 -0500

    remove some unused tools

--- home/features/networking-utils/default.nix
+++ home/features/networking-utils/default.nix
@@ -5,11 +5,7 @@
 
 {
   home.packages = with pkgs; [
-    aria2 # A lightweight multi-protocol & multi-source command-line download utility
     dnsutils # `dig` + `nslookup`
-    ipcalc # it is a calculator for the IPv4/v6 addresses
-    ldns # replacement of `dig`, it provide the command `drill`
-    libpcap
     ngrep
     nmap # A utility for network discovery and security auditing
     openresolv # a resolv.conf management framework
