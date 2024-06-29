0cc8377731d98478fa258607d5abcad17765145d
Ryan Clark <ryanc@accentservices.com>
2024-06-28 22:39:25 -0500

    zsh update

--- home/features/shell/zsh.nix
+++ home/features/shell/zsh.nix
@@ -9,32 +9,40 @@ in
 {
   programs.zsh = {
     enable = true;
-    autosuggestion.enable = true;
+    enableBashCompletion = true;
     enableCompletion = true;
-    syntaxHighlighting.enable = true;
     enableVteIntegration = true;
-    localVariables = {
-      DISABLE_CORRECTION = true;
+    histSize = 25000;
+    syntaxHighlighting = {
+      enable = true;
     };
-    autocd = true;
-    initExtra = ''
-      # eval "$(zellij setup --generate-auto-start zsh)"
-      # if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
-      			# exec tmux
-      # fi
+    autosuggestion = {
+      enable = true;
+      async = true;
 
-      # export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS"
-      # --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
-      # --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
-      # --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
-    '';
-    dotDir = ".config/zsh";
-    completionInit = "autoload -U compinit && compinit";
-    history.expireDuplicatesFirst = true;
-    history.extended = true;
-    history.ignoreDups = true;
-    history.save = 3000000;
-    history.size = 30000000;
+    };
+    # localVariables = {
+    #   DISABLE_CORRECTION = true;
+    # };
+    # autocd = true;
+    # initExtra = ''
+    #   # eval "$(zellij setup --generate-auto-start zsh)"
+    #   # if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
+    #   			# exec tmux
+    #   # fi
+
+    #   # export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS"
+    #   # --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
+    #   # --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
+    #   # --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796"
+    # '';
+    # dotDir = ".config/zsh";
+    # completionInit = "autoload -U compinit && compinit";
+    # history.expireDuplicatesFirst = true;
+    # history.extended = true;
+    # history.ignoreDups = true;
+    # history.save = 3000000;
+    # history.size = 30000000;
   };
   programs.atuin.enableZshIntegration = mkIf config.programs.atuin.enable true;
   programs.eza.enableZshIntegration = mkIf config.programs.eza.enable true;
