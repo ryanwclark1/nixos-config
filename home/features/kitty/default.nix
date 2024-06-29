ea9d4021ecfc790ee47dfb81588041c4e80e54cc
Ryan Clark <ryanc@accentservices.com>
2024-06-28 22:39:42 -0500

    kitty update clipboard and scroll size

--- home/features/kitty/default.nix
+++ home/features/kitty/default.nix
@@ -20,10 +20,11 @@ in
     enable = true;
     package = pkgs.kitty;
     settings = {
-      # shell_integration = "no-rc"; # I prefer to do it manually
-      # performance
-      scrollback_lines = 15000;
+      scrollback_lines = 25000;
       scrollback_pager_history_size = 2048;
+
+      copy_on_select = "clipboard";
+
       wheel_scroll_min_lines = 1;
       window_padding_width = 15;
       update_check_interval = 0;
@@ -33,6 +34,7 @@ in
 
       # changing default behaviors
       confirm_os_window_close = 0;
+
       enable_audio_bell = false;
       dynamic_background_opacity = true;
       allow_remote_control = true;
