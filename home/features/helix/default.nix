dfc721e36db5f47c92ee602e51e01fb0e0fceb82
Ryan Clark <ryanc@accentservices.com>
2024-06-28 21:44:17 -0500

    Update Helix

--- home/features/helix/default.nix
+++ home/features/helix/default.nix
@@ -1,13 +1,38 @@
 {
+  inputs,
+  pkgs,
   ...
 }:
 
 {
+  imports = [./languages.nix];
+
   programs.helix = {
     enable = true;
+    package = inputs.helix.packages.${pkgs.system}.default.overrideAttrs (old: {
+      makeWrapperArgs = with pkgs;
+        old.makeWrapperArgs
+        or []
+        ++ [
+          "--suffix"
+          "PATH"
+          ":"
+          (lib.makeBinPath [
+            clang-tools
+            marksman
+            nil
+            bash-language-server
+            nodePackages.vscode-css-languageserver-bin
+            nodePackages.vscode-langservers-extracted
+            shellcheck
+          ])
+        ];
+    });
+
     settings = {
       editor = {
         color-modes = true;
+        cursorline = true;
         line-number = "relative";
         indent-guides.render = true;
         cursor-shape = {
@@ -15,6 +40,18 @@
           insert = "bar";
           select = "underline";
         };
+        lsp.display-inlay-hints = true;
+        statusline.center = ["position-percentage"];
+        true-color = true;
+        whitespace.characters = {
+          newline = "↴";
+          tab = "⇥";
+        };
+      };
+      keys.normal.space.u = {
+        f = ":format"; # format using LSP formatter
+        w = ":set whitespace.render all";
+        W = ":set whitespace.render none";
       };
     };
   };
---
+++ home/features/helix/languages.nix
@@ -0,0 +1,156 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  programs.helix.languages = {
+    language = let
+      deno = lang: {
+        command = lib.getExe pkgs.deno;
+        args = ["fmt" "-" "--ext" lang];
+      };
+
+      prettier = lang: {
+        command = lib.getExe pkgs.nodePackages.prettier;
+        args = ["--parser" lang];
+      };
+      prettierLangs = map (e: {
+        name = e;
+        formatter = prettier e;
+      });
+      langs = ["css" "scss" "html"];
+    in
+      [
+        {
+          name = "bash";
+          auto-format = true;
+          formatter = {
+            command = lib.getExe pkgs.shfmt;
+            args = ["-i" "2"];
+          };
+        }
+        {
+          name = "clojure";
+          injection-regex = "(clojure|clj|edn|boot|yuck)";
+          file-types = ["clj" "cljs" "cljc" "clje" "cljr" "cljx" "edn" "boot" "yuck"];
+        }
+        {
+          name = "cmake";
+          auto-format = true;
+          language-servers = ["cmake-language-server"];
+          formatter = {
+            command = lib.getExe pkgs.cmake-format;
+            args = ["-"];
+          };
+        }
+        {
+          name = "javascript";
+          auto-format = true;
+          language-servers = ["dprint" "typescript-language-server"];
+        }
+        {
+          name = "json";
+          formatter = deno "json";
+        }
+        {
+          name = "markdown";
+          auto-format = true;
+          formatter = deno "md";
+        }
+        {
+          name = "typescript";
+          auto-format = true;
+          language-servers = ["dprint" "typescript-language-server"];
+        }
+      ]
+      ++ prettierLangs langs;
+
+    language-server = {
+      bash-language-server = {
+        command = lib.getExe pkgs.bash-language-server;
+        args = ["start"];
+      };
+
+      clangd = {
+        command = "${pkgs.clang-tools}/bin/clangd";
+        clangd.fallbackFlags = ["-std=c++2b"];
+      };
+
+      cmake-language-server = {
+        command = lib.getExe pkgs.cmake-language-server;
+      };
+
+      deno-lsp = {
+        command = lib.getExe pkgs.deno;
+        args = ["lsp"];
+        environment.NO_COLOR = "1";
+        config.deno = {
+          enable = true;
+          lint = true;
+          unstable = true;
+          suggest = {
+            completeFunctionCalls = false;
+            imports = {hosts."https://deno.land" = true;};
+          };
+          inlayHints = {
+            enumMemberValues.enabled = true;
+            functionLikeReturnTypes.enabled = true;
+            parameterNames.enabled = "all";
+            parameterTypes.enabled = true;
+            propertyDeclarationTypes.enabled = true;
+            variableTypes.enabled = true;
+          };
+        };
+      };
+
+      dprint = {
+        command = lib.getExe pkgs.dprint;
+        args = ["lsp"];
+      };
+
+      nil = {
+        command = lib.getExe pkgs.nil;
+        config.nil.formatting.command = ["${lib.getExe pkgs.alejandra}" "-q"];
+      };
+
+      typescript-language-server = {
+        command = lib.getExe pkgs.nodePackages.typescript-language-server;
+        args = ["--stdio"];
+        config = let
+          inlayHints = {
+            includeInlayEnumMemberValueHints = true;
+            includeInlayFunctionLikeReturnTypeHints = true;
+            includeInlayFunctionParameterTypeHints = true;
+            includeInlayParameterNameHints = "all";
+            includeInlayParameterNameHintsWhenArgumentMatchesName = true;
+            includeInlayPropertyDeclarationTypeHints = true;
+            includeInlayVariableTypeHints = true;
+          };
+        in {
+          typescript-language-server.source = {
+            addMissingImports.ts = true;
+            fixAll.ts = true;
+            organizeImports.ts = true;
+            removeUnusedImports.ts = true;
+            sortImports.ts = true;
+          };
+
+          typescript = {inherit inlayHints;};
+          javascript = {inherit inlayHints;};
+
+          hostInfo = "helix";
+        };
+      };
+
+      vscode-css-language-server = {
+        command = lib.getExe pkgs.nodePackages.vscode-css-languageserver-bin;
+        args = ["--stdio"];
+        config = {
+          provideFormatter = true;
+          css.validate.enable = true;
+          scss.validate.enable = true;
+        };
+      };
+    };
+  };
+}
