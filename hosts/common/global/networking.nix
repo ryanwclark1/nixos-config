614955a9af64594bbdac7873d23109a7b1383e82
Ryan Clark <ryanc@accentservices.com>
2024-07-12 17:42:29 -0500

    feat: Add DNS configuration for techcasa.io domain
    This commit adds DNS configuration for the `techcasa.io` domain in the `networking.nix` file. It sets the domain and fully qualified domain name (FQDN) based on the `config.networking.hostName` and `config.networking.domain` values. Additionally, it updates the search list to include the `techcasa.io` domain.

--- hosts/common/global/networking.nix
+++ hosts/common/global/networking.nix
@@ -1,13 +1,17 @@
 {
+  config,
   ...
 }:
-
+# let
+#   inherit (config.networking) hostName;
+# in
 {
   networking = {
     networkmanager = {
       enable = true;
       dns = "default";
       appendNameservers = [
+        "100.100.100.100"
         "10.10.100.1"
         "1.1.1.1"
         "1.0.0.1"
@@ -17,6 +21,10 @@
     firewall = {
       enable = true;
     };
-
+    domain = "techcasa.io";
+    fqdn = "${config.networking.hostName}.${config.networking.domain}";
+    search = [
+      "${config.networking.domain}"
+    ];
   };
 }
