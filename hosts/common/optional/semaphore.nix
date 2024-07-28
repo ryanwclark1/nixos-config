99ab7598af30b4313587d3fd9bf4383ed0470ce9
Ryan Clark <ryanc@accentservices.com>
2024-07-28 12:31:49 -0500

    chore: Remove semaphore feature and update frametop configuration

--- hosts/common/optional/semaphore.nix
+++ hosts/common/optional/semaphore.nix
@@ -4,6 +4,14 @@
 }:
 
 {
+  environment.systemPackages = [
+    pkgs.arion
+
+     # Do install the docker CLI to talk to podman.
+     # Not needed when virtualisation.docker.enable = true;
+    pkgs.docker-client
+  ];
+  
   virtualisation.arion = {
     backend = "docker";
     projects = {
