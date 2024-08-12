a8a537387831b0039ef1ecba1368820cd62f6715
Ryan Clark <ryanc@accentservices.com>
2024-08-12 11:39:54 -0500

    Updated github and extenstions

--- home/features/cli/gh.nix
+++ home/features/cli/gh.nix
@@ -6,12 +6,22 @@
 {
   programs.gh = {
     enable = true;
-    gitCredentialHelper.enable = true;
+    package = pkgs.gh;
+    gitCredentialHelper = {
+      enable = true;
+    };
+    settings = {
+      aliases = {
+         co = "pr checkout";
+        pv = "pr view";
+      };
+      git_protocal = "https";
+    };
     extensions = with pkgs; [
       bump #  CLI tool to draft a GitHub Release for the next semantic version
-      gh-markdown-preview
-      gh-dash
-      gh-cal
+      gh-markdown-preview # GitHub CLI extension to preview Markdown looks like GitHub.
+      gh-dash # A beautiful CLI dashboard for GitHub 🚀
+      gh-copilot # Ask for assistance right in your terminal
     ];
   };
 
