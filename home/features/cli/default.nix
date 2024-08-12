bed08d71e8401b4dd5eb4a8ab27d1e3f6c5ae97c
Ryan Clark <ryanc@accentservices.com>
2024-08-12 12:00:59 -0500

    Updated cli tools

--- home/features/cli/default.nix
+++ home/features/cli/default.nix
@@ -9,32 +9,29 @@
     ./aichat.nix
     ./bat.nix
     ./btop.nix
+    ./diffsitter.nix
     ./direnv.nix
     ./exiftool.nix
     ./filesystem_utils.nix
+    ./fx.nix
     ./gh.nix
     ./gpg.nix
+    ./gum.nix
     ./jq.nix
     ./neomutt.nix
     ./network.nix
     ./nixtools.nix
     # ./ssh.nix
+    ./system.nix
     ./viu.nix
+    ./yq.nix
+    ./zk.nix
   ];
   home.packages = with pkgs; [
-    diffsitter # Better diff
     distrobox # Nice escape hatch, integrates docker images with my environment
-    fx # Terminal JSON viewer
-    gum # shell scripts
     httpie # Better curl
     # hurl # httpie/curl alternative
     hyperfine #cli benchmarking tool
-    mprocs # multiple commands in parallel
-    pciutils # lspci
     scrot # A command-line screen capture utility
-    trashy # cli rm with trash support
-    usbutils # lsusb
-    yq-go #jq for yaml https://github.com/mikefarah/yq
-    zk # note taking
   ];
 }
---
+++ home/features/cli/zk.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  programs = {
+    zk = {
+      enable = true;
+      package = pkgs.zk;
+      settings = {
+         note = {
+          language = "en";
+          default-title = "Untitled";
+          filename = "{{id}}-{{slug title}}";
+          extension = "md";
+          template = "default.md";
+          id-charset = "alphanum";
+          id-length = 4;
+          id-case = "lower";
+        };
+        extra = {
+          author = "Ryan";
+        };
+      };
+    };
+  };
+}
