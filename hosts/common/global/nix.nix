c1b75642dd383952f463a94653f02277bcbc1a53
Ryan Clark <ryanwclark@yahoo.com>
2024-08-14 10:05:37 -0500

    Updated nix settings

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -7,17 +7,21 @@
 {
   nix = {
     package = lib.mkDefault pkgs.nix;
+
     settings = {
       auto-optimise-store = lib.mkDefault true;
-      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "repl-flake"
+      ];
       warn-dirty = false;
     };
+
     gc = {
       automatic = true;
-      dates = "weekly";
       # Keep the last 5 generations
       options = "--delete-older-than +5";
     };
-
   };
 }
