2b1d4367948ea98df25cfa1025af60f5b925f957
Ryan Clark <ryanwclark@yahoo.com>
2024-08-15 12:15:12 -0500

    Update home global with persistence

--- home/global/default.nix
+++ home/global/default.nix
@@ -19,19 +19,33 @@ let
 in
 {
   imports = [
-    inputs.nix-colors.homeManagerModule
+    inputs.impermanence.nixosModules.home-manager.impermanence
+    # inputs.nix-colors.homeManagerModule
     ./global-fonts.nix
     ./style.nix
-  ] ++ (builtins.attrValues outputs.homeManagerModules);
+  ]
+  ++ (builtins.attrValues outputs.homeManagerModules);
 
-  nixpkgs = {
-    overlays = builtins.attrValues outputs.overlays;
-    config = {
-      allowUnfree = true;
-      allowUnfreePredicate = (_: true);
+  nix = {
+    package = lib.mkDefault pkgs.nix;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      warn-dirty = false;
     };
   };
 
+  # nixpkgs = {
+  #   overlays = builtins.attrValues outputs.overlays;
+  #   config = {
+  #     allowUnfree = true;
+  #     allowUnfreePredicate = (_: true);
+  #   };
+  # };
+
+  systemd.user.startServices = "sd-switch";
 
   programs = {
     home-manager.enable = true;
@@ -72,55 +86,25 @@ in
     };
   };
 
+
+  persistence = {
+    "/persist${config.home.homeDirectory}" = {
+      defaultDirectoryMethod = "symlink";
+      directories = [
+        "documents"
+        "downloads"
+        "pictures"
+        "videos"
+        ".local/bin"
+        ".local/share/nix" # trusted settings and repl history
+      ];
+      allowOther = true;
+    };
+  };
+
   # home.file = {
   #   ".colorscheme".text = config.colorscheme.slug;
   #   ".colorscheme.json".text = builtins.toJSON config.colorscheme;
   # };
 
-  home.packages =
-    let
-      specialisation = pkgs.writeShellScriptBin "specialisation" ''
-        profiles="$HOME/.local/state/nix/profiles"
-        current="$profiles/home-manager"
-        base="$profiles/home-manager-base"
-
-        # If current contains specialisations, link it as base
-        if [ -d "$current/specialisation" ]; then
-          echo >&2 "Using current profile as base"
-          ln -sfT "$(readlink "$current")" "$base"
-        # Check that $base contains specialisations before proceeding
-        elif [ -d "$base/specialisation" ]; then
-          echo >&2 "Using previously linked base profile"
-        else
-          echo >&2 "No suitable base config found. Try 'home-manager switch' again."
-          exit 1
-        fi
-
-        if [ "$1" = "list" ] || [ "$1" = "-l" ] || [ "$1" = "--list" ]; then
-          find "$base/specialisation" -type l -printf "%f\n"
-          exit 0
-        fi
-
-        echo >&2 "Switching to ''${1:-base} specialisation"
-        if [ -n "$1" ]; then
-          "$base/specialisation/$1/activate"
-        else
-          "$base/activate"
-        fi
-      '';
-      toggle-theme = pkgs.writeShellScriptBin "toggle-theme" ''
-        if [ -n "$1" ]; then
-          theme="$1"
-        else
-          current="$(${lib.getExe pkgs.jq} -re '.variant' "$HOME/.colorscheme.json")"
-          if [ "$current" = "light" ]; then
-            theme="dark"
-          else
-            theme="light"
-          fi
-        fi
-        ${lib.getExe specialisation} "$theme"
-      '';
-    in
-    [ specialisation toggle-theme ];
 }
