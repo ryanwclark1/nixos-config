4665dfb21d2c74515de548613e01e95d33fe66e7
Ryan Clark <ryanwclark@yahoo.com>
2024-08-14 20:01:32 -0500

    Add mako for alerts

---
+++ home/features/desktop/hyprland/mako.nix
@@ -0,0 +1,18 @@
+{
+  ...
+}:
+
+{
+  services.mako = {
+    enable = true;
+    actions = true;
+    padding = "10,20";
+    anchor = "top-center";
+    width = 400;
+    height = 150;
+    borderSize = 2;
+    defaultTimeout = 12000;
+    layer = "overlay";
+    sort = "-time";
+  };
+}
