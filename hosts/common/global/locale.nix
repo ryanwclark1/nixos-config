bdd1be5d036b937017587c3db75f8a53b662adff
Ryan Clark <ryanwclark@yahoo.com>
2024-08-15 10:01:40 -0500

    update locale

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -6,6 +6,9 @@
 {
   i18n = {
     defaultLocale = lib.mkDefault "en_US.UTF-8";
+    supportedLocales = lib.mkDefault [
+      "en_US.UTF-8/UTF-8"
+    ];
   };
   services.geoclue2 = {
     enable = true;
