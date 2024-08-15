e7f84b63bbed2c77e35e44c56de7d22fc6b3ada6
Ryan Clark <ryanwclark@yahoo.com>
2024-08-15 09:44:57 -0500

    Add file open limits and qt6 fix

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -20,9 +20,10 @@
     # ./ssh-serve-store.nix
   ] ++ (builtins.attrValues outputs.nixosModules);
 
+  home-manager.useGlobalPkgs = true;
   home-manager.extraSpecialArgs = { inherit inputs outputs; };
 
-# allowUnfree isn't being inherited from the global flake
+  # allowUnfree isn't being inherited from the global flake
   nixpkgs = {
     overlays = builtins.attrValues outputs.overlays;
     config = {
@@ -30,5 +31,26 @@
     };
   };
 
+  environment.profileRelativeSessionVariables = {
+    QT_PLUGIN_PATH = ["/lib/qt-6/plugins"];
+  };
 
+  hardware.enableRedistributableFirmware = true;
+  networking.domain = "techcasa.io";
+
+  # Increase open file limit for sudoers
+  security.pam.loginLimits = [
+    {
+      domain = "@wheel";
+      item = "nofile";
+      type = "soft";
+      value = "524288";
+    }
+    {
+      domain = "@wheel";
+      item = "nofile";
+      type = "hard";
+      value = "1048576";
+    }
+  ];
 }
