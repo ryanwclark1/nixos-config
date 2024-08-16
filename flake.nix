a98fd9d8a03f00f767d35af8958e3b0dde874621
Ryan Clark <ryanc@accentservices.com>
2024-08-16 18:42:26 -0500

    flake structure updated

--- flake.nix
+++ flake.nix
@@ -2,18 +2,24 @@
   description = "Nixos config flake";
 
   inputs = {
+    #################### Official NixOS and HM Package Sources ####################
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+
     nixos-hardware.url = "github:nixos/nixos-hardware";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+
+    #################### Utilities ####################
+
     nix-colors.url = "github:misterio77/nix-colors";
     systems.url = "github:nix-systems/default-linux";
     impermanence.url = "github:nix-community/impermanence";
     disko = {
       url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
