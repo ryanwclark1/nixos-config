44a8865239a0958004a6b76ad4a237964e2876cb
Ryan Clark <ryanc@accentservices.com>
2024-08-18 03:29:38 -0500

    reconfig files

--- home/features/desktop/hyprland/default.nix
+++ home/features/desktop/hyprland/default.nix
@@ -11,23 +11,44 @@
     ./hyprland.nix
     ./hyprlock.nix
     ./mako.nix
-    ./rofi.nix
-    ./swayidle.nix
-    ./swaylock.nix
+    # ./rofi.nix
+    # ./swayidle.nix
+    # ./swaylock.nix
     ./waybar.nix
     ./wofi.nix
     ./variables.nix
   ];
 
-  home.packages = with pkgs; [
-    wf-recorder
-    wl-clipboard
-    swaybg
-    # inputs.hypr-contrib.packages.${pkgs.system}.grimblast
-    hyprpicker
-    grim
-    slurp
-    wl-clip-persist
-    wf-recorder
-  ];
+  home = {
+    file.".config/pipewire/pipewire.conf".source = ./config/pipewire/pipewire.conf;
+    file.".emoji".source = ./config/emoji;
+    file."Pictures/wallpapers" = {
+      source = ../../../../hosts/common/wallpaper;
+      recursive = true;
+    };
+    # file.".config/rofi" = {
+    #   source = ./config/rofi;
+    #   recursive = true;
+    # };
+    # file.".config/swaync" = {
+    #   source = ./config/swaync;
+    #   recursive = true;
+    # };
+    # file.".config/hypr" = {
+    #   source = ./config/hyprland;
+    #   recursive = true;
+    # };
+
+    packages = with pkgs; [
+      wf-recorder
+      wl-clipboard
+      swaybg
+      # inputs.hypr-contrib.packages.${pkgs.system}.grimblast
+      hyprpicker
+      grim
+      slurp
+      wl-clip-persist
+      wf-recorder
+    ];
+  };
 }
