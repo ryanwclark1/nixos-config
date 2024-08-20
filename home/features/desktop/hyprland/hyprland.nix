73e3149df1625eed4bf74d653eb6fb69f29fbcf9
Ryan Clark <ryanc@accentservices.com>
2024-08-20 13:45:21 -0500

    wofi config

--- home/features/desktop/hyprland/hyprland.nix
+++ home/features/desktop/hyprland/hyprland.nix
@@ -434,7 +434,7 @@ in {
             wofi = lib.getExe config.programs.wofi.package;
           in
             lib.optionals config.programs.wofi.enable [
-              "SUPER,x,exec,${wofi} -S drun -x 10 -y 10 -W 25% -H 60%"
+              "SUPER,x,exec,${wofi} -S drun" # -x 10 -y 10 -W 25% -H 60%
               "SUPER,s,exec,specialisation $(specialisation | ${wofi} -S dmenu)"
               "SUPER,d,exec,${wofi} -S run"
 
