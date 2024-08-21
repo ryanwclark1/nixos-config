ac073695037abb50e3eed5aadcb01724f9c2923d
Ryan Clark <ryanc@accentservices.com>
2024-08-20 23:15:10 -0500

    Update to python 312

--- home/features/development/python.nix
+++ home/features/development/python.nix
@@ -6,11 +6,11 @@
 {
   home.packages = with pkgs; [
 
-    python311
-    python311Packages.poetry-core
-    python311Packages.pdm-backend
-    python311Packages.pip
-    # python311Packages.pipx
+    python312
+    python312Packages.poetry-core
+    python312Packages.pdm-backend
+    python312Packages.pip
+    # python312Packages.pipx
     # poetry
     functiontrace-server
     # memray
