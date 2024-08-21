1e757715b9f9e1359539e53caa707e907f47b339
Ryan Clark <ryanc@accentservices.com>
2024-08-21 11:40:15 -0500

    nixvim conversion

---
+++ home/features/nvim/plugins/alpha.nix
@@ -0,0 +1,114 @@
+{
+  ...
+}:
+
+{
+  programs.nixvim.plugins = {
+    alpha = {
+      enable = true;
+      iconsEnabled = true;
+      layout = [
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          opts = {
+            hl = "Type";
+            position = "center";
+          };
+          type = "text";
+          val = [
+              "                                                     "
+              "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ "
+              "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ "
+              "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ "
+              "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ "
+              "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ "
+              "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ "
+              "                                                     "
+          ];
+        }
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          type = "group";
+          val = [
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[enew]] end";
+              };
+              opts = {
+                shortcut = "n";
+              };
+              type = "button";
+              val = "󰈔 New file";
+            }
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[Explore]] end";
+              };
+              opts = {
+                shortcut = "e";
+              };
+              type = "button";
+              val = " Explore";
+            }
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[Git | only]] end";
+              };
+              opts = {
+                shortcut = "g";
+              };
+              type = "button";
+              val = " Git summary";
+            }
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[e ~/Documents/Notes/scratch.md]] end";
+              };
+              opts = {
+                shortcut = "s";
+              };
+              type = "button";
+              val = " Scratchpad";
+            }
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[e ~/nix-config/flake.nix]] end";
+              };
+              opts = {
+                shortcut = "c";
+              };
+              type = "button";
+              val = "  Nix config flake";
+            }
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[qa]] end";
+              };
+              opts = {
+                shortcut = "q";
+              };
+              type = "button";
+              val = "󰅙  Quit nvim";
+            }
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[checkhealth]] end";
+              };
+              opts = {
+                shortcut = "c";
+              };
+              type = "button";
+              val = " Check health";
+            }
+          ];
+        }
+      ];
+    };
+  };
+}
