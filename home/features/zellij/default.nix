d5cdfe40ec826f96cd3123989b47a8a2af2ca1d4
Ryan Clark <ryanc@accentservices.com>
2024-08-22 13:51:03 -0500

    zellij updates on configs

--- home/features/zellij/default.nix
+++ home/features/zellij/default.nix
@@ -10,28 +10,41 @@
 {
   programs.zellij =
   let
-    fish = lib.getExe pkgs.fish;
+    # fish = lib.getExe pkgs.fish;
+    zsh = lib.getExe pkgs.zsh;
     wl-copy = "${pkgs.wl-clipboard}/bin/wl-copy";
   in
   {
     enable = true;
     package = pkgs.zellij;
-    settings = {
-      on_force_close = "quit";
+    # https://zellij.dev/documentation/options
+    settings =
+    {
+      on_force_close = "detach";
       simplified_ui = false;
-      default_shell = fish;
-      pane_frames = true;
+      default_shell = zsh;
       default_layout = "default";
+      default_mode = "normal";
       mouse_mode = false;
-      copy_command = wl-copy;
-      copy_clipboard = "primary";
-      copy_on_select = true;
       scroll_buffer_size = 25000;
+      copy_command = wl-copy;
+      copy_clipboard = "system";
+      copy_on_select = true;
       scrollback_editor = "$EDITOR";
+      mirror_session = true;
+      pane_frames = true;
+      ui = {
+        pane_frame = {
+          rounded_corners = true;
+          hide_session_name = false;
+        };
+      };
+      session_serialization = true;
+      pane_viewport_serialization = false;
+      # Serializes all scroll back to limit
+      scrollback_lines_to_serialize = 0;
+      disable_session_metadata = false;
     };
-    enableBashIntegration = lib.mkIf config.programs.bash.enable true;
-    enableFishIntegration = lib.mkIf config.programs.fish.enable true;
-    enableZshIntegration = lib.mkIf config.programs.zsh.enable true;
   };
 
   home.shellAliases = {
