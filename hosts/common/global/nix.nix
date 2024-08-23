d592e441d44ce3b368c40503b26429854eb4b702
Ryan Clark <ryanc@accentservices.com>
2024-08-23 16:36:32 -0500

    Connection timeout 40

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -10,7 +10,7 @@
     settings = {
       auto-optimise-store = true;
       # See https://jackson.dev/post/nix-reasonable-defaults/
-      connect-timeout = 5;
+      connect-timeout = 40;
       log-lines = 25;
       min-free = 128000000; # 128MB
       max-free = 1000000000; # 1GB
