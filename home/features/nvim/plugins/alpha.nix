47fe945a6362a5e262e011258137f77364d16040
Ryan Clark <ryanc@accentservices.com>
2024-08-23 21:24:47 -0500

    Alpha updates

--- home/features/nvim/plugins/alpha.nix
+++ home/features/nvim/plugins/alpha.nix
@@ -8,130 +8,130 @@
       enable = true;
       iconsEnabled = true;
       theme = "dashboard";
-      # layout = [
-      #   {
-      #     type = "padding";
-      #     val = 2;
-      #   }
-      #   {
-      #     opts = {
-      #       hl = "Type";
-      #       position = "center";
-      #     };
-      #     type = "text";
-      #     val = [
-      #         "                                                     "
-      #         "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ "
-      #         "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ "
-      #         "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ "
-      #         "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ "
-      #         "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ "
-      #         "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ "
-      #         "                                                     "
-      #     ];
-      #   }
-      #   {
-      #     type = "padding";
-      #     val = 2;
-      #   }
-      #   {
-      #     type = "group";
-      #     opts = {
-      #       position = "center";
-      #     };
-      #     val = [
-      #       {
-      #         on_press = {
-      #           __raw = "function() vim.cmd[[enew]] end";
-      #         };
-      #         opts = {
-      #           position = "left";
-      #           shortcut = "n";
-      #         };
-      #         type = "button";
-      #         val = " 󰈔 New file";
-      #       }
-      #       {
-      #         on_press = {
-      #           __raw = "function() vim.cmd[[Explore]] end";
-      #         };
-      #         opts = {
-      #           position = "center";
-      #           shortcut = "e";
-      #         };
-      #         type = "button";
-      #         val = "  Explore        ";
-      #       }
-      #       {
-      #         on_press = {
-      #           __raw = "function() vim.cmd[[Git | only]] end";
-      #         };
-      #         opts = {
-      #           position = "center";
-      #           shortcut = "g";
-      #         };
-      #         type = "button";
-      #         val = "  Git Summary     ";
-      #       }
-      #       {
-      #         on_press = {
-      #           __raw = "function() vim.cmd[[e ~/Documents/Notes/scratch.md]] end";
-      #         };
-      #         opts = {
-      #           position = "center";
-      #           shortcut = "s";
-      #         };
-      #         type = "button";
-      #         val = "  Scratchpad       ";
-      #       }
-      #       {
-      #         on_press = {
-      #           __raw = "function() vim.cmd[[e ~/nix-config/flake.nix]] end";
-      #         };
-      #         opts = {
-      #           position = "center";
-      #           shortcut = "c";
-      #         };
-      #         type = "button";
-      #         val = "   Nix Config Flake";
-      #       }
-      #       {
-      #         on_press = {
-      #           __raw = "function() vim.cmd[[checkhealth]] end";
-      #         };
-      #         opts = {
-      #           position = "center";
-      #           shortcut = "c";
-      #         };
-      #         type = "button";
-      #         val = "   Check health    ";
-      #       }
-      #       {
-      #         on_press = {
-      #           __raw = "function() vim.cmd[[qa]] end";
-      #         };
-      #         opts = {
-      #           position = "center";
-      #           shortcut = "q";
-      #         };
-      #         type = "button";
-      #         val = " 󰅙  Quit nvim       ";
-      #       }
-      #     ];
-      #   }
-      #   {
-      #     type = "padding";
-      #     val = 2;
-      #   }
-      #   {
-      #     opts = {
-      #       hl = "Keyword";
-      #       position = "center";
-      #     };
-      #     type = "text";
-      #     val = "Inspiring quote here.";
-      #   }
-      # ];
+      layout = [
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          opts = {
+            hl = "Type";
+            position = "center";
+          };
+          type = "text";
+          val = [
+              "                                                     "
+              "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ "
+              "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ "
+              "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ "
+              "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ "
+              "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ "
+              "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ "
+              "                                                     "
+          ];
+        }
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          type = "group";
+          opts = {
+            position = "center";
+          };
+          val = [
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[enew]] end";
+              };
+              opts = {
+                position = "left";
+                shortcut = "n";
+              };
+              type = "button";
+              val = " 󰈔 New file";
+            }
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[Explore]] end";
+              };
+              opts = {
+                position = "center";
+                shortcut = "e";
+              };
+              type = "button";
+              val = "  Explore        ";
+            }
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[Git | only]] end";
+              };
+              opts = {
+                position = "center";
+                shortcut = "g";
+              };
+              type = "button";
+              val = "  Git Summary     ";
+            }
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[e ~/Documents/Notes/scratch.md]] end";
+              };
+              opts = {
+                position = "center";
+                shortcut = "s";
+              };
+              type = "button";
+              val = "  Scratchpad       ";
+            }
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[e ~/nix-config/flake.nix]] end";
+              };
+              opts = {
+                position = "center";
+                shortcut = "c";
+              };
+              type = "button";
+              val = "   Nix Config Flake";
+            }
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[checkhealth]] end";
+              };
+              opts = {
+                position = "center";
+                shortcut = "c";
+              };
+              type = "button";
+              val = "   Check health    ";
+            }
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[qa]] end";
+              };
+              opts = {
+                position = "center";
+                shortcut = "q";
+              };
+              type = "button";
+              val = " 󰅙  Quit nvim       ";
+            }
+          ];
+        }
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          opts = {
+            hl = "Keyword";
+            position = "center";
+          };
+          type = "text";
+          val = "Inspiring quote here.";
+        }
+      ];
     };
   };
 }
