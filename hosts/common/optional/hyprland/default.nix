f12cc0b1429c6b1fd049c27fca3423d672f3f18c
Ryan Clark <ryanc@accentservices.com>
2024-08-26 20:13:16 -0500

    chore: Update hyprland default.nix
    Improve hyprland configuration by removing commented out waybar.nix and adding eww script to the default.nix file.

--- hosts/common/optional/hyprland/default.nix
+++ hosts/common/optional/hyprland/default.nix
@@ -6,23 +6,27 @@
 }:
 
 {
-  services = {
-    xserver = {
-      windowManager = {
-        hypr = {
-          enable = true;
-        };
-      };
-    };
-  };
+  # services = {
+  #   xserver = {
+  #     windowManager = {
+  #       hypr = {
+  #         enable = true;
+  #       };
+  #     };
+  #   };
+  # };
 
   programs = {
     hyprland = {
-      systemd.setPath.enable = false;
-      xwayland.enable = true;
       enable = true;
-      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-      portalPackage = pkgs.xdg-desktop-portal-hyprland;
+      package = (pkgs.hyprland.override { # or inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland
+        enableXWayland = true;  # whether to enable XWayland
+        legacyRenderer = false; # whether to use the legacy renderer (for old GPUs)
+        withSystemd = true;     # whether to build with systemd support
+      });
+      # systemd.setPath.enable = true;
+      # xwayland.enable = true;
+      # portalPackage = pkgs.xdg-desktop-portal-hyprland;
     };
     mtr.enable = true;
     gnupg.agent = {
