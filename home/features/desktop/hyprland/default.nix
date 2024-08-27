ef8c62870ce0f045825a5871dc94f82a56e41101
Ryan Clark <ryanc@accentservices.com>
2024-08-27 12:48:56 -0500

    feat: Enable hyprbars plugin in hyprland default.nix
    This commit enables the hyprbars plugin in the hyprland default.nix file. The plugin is added to the list of plugins under the wayland.windowManager.hyprland configuration. This change enhances the functionality of the hyprland desktop by adding support for the hyprbars plugin.

--- home/features/desktop/hyprland/default.nix
+++ home/features/desktop/hyprland/default.nix
@@ -7,14 +7,12 @@
   imports = [
     ./basic-binds.nix
     ./cliphist.nix
-    # ./hyprbars.nix
+    ./hyprbars.nix
     ./hyprland.nix
     ./hyprlock
     ./hypridle
     ./mako.nix
     ./rofi.nix
-    # ./swayidle.nix
-    # ./swaylock.nix
     ./waybar.nix
     # ./eww
     # ./wofi.nix
@@ -22,8 +20,6 @@
   ];
 
   home = {
-    # file.".config/pipewire/pipewire.conf".source = ./config/pipewire/pipewire.conf;
-    # file.".emoji".source = ./config/emoji;
     file."Pictures/wallpapers" = {
       source = ../../../../hosts/common/wallpaper;
       recursive = true;
@@ -32,13 +28,8 @@
     packages = with pkgs; [
       wf-recorder
       wl-clipboard
-
-      # swaybg
-      # inputs.hypr-contrib.packages.${pkgs.system}.grimblast
-      # hyprpicker
-      # grim
-      # slurp
-      # wl-clip-persist
+      grim
+      slurp
     ];
   };
 }
