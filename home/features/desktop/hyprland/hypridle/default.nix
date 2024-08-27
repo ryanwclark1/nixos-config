cf5ae7f5e0fd5ec664b8b079cd453f268650f6f0
Ryan Clark <ryanc@accentservices.com>
2024-08-27 11:38:45 -0500

    feat: Add hypridle program configuration to default.nix
    This commit adds the configuration for the hypridle program to the default.nix file. The hypridle program is enabled with the "enable" flag set to true, and the package is set to pkgs.hypridle. Additionally, the settings for the program are defined, including the general settings and the listener settings. This change enhances the functionality of the hyprland desktop by adding support for the hypridle program.

---
+++ home/features/desktop/hyprland/hypridle/default.nix
@@ -0,0 +1,39 @@
+{ pkgs, ... }:
+
+{
+  programs = {
+    hypridle = {
+      enable = true;
+      package = pkgs.hypridle;
+      settings =
+      {
+        general = {
+          after_sleep_cmd = "hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
+          # before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
+          ignore_dbus_inhibit = false;
+          lock_cmd = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances.
+        };
+
+        listener = [
+          {
+            timeout = 900;
+            on-timeout = "hyprlock";
+          }
+          # dpms
+          {
+            timeout = 1200;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+          # Suspend
+          {
+            # SUSPEND TIMEOUT
+            timeout = 20000;
+            #SUSPEND ONTIMEOUT
+            on-timeout = "systemctl suspend";
+          }
+        ];
+      };
+    };
+  };
+}
