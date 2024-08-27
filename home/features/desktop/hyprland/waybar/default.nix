5505caae0691639e4e072a9c187ec0c256f6f782
Ryan Clark <ryanc@accentservices.com>
2024-08-27 16:21:54 -0500

    feat: Migrate rofi and cliphist services to directory

--- home/features/desktop/hyprland/waybar/default.nix
+++ home/features/desktop/hyprland/waybar/default.nix
@@ -7,6 +7,10 @@ let
   pavucontrol = "${pkgs.pavucontrol}/bin/pavucontrol";
   kitty = "${pkgs.kitty}/bin/kitty";
   nmtui = "${pkgs.networkmanager}/bin/nmtui";
+  nm-connection = "${pkgs.networkmanager}/bin/nm-connection-editor";
+  mission-center = "${pkgs.mission-center}/bin/mission-center";
+  cliphist = "${pkgs.cliphist}/bin/cliphist";
+  rofi = "${pkgs.rofi}/bin/rofi";
 in
 {
   programs = {
@@ -33,6 +37,78 @@ in
           };
           on-scroll-up = "hyprctl dispatch workspace e+1";
           on-scroll-down = "hyprctl dispatch workspace e-1";
+        };
+
+        "wlr/taskbar" = {
+          format = "{icon}";
+          icon-size = 18;
+          tooltip-format = "{title}";
+          on-click = "activate";
+          on-click-middle = "close";
+          ignore-list = ["Alacritty" "kitty"];
+          # app_ids-mapping = {
+          #   firefoxdeveloperedition = "firefox-developer-edition"
+          # };
+          # rewrite = {
+          #   "Firefox Web Browser" = "Firefox";
+          #   "Foot Server" = "Terminal"
+          # };
+        };
+
+        # "hyprland/window" = {
+        #   rewrite = {
+        #     (.*) - Brave = "$1";
+        #     (.*) - Chromium = "$1";
+        #     (.*) - Brave Search = "$1";
+        #     (.*) - Outlook = "$1";
+        #     (.*) Microsoft Teams = "$1"
+        #   };
+        #   separate-outputs = true
+        # };
+
+        # Empty
+        "custom/empty" = {
+          format = "";
+        };
+
+        # Empty
+        "custom/tools" = {
+          format = "";
+          tooltip-format = "Tools";
+        };
+
+        # Cliphist
+        "custom/cliphist" = {
+          format = "";
+          on-click = "sleep 0.1 && ${cliphist} list | ${rofi} -demu -replace -config ~/.config/rofi/config-cliphist.rasi | cliphist delete";
+          on-click-right = "sleep 0.1 && ${cliphist} list | ${rofi} -demu -replace -config ~/.config/rofi/config-cliphist.rasi | cliphist decode | wl-copy";
+          on-click-middle = "${cliphist} wipe";
+          tooltip-format = "Clipboard Manager";
+        };
+
+        # Power Menu
+        "custom/exit" = {
+          format = "";
+          on-click = "wlogout";
+          tooltip-format = "Power Menu";
+        };
+
+        # Hypridle inhibitor
+        "custom/hypridle" = {
+          format = "";
+          return-type = "json";
+          escape = true;
+          exec-on-event = true;
+          interval = 60;
+          exec = "~/.config/hypr/scripts/hypridle.sh status";
+          on-click = "~/.config/hypr/scripts/hypridle.sh toggle";
+          on-click-right = "hyprlock";
+        };
+
+        # System tray
+        tray = {
+          icon-size = 21;
+          spacing = 10;
         };
 
         clock = {
@@ -76,51 +152,124 @@ in
           interval = 5;
           format = "  {}%";
           tooltip = true;
+          on-click = "${mission-center}";
         };
 
         cpu = {
           interval = 5;
           format = "  {usage:2}%";
           tooltip = true;
+          on-click = "${mission-center}";
         };
 
         "custom/gpu" = {
             interval = 5;
             exec = "${cat} /sys/class/drm/card0/device/gpu_busy_percent";
             format = "󰒋  {}%";
+            on-click = "${mission-center}";
         };
 
         disk = {
+          interval = 30;
           format = "  {free}";
           tooltip = true;
+          on-click = "${mission-center}";
+        };
+
+        "hyprland/language" = {
+          format = "/ K {short}";
+        };
+
+        # Group Hardware
+        "group/hardware" = {
+          orientation = "inherit";
+          drawer = {
+            transition-duration = 300;
+            children-class = "not-memory";
+            transition-left-to-right = false;
+          };
+          modules = [
+            "custom/system"
+            "disk"
+            "cpu"
+            "custom/gpu"
+            "memory"
+            "hyprland/language"
+          ];
+        };
+
+        # Group Tools
+        "group/tools" = {
+          orientation = "inherit";
+          drawer = {
+            transition-duration = 300;
+            children-class = "not-memory";
+            transition-left-to-right = false;
+          };
+          modules = [
+            "custom/tools"
+            "custom/cliphist"
+            "custom/hypridle"
+            "custom/hyprshade"
+          ];
+        };
+
+        # Group Links
+        "group/links" = {
+          orientation = "horizontal";
+          modules = [
+            custom/chatgpt"
+            custom/empty"
+          ];
+        };
+
+        # Group Settings
+        "group/settings" = {
+          orientation = "inherit";
+          drawer = {
+            transition-duration = 300;
+            children-class = "not-memory";
+            transition-left-to-right = true;
+          };
+          modules = [
+            "custom/settings"
+            "custom/waybarthemes"
+            "custom/wallpaper"
+          ];
         };
 
         network = {
           interval = 3;
+          format = "{ifname}";
           format-icons = [ "󰤯" "󰤟" "󰤢" "󰤥" "󰤨" ];
           format-ethernet = ": {bandwidthDownOctets} : {bandwidthUpOctets}";
           format-wifi = "{icon} {signalStrength}% {essid}";
           format-disconnected = "󰤮";
-          tooltip = true;
+          # tooltip = true;
           tooltip-format = ''
             {ifname}
             {ipaddr}/{cidr}
             Up: {bandwidthUpBits}
             Down: {bandwidthDownBits}
           '';
-          max-length = 15;
+          tooltip-format-ethernet = '' {ifname}\nIP: {ipaddr}\n up: {bandwidthUpBits} down: {bandwidthDownBits}'';
+          tooltip-format-disconnected = "Disconnected";
+          tooltip-format-wifi = ''  {ifname} @ {essid}\nIP: {ipaddr}\nStrength: {signalStrength}%\nFreq: {frequency}MHz\nUp: {bandwidthUpBits} Down: {bandwidthDownBits}'';
+          max-length = 50;
           on-click = "${kitty} -e ${nmtui}";
+          on-click-right = "${nm-connection}";
         };
 
-        "tray" = {
-          spacing = 12;
-        };
+        # tray = {
+        #   spacing = 12;
+        # };
 
+        # Audio
         pulseaudio = {
           format = "{icon} {volume}% {format_source}";
           format-bluetooth = "{volume}% {icon} {format_source}";
           format-bluetooth-muted = " {icon} {format_source}";
-          format-muted = "   0%{format_source}";
+          format-muted = "  {format_source}";
           format-source = " {volume}%";
           format-source-muted = "";
           format-icons = {
@@ -131,8 +280,24 @@ in
             car = "";
             default = [ "" "" "" ];
           };
-          on-click = pavucontrol;
+          on-click = "${pavucontrol}";
           tooltip-format = "{source_volume}% / {desc}";
+        };
+
+        # Bluetooth
+        bluetooth = {
+          format = " {status}";
+          format-disabled = "";
+          format-off = "";
+          interval = 30;
+          on-click = "blueman-manager";
+          format-no-controller = "";
+        };
+
+        user = {
+          format = "{user}";
+          interval = 60;
+          icon = false;
         };
 
         "custom/notification" = {
@@ -161,6 +326,28 @@ in
             activated = "󰒳";
             deactivated = "󰒲";
           };
+        };
+
+        backlight = {
+          format = "{icon} {percentage}%";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+            ""
+            ""
+            ""
+            ""
+            ""
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+          scroll-step = 1;
         };
 
         battery = {
