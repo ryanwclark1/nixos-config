bcd58c2a0e3478f330749b2a9a13bfaef5581ac1
Ryan Clark <ryanc@accentservices.com>
2024-08-30 08:02:04 -0500

    refactor: Update home.packages in js.nix
    This commit updates the home.packages list in the js.nix file. It adds the 'bun' package, removes the 'deno' package, and includes the 'npm' and 'pnpm' packages. These changes improve the development environment by updating the package list and removing outdated packages.

--- home/features/development/js.nix
+++ home/features/development/js.nix
@@ -10,23 +10,13 @@
     nodePackages.npm
     nodePackages.pnpm
     yarn
-    # deno
+    deno
     tailwindcss
+    # npm
+    # pnpm
+  ])
+  ++ (with pkgs.nodePackages; [
     npm
     pnpm
   ]);
-  # )
-  # ++ (with pkgs.nodePackages; [
-
-  # ])
-  # ;
-
-
-  home.sessionPath = [
-    "$HOME/.node/bin"
-    "$HOME/.deno/bin"
-    "$HOME/.bun/bin"
-  ];
-
-
 }
