6d851b14e7e183754b447afa04171b472a16dfc2
Ryan Clark <ryanc@accentservices.com>
2024-08-31 15:05:54 -0500

    refactor: Update waybar modules-right configuration

--- home/features/desktop/hyprland/hypridle/default.nix
+++ home/features/desktop/hyprland/hypridle/default.nix
@@ -9,7 +9,7 @@
       {
         general = {
           after_sleep_cmd = "hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
-          # before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
+          before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
           ignore_dbus_inhibit = false;
           lock_cmd = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances.
         };
@@ -26,12 +26,12 @@
             on-resume = "hyprctl dispatch dpms on";
           }
           # Suspend
-          {
-            # SUSPEND TIMEOUT
-            timeout = 20000;
-            #SUSPEND ONTIMEOUT
-            on-timeout = "systemctl suspend";
-          }
+          # {
+          #   # SUSPEND TIMEOUT
+          #   timeout = 20000;
+          #   #SUSPEND ONTIMEOUT
+          #   on-timeout = "systemctl suspend";
+          # }
         ];
       };
     };
