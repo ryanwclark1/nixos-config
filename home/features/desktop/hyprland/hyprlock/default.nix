e1f0024075b46005aa4b41114dcaf646479c2b47
Ryan Clark <ryanc@accentservices.com>
2024-08-31 17:07:49 -0500

    refactor: Update waybar modules-right configuration

--- home/features/desktop/hyprland/hyprlock/default.nix
+++ home/features/desktop/hyprland/hyprlock/default.nix
@@ -26,69 +26,48 @@ with config.stylix.fonts;
           # monitor =
           path = "${config.stylix.image}";
           # color = "${base00}";
-          # blur_passes = 50;
+          blur_passes = 1; # 0 disables blurring
+          blur_size = 7;
+          noise = 0.0117;
+          contrast = 0.8916;
+          brightness = 0.8172;
+          vibrancy = 0.1696;
+          vibrancy_darkness = 0.0;
         };
 
         # TIME
         label = {
           # monitor =
-          text = "Hi there, $USER";
+          text = "$TIME";
           color = "${base00}";
-          font_size = 80;
+          font_size = 60;
           font_family = "${sansSerif.name}";
-          shadow_passes = 3;
-          shadow_size = 3;
-
-          position = "0, -100";
+          shadow_passes = 1;
+          shadow_size = 1;
+          position = "0, -200";
           halign = "center";
           valign = "top";
         };
 
-
-
-        # DATE
-        # label = {
-        #   # monitor =
-        #   # text = cmd[update:43200000] echo "$(date +"%A, %d %B %Y")"
-        #   # color = $text;
-        #   font_size = 18;
-        #   # font_family = $font;
-        #   position = "0, -300";
-        #   halign = "center";
-        #   valign = "top";
-        # };
-
-        # USER AVATAR
-
-        # image {
-        #   monitor =
-        #   path = ~/Pictures/pp/pp.png
-        #   size = 125
-        #   border_color = $accent
-
-        #   position = 0, -450
-        #   halign = center
-        #   valign = center
-        # }
-
         # INPUT FIELD
         input-field = {
           # monitor =
-          size = "300, 60";
-          outline_thickness = 4;
-          dots_size = 0.2;
-          dots_spacing = 0.4;
-          dots_center = true;
+          size = "400, 60";
+          outline_thickness = 1;
+          dots_size = 0.33; # Scale of input-field height, 0.2 - 0.8
+          dots_spacing = 0.15; # Scale of dots' absolute size, 0.0 - 1.0
+          dots_center = false;
+          dots_rounding = -1; # -1 default circle, -2 follow input-field rounding
           # outer_color = $accent;
-          # inner_color = $surface0;
+          inner_color = "${base05}";
           font_color = "${base00}";
           fade_on_empty = false;
           placeholder_text = "<span><i>󰌾  Logged in as </i><span>$USER</span></span>";
           hide_input = false;
           # check_color = $accent;
-          # fail_color = $red;
+          fail_color = "${base08}";
           fail_text = ''<i>$FAIL <b>($ATTEMPTS)</b></i>'';
-          # capslock_color = $yellow;
+          capslock_color = "${base0A}";
           position = "0, -100";
           halign = "center";
           valign = "center";
@@ -99,29 +78,22 @@ with config.stylix.fonts;
 
 }
 
-
-# {
-#   colors,
-#   font,
-#   mkRGBA,
-#   mkRGB,
-#   mkHHex,
-#   radius,
-#   wallpaper,
-#   ...
-# }: ''
-
-#   general {
-#     grace = 0
-#     hide_cursor = true
-#   }
+    # USER AVATAR
+    # image {
+    #   monitor =
+    #   path = ~/Pictures/pp/pp.png
+    #   size = 125
+    #   border_color = $accent
+    #   position = 0, -450
+    #   halign = center
+    #   valign = center
+    # }
 
 #   # wallpaper
 #   background {
 #       monitor =
 #       path = ${wallpaper.package}/${wallpaper.name}   # supports png, jpg, webp (no animations, though)
 #       color = ${mkRGBA colors.background 1.0}
-
 #       blur_passes = 1 # 0 disables blurring
 #       blur_size = 7
 #       noise = 0.0117
@@ -139,11 +111,9 @@ with config.stylix.fonts;
 #       border_size = 0
 #       rotate = 0
 #       xray = false # if true, make a "hole" in the background (rectangle of specified size, no rotation)
-
 #       position = 150, 0
 #       halign = left
 #       valign = center
-
 #       shadow_passes = 3
 #   }
 
@@ -157,11 +127,9 @@ with config.stylix.fonts;
 #       border_size = 0
 #       border_color =${mkRGB colors.accent}
 #       reload_time = -1 # seconds between reloading, 0 to reload with SIGUSR2
-
 #       position = 650, 0
 #       halign = center
 #       valign = center
-
 #       shadow_passes = 3
 #   }
 
@@ -192,81 +160,8 @@ with config.stylix.fonts;
 #       bothlock_color = -1 # when both locks are active. -1 means don't change outer color (same for above)
 #       invert_numlock = false # change color if numlock is off
 #       swap_font_color = false # see below
-
 #       position = -150, -300
 #       halign = center
 #       valign = center
-
-#       shadow_passes = 3
-
-#   }
-
-#   # Salutation message
-#   label {
-#       monitor =
-#       text =Hi there, <span foreground='#${mkHHex colors.accent}'>$USER</span>.
-#       text_align = left # center/right or any value for default left. multi-line text alignment inside label container
-#       color = ${mkRGB colors.text}
-#       font_size = 46
-#       font_family = ${font.name}
-#       rotate = 0 # degrees, counter-clockwise
-
-#       position = 200, 300
-#       halign = left
-#       valign = center
-
 #       shadow_passes = 3
 #   }
-
-#   # Date
-#   label {
-#       monitor =
-#       text =cmd[update:3600000] echo "It's <span foreground='#${mkHHex colors.accent}'>$(date +%A)</span>, <span foreground='#${mkHHex colors.accent}'>$(date +%B)</span> the <span foreground='#${mkHHex colors.accent}'>$(date +%d)</span> of <span foreground='#${mkHHex colors.accent}'>$(date +%Y)</span>."
-#       text_align = left # center/right or any value for default left. multi-line text alignment inside label container
-#       color = ${mkRGB colors.text}
-#       font_size = 46
-#       font_family = Noto Sans
-#       rotate = 0 # degrees, counter-clockwise
-
-#       position = 200, 100
-#       halign = left
-#       valign = center
-
-#       shadow_passes = 3
-
-#   }
-
-#   # time
-#   label {
-#       monitor =
-#       text =cmd[update:1000] echo "It's also <span foreground='#${mkHHex colors.accent}'>$(date +%H)</span>:<span foreground='#${mkHHex colors.accent}'>$(date +%M)</span>:<span foreground='#${mkHHex colors.accent}'>$(date +%S)</span>."
-#       text_align = left # center/right or any value for default left. multi-line text alignment inside label container
-#       color = ${mkRGB colors.text}
-#       font_size = 46
-#       font_family = ${font.name}
-#       rotate = 0 # degrees, counter-clockwise
-
-#       position = 200, -50
-#       halign = left
-#       valign = center
-
-#       shadow_passes = 3
-
-#   }
-
-#   # Funni note
-#   label {
-#       monitor =
-#       text = cmd[update:0] hyprctl splash
-#       text_align = right # center/right or any value for default left. multi-line text alignment inside label container
-#       color = ${mkRGB colors.text}
-#       font_size = 25
-#       font_family = ${font.name}
-#       rotate = 0 # degrees, counter-clockwise
-
-#       position = 0, 10
-#       halign = center
-#       valign = bottom
-#   }
-
-# ''
