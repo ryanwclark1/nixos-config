09fdcb8320407f3ed56c9017cc35c0b137d1117f
Ryan Clark <ryanc@accentservices.com>
2024-09-01 19:46:05 -0500

    test microphone script gen

---
+++ home/features/desktop/hyprland/waybar/scripts/mic.nix
@@ -0,0 +1,36 @@
+{ pkgs }:
+
+pkgs.writeShellScriptBin "microphone" ''
+  set -e
+
+  # https://blog.dhampir.no/content/sleeping-without-a-subprocess-in-bash-and-how-to-sleep-forever
+  snore() {
+      local IFS
+      [[ -n "${_snore_fd:-}" ]] || exec {_snore_fd}<> <(:)
+      read -r ${1:+-t "$1"} -u $_snore_fd || :
+  }
+
+  DELAY=0.2
+
+  while snore $DELAY; do
+      WP_OUTPUT=$(wpctl get-volume @DEFAULT_AUDIO_SOURCE@)
+
+      if [[ $WP_OUTPUT =~ ^Volume:[[:blank:]]([0-9]+)\.([0-9]{2})([[:blank:]].MUTED.)?$ ]]; then
+          if [[ -n ${BASH_REMATCH[3]} ]]; then
+              printf "\n"
+          else
+              VOLUME=$((10#${BASH_REMATCH[1]}${BASH_REMATCH[2]}))
+              ICON=(
+                  ""
+              )
+
+              if [[ $VOLUME -gt 0 ]]; then
+                  printf "%s" "${ICON[0]} "
+              fi
+              printf "$VOLUME%%\n"
+          fi
+      fi
+  done
+
+  exit 0
+''
