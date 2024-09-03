4c40b57f18f540617134d119b62378a05d01c14d
Ryan Clark <ryanc@accentservices.com>
2024-09-03 10:20:41 -0500

    Try cat pink colors for starship

--- home/features/starship/default.nix
+++ home/features/starship/default.nix
@@ -12,7 +12,7 @@ with config.lib.stylix.colors.withHashtag;
     enable = true;
     # Configuration written to ~/.config/starship.toml
     settings = {
-      format = "[](${base08})\$os\$username\$hostname\$localip\${custom.yazi}\[](bg:${base09} fg:${base08})\$directory\[](fg:${base09} bg:${base0A})\$git_branch\$git_status\[](fg:${base0A} bg:${base0B})\$bun\$c\$cmake\$dart\$golang\$haskell\$java\$kotlin\$kubernetes\$lua\$nodejs\$php\$python\$rust\$swift\$zig[](fg:${base0B} bg:${base01})\$docker_context\[](fg:${base01})\$character";
+      format = "[](${base0E})\$os\$username\$hostname\$localip\${custom.yazi}\[](bg:${base07} fg:${base0E})\$directory\[](fg:${base07} bg:${base05})\$git_branch\$git_status\[](fg:${base05} bg:${base0F})\$bun\$c\$cmake\$dart\$golang\$haskell\$java\$kotlin\$kubernetes\$lua\$nodejs\$php\$python\$rust\$swift\$zig[](fg:${base0F} bg:${base06})\$docker_context\[](fg:${base06})\$character";
       add_newline = true;
       scan_timeout = 30;
       command_timeout = 500;
@@ -24,7 +24,7 @@ with config.lib.stylix.colors.withHashtag;
 
       os = {
         disabled = false;
-        style = "bg:${base08} fg:${base04}";
+        style = "bg:${base0E} fg:${base00}";
         symbols = {
           Windows = "󰍲";
           Ubuntu = "󰕈";
@@ -52,15 +52,15 @@ with config.lib.stylix.colors.withHashtag;
       username = {
         show_always = true;
         disabled = false;
-        style_user = "bg:${base08} fg:${base04}";
-        style_root = "bg:${base08} fg:${base04}";
+        style_user = "bg:${base0E} fg:${base00}";
+        style_root = "bg:${base0E} fg:${base00}";
         format = "[ $user ]($style)";
       };
 
       localip = {
         disabled = false;
         ssh_only = true;
-        style = "bg:${base08} fg:${base04}";
+        style = "bg:${base0E} fg:${base00}";
         format = "[ $localipv4 ]($style)";
       };
 
@@ -68,12 +68,12 @@ with config.lib.stylix.colors.withHashtag;
         disabled = false;
         ssh_only = true;
         ssh_symbol = "";
-        style = "bg:${base08} fg:${base04}";
+        style = "bg:${base0E} fg:${base00}";
         format = "[$ssh_symbol]($style)";
       };
 
       directory = {
-        style = "fg:${base04} bg:${base09}";
+        style = "fg:${base00} bg:${base07}";
         format = "[ $path ]($style)";
         truncation_length = 8;
         truncate_to_repo = true;
@@ -125,126 +125,126 @@ with config.lib.stylix.colors.withHashtag;
 
       container = {
         symbol = "";
-        style = "fg:${base04} bg:${base09}";
+        style = "fg:${base00} bg:${base07}";
         format = "[$symbol\ [$name]]($style)";
       };
 
       git_branch = {
         symbol = "";
-        style = "bg:${base0A}";
-        format = "[[ $symbol $branch ](fg:${base04} bg:${base0A})]($style)";
+        style = "bg:${base05}";
+        format = "[[ $symbol $branch ](fg:${base00} bg:${base05})]($style)";
       };
 
       git_status = {
-        style = "bg:${base0A}";
-        format = "[[($all_status$ahead_behind )](fg:${base04} bg:${base0A})]($style)";
+        style = "bg:${base05}";
+        format = "[[($all_status$ahead_behind )](fg:${base00} bg:${base05})]($style)";
       };
 
       bun = {
         symbol = "";
-        style = "bg:${base0B}";
-        format = "[[ $symbol( $version) ](fg:${base04} bg:${base0B})]($style)";
+        style = "bg:${base0F}";
+        format = "[[ $symbol( $version) ](fg:${base00} bg:${base0F})]($style)";
       };
 
       c = {
         symbol = " ";
-        style = "bg:${base0B}";
-        format = "[[ $symbol( $version) ](fg:${base04} bg:${base0B})]($style)";
+        style = "bg:${base0F}";
+        format = "[[ $symbol( $version) ](fg:${base00} bg:${base0F})]($style)";
       };
 
       cmake = {
         symbol = " ";
-        style = "bg:${base0B}";
-        format = "[[ $symbol( $version) ](fg:${base04} bg:${base0B})]($style)";
+        style = "bg:${base0F}";
+        format = "[[ $symbol( $version) ](fg:${base00} bg:${base0F})]($style)";
       };
 
       dart = {
         symbol = " ";
-        style = "bg:${base0B}";
-        format = "[[ $symbol( $version) ](fg:${base04} bg:${base0B})]($style)";
+        style = "bg:${base0F}";
+        format = "[[ $symbol( $version) ](fg:${base00} bg:${base0F})]($style)";
         disabled = true;
       };
 
       golang = {
         symbol = "";
-        style = "bg:${base0B}";
-        format = "[[ $symbol( $version) ](fg:${base04} bg:${base0B})]($style)";
+        style = "bg:${base0F}";
+        format = "[[ $symbol( $version) ](fg:${base00} bg:${base0F})]($style)";
       };
 
       haskell = {
         symbol = "";
-        style = "bg:${base0B}";
-        format = "[[ $symbol( $version) ](fg:${base04} bg:${base0B})]($style)";
+        style = "bg:${base0F}";
+        format = "[[ $symbol( $version) ](fg:${base00} bg:${base0F})]($style)";
       };
 
       java = {
         symbol = " ";
-        style = "bg:${base0B}";
-        format = "[[ $symbol( $version) ](fg:${base04} bg:${base0B})]($style)";
+        style = "bg:${base0F}";
+        format = "[[ $symbol( $version) ](fg:${base00} bg:${base0F})]($style)";
       };
 
       kotlin = {
         symbol = "";
-        style = "bg:${base0B}";
-        format = "[[ $symbol( $version) ](fg:${base04} bg:${base0B})]($style)";
+        style = "bg:${base0F}";
+        format = "[[ $symbol( $version) ](fg:${base00} bg:${base0F})]($style)";
       };
 
       kubernetes = {
         symbol = "󱃾";
-        style = "bg:${base0B}";
-        format = "[[ $symbol$context( \$namespace\ ) ](fg:${base04} bg:${base0B})]($style)";
+        style = "bg:${base0F}";
+        format = "[[ $symbol$context( \$namespace\ ) ](fg:${base00} bg:${base0F})]($style)";
       };
 
       lua = {
         symbol = "";
-        style = "bg:${base0B}";
-        format = "[[ $symbol( $version) ](fg:${base04} bg:${base0B})]";
+        style = "bg:${base0F}";
+        format = "[[ $symbol( $version) ](fg:${base00} bg:${base0F})]";
         disabled = true;
       };
 
       nodejs = {
         symbol = "";
-        style = "bg:${base0B}";
-        format = "[[ $symbol( $version) ](fg:${base04} bg:${base0B})]($style)";
+        style = "bg:${base0F}";
+        format = "[[ $symbol( $version) ](fg:${base00} bg:${base0F})]($style)";
       };
 
       php = {
         symbol = "";
-        style = "bg:${base0B}";
-        format = "[[ $symbol( $version) ](fg:${base04} bg:${base0B})]($style)";
+        style = "bg:${base0F}";
+        format = "[[ $symbol( $version) ](fg:${base00} bg:${base0F})]($style)";
       };
 
       python = {
         symbol = "";
-        style = "bg:${base0B}";
+        style = "bg:${base0F}";
         version_format = "$raw";
         format = "[ $symbol( $version )(\($virtualenv\) )]($style)";
       };
 
       rust = {
         symbol = "";
-        style = "bg:${base0B}";
-        format = "[[ $symbol( $version) ](fg:${base04} bg:${base0B})]($style)";
+        style = "bg:${base0F}";
+        format = "[[ $symbol( $version) ](fg:${base00} bg:${base0F})]($style)";
       };
 
       swift = {
         symbol = "";
-        style = "bg:${base0B}";
-        format = "[[ $symbol( $version) ](fg:${base04} bg:${base0B})]($style)";
+        style = "bg:${base0F}";
+        format = "[[ $symbol( $version) ](fg:${base00} bg:${base0F})]($style)";
         disabled = true;
       };
 
       zig = {
         symbol = "";
-        style = "bg:${base0B}";
-        format = "[[ $symbol( $version) ](fg:${base04} bg:${base0B})]";
+        style = "bg:${base0F}";
+        format = "[[ $symbol( $version) ](fg:${base00} bg:${base0F})]";
         disabled = true;
       };
 
       docker_context = {
         symbol = "";
-        style = "bg:${base01}";
-        format = "[[ $symbol( $context) ](fg:${base0C} bg:${base01})]($style)";
+        style = "bg:${base06}";
+        format = "[[ $symbol( $context) ](fg:${base0C} bg:${base06})]($style)";
       };
 
       custom.yazi = {
