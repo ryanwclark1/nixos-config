46654c055e5c2a881062dd7e0b56cf84d9e39b0c
Ryan Clark <ryanc@accentservices.com>
2024-09-04 13:58:00 -0500

    chore: Remove unused nix tools and update xdg cache mappings
    This commit removes unused nix tools and updates xdg cache mappings. The removal of unused nix tools helps to declutter the repository and improve maintainability. The update to xdg cache mappings ensures that the correct cache directories are used, enhancing the overall functionality and compatibility of the application.

--- home/features/media/default.nix
+++ home/features/media/default.nix
@@ -10,19 +10,18 @@
     ./drawio.nix
     ./ffmpeg.nix
     ./gimp.nix
-    ./graphviz.nix
+    # ./graphviz.nix
     ./gstreamer.nix
     ./handbrake.nix
     ./imagemagick.nix
     ./inkscape.nix
-    ./kdenlive.nix
+    # ./kdenlive.nix
     ./krita.nix
     ./mpv.nix
     ./music.nix
     # ./obs.nix
     ./pavucontrol.nix
     ./playerctl.nix
-    ./vhs.nix
     ./vlc.nix
     ./yt-dlp.nix
   ];
