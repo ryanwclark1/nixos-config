9a27260cd3d9863fe46a1fb93838cf772f87674b
Ryan Clark <ryanc@accentservices.com>
2024-09-03 20:41:37 -0500

    refactor: Update zathura.nix to include font configuration
    This commit updates the zathura.nix file to include font configuration using the `config.stylix.fonts` property. The `font` option is set to `${monospace.name}` to use the monospace font defined in the configuration. Additionally, the `recolor` option is set to `true` to enable book content color recoloring. These changes enhance the visual experience and customization options of the Zathura application.

--- home/features/productivity/zathura.nix
+++ home/features/productivity/zathura.nix
@@ -1,6 +1,8 @@
 {
+  config,
   ...
 }:
+with config.stylix.fonts;
 
 {
   programs.zathura = {
@@ -10,11 +12,12 @@
       ###########
       # Options #
       ###########
-      font = "DejaVu Sans 14";
+      font = "${monospace.name}";
       adjust-open = "width";
       pages-per-row = 1;
       selection-clipboard = "clipboard";
       incremental-search = true;
+      recolor = true;
 
       window-title-home-tilde = true;
       window-title-basename = true;
@@ -25,51 +28,6 @@
       statusbar-v-padding = 0;
       page-padding = 1;
 
-      #########
-      # Theme #
-      #########
-      # GRUVBOX Theme
-      # https://github.com/eastack/zathura-gruvbox/blob/master/zathura-gruvbox-dark
-
-      # notification-error-bg = "#282828"; # bg
-      # notification-error-fg = "#fb4934"; # bright:red
-      # notification-warning-bg = "#282828"; # bg
-      # notification-warning-fg = "#fabd2f"; # bright:yellow
-      # notification-bg = "#282828"; # bg
-      # notification-fg = "#b8bb26"; # bright:green
-
-      # completion-bg = "#504945"; # bg2
-      # completion-fg = "#ebdbb2"; # fg
-      # completion-group-bg = "#3c3836"; # bg1
-      # completion-group-fg = "#928374"; # gray
-      # completion-highlight-bg = "#83a598"; # bright:blue
-      # completion-highlight-fg = "#504945"; # bg2
-
-      # # Define the color in index mode
-      # index-bg = "#504945"; # bg2
-      # index-fg = "#ebdbb2"; # fg
-      # index-active-bg = "#83a598"; # bright:blue
-      # index-active-fg = "#504945"; # bg2
-
-      # inputbar-bg = "#282828"; # bg
-      # inputbar-fg = "#ebdbb2"; # fg
-
-      # statusbar-bg = "#504945"; # bg2
-      # statusbar-fg = "#ebdbb2"; # fg
-
-      # highlight-color = "#fabd2f"; # bright:yellow
-      # highlight-active-color = "#fe8019"; # bright:orange
-
-      # default-bg = "#282828"; # bg
-      # default-fg = "#ebdbb2"; # fg
-      # render-loading = true;
-      # render-loading-bg = "#282828"; # bg
-      # render-loading-fg = "#ebdbb2"; # fg
-
-      # # Recolor book content's color
-      # recolor-lightcolor = "#282828"; # bg
-      # recolor-darkcolor = "#ebdbb2"; # fg
-      # recolor-keephue = true; # keep original color
     };
 
     ################
