d6906bbdfca50d1303178dfd910d5ce109e71e88
Ryan Clark <ryanc@accentservices.com>
2024-09-05 11:59:58 -0500

    syntax and formatting update

--- hosts/common/users/administrator/default.nix
+++ hosts/common/users/administrator/default.nix
@@ -17,37 +17,39 @@ in
   };
   # switch back to mutableusers = false
   users.mutableUsers = true;
-  users.users.administrator = {
-    isNormalUser = true;
-    home = "/home/${user}";
-    shell = "${pkgs.zsh}/bin/zsh";
-    extraGroups = [
-      "audio"
-      "video"
-      "wheel"
-    ] ++ ifTheyExist [
-      "deluge"
-      "docker"
-      "git"
-      "i2c"
-      "libvirtd"
-      "mysql"
-      "network"
-      "networkmanager"
-      "plugdev"
-      "podman"
-      "postgres"
-      "syncthing"
-      "wireshark"
-    ];
+  users = {
+    users.administrator = {
+      isNormalUser = true;
+      home = "/home/${user}";
+      shell = "${pkgs.zsh}/bin/zsh";
+      extraGroups = [
+        "audio"
+        "video"
+        "wheel"
+      ] ++ ifTheyExist [
+        "deluge"
+        "docker"
+        "git"
+        "i2c"
+        "libvirtd"
+        "mysql"
+        "network"
+        "networkmanager"
+        "plugdev"
+        "podman"
+        "postgres"
+        "syncthing"
+        "wireshark"
+      ];
 
-    openssh.authorizedKeys.keys = [
-      (builtins.readFile ./keys/id_frametop.pub)
-    ];
+      openssh.authorizedKeys.keys = [
+        (builtins.readFile ./keys/id_frametop.pub)
+      ];
 
-    hashedPasswordFile = config.sops.secrets.administrator-password.path;
+      hashedPasswordFile = config.sops.secrets.administrator-password.path;
 
-    packages = [ pkgs.home-manager ];
+      packages = [ pkgs.home-manager ];
+    };
   };
 
   home-manager.users.administrator = import ../../../../home/${config.networking.hostName}.nix;
