fcbe41550323488b0fe3a17f8a1af6b8396d2da8
Ryan Clark <ryanc@accentservices.com>
2024-09-05 11:58:39 -0500

    feat: Update global locale configuration
    This commit updates the global locale configuration in the `locale.nix` file. It modifies the `services.geoclue2` and `services.timesyncd` sections, enabling the `geoclue2` service and disabling data submission, and enabling the `timesyncd` service. Additionally, it updates the `location.provider` to use `geoclue2` and sets the `time.hardwareClockInLocalTime` to `false` and `time.timeZone` to `America/Chicago`. These changes enhance the functionality and user experience of the application by improving location and time synchronization.

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -10,13 +10,21 @@
       "en_US.UTF-8/UTF-8"
     ];
   };
-  services.geoclue2 = {
-    enable = true;
-    submitData = false;
-  };
-  services.timesyncd = {
-    enable = true;
-  };
+
   location.provider = "geoclue2";
-  time.timeZone = lib.mkDefault "America/Chicago";
+
+  services = {
+    geoclue2 = {
+      enable = true;
+      submitData = false;
+    };
+    timesyncd = {
+      enable = true;
+    };
+  };
+
+  time = {
+    hardwareClockInLocalTime = false;
+    timeZone = lib.mkDefault "America/Chicago";
+  };
 }
