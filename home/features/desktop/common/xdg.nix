03971bbc7d21aa7d4e42b4c6e6981cc2af92b8a6
Ryan Clark <ryanc@accentservices.com>
2024-09-05 08:56:51 -0500

    feat: Update xdg.nix to include new desktopEntries and mimeApps configurations
    This commit updates the xdg.nix file to include new configurations for desktopEntries and mimeApps. The desktopEntries configuration adds two new entries for the Firefox web browser and the eog image viewer, providing users with convenient options for browsing the web and viewing images. The mimeApps configuration updates the default applications for various file types, including audio, video, images, and text files, ensuring that the appropriate programs are used to open these files. These changes enhance the functionality and user experience of the desktop environment.

--- home/features/desktop/common/xdg.nix
+++ home/features/desktop/common/xdg.nix
@@ -12,8 +12,14 @@
 
   xdg = {
     enable = true;
-    cacheHome = config.home.homeDirectory + "/.local/cache";
     configFile."mimeapps.list".force = true;
+    cacheHome = config.home.homeDirectory + "/.local/cache";
+    configHome = "${config.home.homeDirectory}/.config";
+    dataHome = "${config.home.homeDirectory}/.local/share";
+    systemDirs = {
+      config = [ "${config.home.homeDirectory}/.config" ];
+      data = [ "${config.home.homeDirectory}/.local/share" "/usr/share" "/usr/share/applications/" "/usr/local/share/applications/" ];
+    };
     userDirs = {
       enable = true;
       createDirectories = true;
@@ -30,32 +36,110 @@
         XDG_SCREENCAST_DIR = "${config.xdg.userDirs.videos}/Screencast";
       };
     };
+    desktopEntries = {
+      firefox = {
+        name = "Firefox";
+        genericName = "Web Browser";
+        exec = "firefox %U";
+        terminal = false;
+        categories = [ "Application" "Network" "WebBrowser" ];
+        mimeType = [
+          "text/html"
+          "text/xml"
+          "x-scheme-handler/http"
+          "x-scheme-handler/https"
+          "x-scheme-handler/chrome"
+          "application/x-extension-htm"
+          "application/x-extension-html"
+          "application/x-extension-shtml"
+          "application/xhtml+xml"
+          "application/x-extension-xhtml"
+          "application/x-extension-xht"
+        ];
+      };
+      eog = {
+        name = "eog";
+        genericName = "Image Viewer";
+        exec = "eog %U";
+        terminal = false;
+        categories = [ "Application" "Graphics" "Viewer" ];
+        mimeType = [
+          "image/png"
+          "image/heic"
+          "image/heif"
+          "image/jpeg"
+          "image/gif"
+          "image/bmp"
+          "image/jpg"
+          "image/tiff"
+          "image/x-bmp"
+          "image/x-ico"
+        ];
+      };
+    };
     mimeApps =
     {
       enable = true;
       defaultApplications =
         let
-          browser = ["firefox"];
+          browser = [
+            "firefox.desktop"
+          ];
           videoPlayers = [
             "mpv"
             "vlc"
           ];
-          imageViewers = [ "imv.desktop" ];
+          imageViewers = [
+            "eog.desktop"
+            "imv.desktop"
+          ];
+
+          codeEditors = [
+            "code.desktop"
+          ];
         in
         {
+          #audio video
           "audio/mp3" = videoPlayers;
           "audio/aac" = videoPlayers;
           "audio/wav" = videoPlayers;
           "video/mp4" = videoPlayers;
           "video/mpeg" = videoPlayers;
+          "video/mov" = videoPlayers;
 
+          #images
           "image/png" = imageViewers;
           "image/jpeg" = imageViewers;
           "image/gif" = imageViewers;
-          "image/webp" = imageViewers;
+          "image/bmp" = imageViewers;
+          "image/jpg" = imageViewers;
+          "image/tiff" = imageViewers;
+          "image/x-bmp" = imageViewers;
+          "image/x-ico" = imageViewers;
+          "image/heic" = imageViewers;
+          "image/heif" = imageViewers;
+
+          #vscode for text etc
+          "text/plain" = codeEditors;
+          "text/x-c" = codeEditors;
+          "text/x-c++" = codeEditors;
+          "text/x-c++src" = codeEditors;
+          "text/x-chdr" = codeEditors;
+          "text/x-csrc" = codeEditors;
+          "text/x-diff" = codeEditors;
+          "text/x-dsrc" = codeEditors;
+          "text/x-haskell" = codeEditors;
+          "text/x-java" = codeEditors;
+          "text/x-makefile" = codeEditors;
+          "text/x-moc" = codeEditors;
+          "text/x-pcs-gcd" = codeEditors;
+          "text/x-perl" = codeEditors;
+          "text/x-python" = codeEditors;
+          "text/x-scala" = codeEditors;
+          "text/x-scheme" = codeEditors;
+          "text/css" = codeEditors;
 
           # "text/plain" = textEditors;
-
           "text/csv" = [ "libreoffice" ];
           "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = [ "libreoffice" ];
           "application/vnd.ms-excel" = [ "libreoffice" ];
@@ -68,12 +152,18 @@
           ];
           "application/vnd.ms-powerpoint" = [ "libreoffice" ];
           # "application/pdf" = [ "wps-office-pdf.desktop" ];
-          "image/svg+xml" = browser;
+
+          #web
           "text/html" = browser;
           "x-scheme-handler/http" = browser;
           "x-scheme-handler/https" = browser;
-          "x-scheme-handler/about" = browser;
-          "x-scheme-handler/unknown" = browser;
+          "x-scheme-handler/chrome" = browser;
+          "application/x-extension-htm" = browser;
+          "application/x-extension-html" = browser;
+          "application/x-extension-shtml" = browser;
+          "application/xhtml+xml" = browser;
+          "application/x-extension-xhtml" = browser;
+          "application/x-extension-xht" = browser;
         };
     };
   };
