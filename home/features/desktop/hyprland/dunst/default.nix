f38c2cf5567cb8d1016b84cf6c26f9659ed2fab5
Ryan Clark <ryanc@accentservices.com>
2024-09-06 12:12:54 -0500

    chore: Update dunst configuration styles and options

--- home/features/desktop/hyprland/dunst/default.nix
+++ home/features/desktop/hyprland/dunst/default.nix
@@ -1,99 +1,123 @@
-{ pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with config.lib.stylix.colors.withHashtag;
+with config.stylix.fonts;
 
 {
   services = {
-    dunst = {
+    dunst =
+    let
+      font = "${monospace.name}";
+      background-default = "${base00}";
+      border = "${base0D}";
+      progress = "${base01}";
+      text-color = "${base0A}";
+      background-low = "${base06}";
+      text-color-low = "${base0A}";
+      backgroud-high = "${base0F}";
+      test-color-high = "${base06}";
+      opacity = lib.toHexString (((builtins.ceil (config.stylix.opacity.popups * 100)) * 255) / 100);
+      rofi = lib.getExe config.programs.rofi.package;
+    in
+    {
       enable = true;
       package = pkgs.dunst;
-      # $XDG_CONFIG_HOME/dunst/dunstrc
       settings = {
+        # https://dunst-project.org/documentation
         global = {
           monitor = 0;
           follow = "none";
-          width = 300;
-          height = 300;
-          offset = "30x30";
+          width = 500;
+          height = 800;
+          notification_limit = 20;
+          offset = "0x10";
           origin = "top-center";
           scale = 0;
-          notification_limit = 20;
           progress_bar = true;
+          progress_bar_horizontal_alignmen = "center";
           progress_bar_height = 10;
-          progress_bar_frame_width = 1;
           progress_bar_min_width = 150;
           progress_bar_max_width = 300;
-          progress_bar_corner_radius = 10;
-          indicate_hidden = "yes";
-          transparency = 30;
+          progress_bar_frame_width = 1;
+          progress_bar_corner_radius = 0;
+          progress_bar_corners = "all";
+          icon_corner_radius = 0;
+          icon_corners = "all";
+          indicate_hidden = true;
+          # transparency = 30;
           seperator_height = 2;
           padding = 8;
           horizontal_padding = 8;
           text_icon_padding = 0;
-          frame_width = 1;
-          frame_color = "#ffffff";
+          frame_width = 3;
           gap_size = 0;
-          seperator_color = "frame";
-          sort = "yes";
-          icon_corner_radius = 0;
-          font = "Droid Sans 9";
-          line_height = 1;
-          markup = "full";
-          format = "<b>%s</b>\n%b";
-          alignment = "left";
+          frame_color = "${border}";
+          seperator_color = "${border}";
+          sort = true;
+          idle_threshold = 0;
+          layer = "overlay";
+          force_xwayland = false;
+          font = "${font} 12";
+          line_height = 0;
+          format = ''<b>%s</b>\n%b'';
           vertical_alignment = "center";
           show_age_threshold = 60;
-          ellipsize = "middle";
-          ignore_newline = "no";
+          ignore_newline = false;
           stack_duplicates = true;
           hide_duplicate_count = false;
-          show_indicators = "yes";
-          enable_recursive_icon_lookup = true;
+          show_indicators = true;
+          # enable_recursive_icon_lookup = true;
           # icon_theme = "Papirus-Dark,Adwaita";
-          icon_position = "left";
-          min_icon_size = 32;
-          max_icon_size = 128;
-          sticky_history = "yes";
+          # icon_position = "left";
+          # min_icon_size = 32;
+          # max_icon_size = 128;
+
+          sticky_history = true;
           history_length = 20;
-          dmenu = "dmenu -p dunst:";
+          dmenu = "${rofi} -dmenu -p dunst";
           browser = "xdg-open";
           alway_run_script = true;
           title = "Dunst";
           class = "Dunst";
-          corner_radius = 10;
-          ignore_dbusclose = false;
-          force_xwayland = false;
           force_xinerama = false;
+          corner_radius = 10;
+          corners = "all";
           mouse_left_click = "close_current";
           mouse_middle_click = "do_action, close_current";
           mouse_right_click = "close_all";
+          ignore_dbusclose = false;
+          override_pause_level = 0;
         };
-
         urgency_low = {
-          background = "#37474f";
-          foreground = "#eceff1";
-          timeout = 10;
+          background = "${background-low}${opacity}";
+          foreground = "${text-color-low}";
+          timeout = 15;
         };
 
         urgency_normal = {
-          background = "#37474f";
-          foreground = "#eceff1";
-          timeout = 10;
+          background = "${background-default}${opacity}";
+          foreground = "${text-color}";
+          timeout = 15;
         };
 
         urgency_critical = {
-          background = "#37474f";
-          foreground = "#eceff1";
-          timeout = 10;
+          background = "${backgroud-high}${opacity}";
+          foreground = "${test-color-high}";
+          timeout = 20;
         };
 
       };
-      # setting.global.icon_path = "/usr/share/icons/gnome/16x16/status/:/usr/share/icons/gnome/16x16/devices/";
-      # iconTheme = {
-      #  name = "Papirus";
-      #  size = 32;
-      #  package = pkgs.papirus-icon-theme;
-      # };
-      # Allows using a mutable configuration file generated from the immutable one, useful in scenarios where live reloading is desired.
-      configFile = "$XDG_CONFIG_HOME/dunst/dunstrc";
+      iconTheme = {
+       name = "Papirus";
+       size = "32x32";
+       package = pkgs.papirus-icon-theme;
+      };
+      configFile = "${config.xdg.configHome}/dunst/dunstrc";
       # Set the service's WAYLAND_DISPLAY environment variable.
       # waylandDisplay = "";
 
