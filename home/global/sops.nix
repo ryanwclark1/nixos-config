275fa1b7b42cbbcc14ed0961f5f0f920b0aabcba
Ryan Clark <ryanc@accentservices.com>
2024-09-17 15:08:40 -0500

    chore: Update sops-nix configuration and remove unused files

--- home/global/sops.nix
+++ home/global/sops.nix
@@ -8,17 +8,23 @@
     inputs.sops-nix.homeManagerModules.sops
   ];
 
+  # sops-nix options: https://dl.thalheim.io/
   sops = {
-    age.keyFile = "/home/administrator/.config/sops/age/keys.txt";
-
-    defaultSopsFile = ../../secrets.yaml;
-    validateSopsFiles = false;
-
+    defaultSopsFile = ../../secrets/secrets.yaml;
+    validateSopsFiles = true;
+    # generate a new key if none is found
+    generateKey = true;
+    age = {
+      # automatically import host SSH keys as age keys
+      sshKeyPaths = [
+        "/etc/ssh/ssh_host_ed25519_key"
+        "/home/administrator/.ssh/ssh_host_ed25519_key"
+      ];
+      keyFile = "$HOME/.config/sops/age/keys.txt";
+    };
     secrets = {
       administrator-password = {};
-      "private_keys/administrator" = {
-        path = "/home/administrator/.ssh/id_demo";
-      };
+      "private_keys/administrator" = {};
     };
   };
 }
