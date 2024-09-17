03c5709b6607e13393904de054e24188f67d9e9a
Ryan Clark <ryanc@accentservices.com>
2024-09-17 16:31:34 -0500

    chore: Update frametop services configuration

--- hosts/frametop/services/default.nix
+++ hosts/frametop/services/default.nix
@@ -7,34 +7,19 @@
 {
   imports = [
     inputs.vscode-server.nixosModules.default
+    ./auto-cpufreq.nix
+    ./fprintd.nix
+    ./framework-firmware.nix
+    ./libinput.nix
+    ./logind.nix
+    ./upower.nix
   ];
 
   services = {
-    fwupd = {
-      enable = true;
-      package = pkgs.fwupd;
-    };
-    fprintd = {
-      enable = true;
-    };
-    logind = {
-      lidSwitch = "suspend";
-      lidSwitchExternalPower = "lock";
-    };
     vscode-server = {
       enable = true;
     };
-    libinput = {
-      enable = true;
-      touchpad = {
-        naturalScrolling = false;
-        scrollMethod = "twofinger";
-      };
-    };
-};
+  };
 
-  # Issues with kde powerdevil
-  powerManagement.powertop.enable = true;
 
-  security.pam.services.login.fprintAuth = true;
 }
