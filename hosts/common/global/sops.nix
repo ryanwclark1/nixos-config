91558587eeffeeaf87b5ffe19057ac2ef60efff4
Ryan Clark <ryanc@accentservices.com>
2024-09-17 15:09:01 -0500

    chore: Update sops-nix configuration and remove unused files

--- hosts/common/global/sops.nix
+++ hosts/common/global/sops.nix
@@ -18,23 +18,20 @@ in
 
   # sops-nix options: https://dl.thalheim.io/
   sops = {
-
     defaultSopsFile = "${secretsFile}";
-    # defaultSopsFile = ../../../secrets.yaml;
-    validateSopsFiles = false;
-
+    # defaultSopsFile = ../../../secrets/secrets.yaml;
+    validateSopsFiles = true;
+    # generate a new key if none is found
+    generateKey = true;
     age = {
       # automatically import host SSH keys as age keys
-      # sshKeyPaths = [
-      #   "/etc/ssh/ssh_host_ed25519_key"
-      #   "/home/administrator/ssh_host_ed25519_key"
-      # ];
-      sshKeyPaths = map getKeyPath keys;
+      sshKeyPaths = [
+        "/etc/ssh/ssh_host_ed25519_key"
+        "/home/administrator/.ssh/ssh_host_ed25519_key"
+      ];
+      # sshKeyPaths = map getKeyPath keys;
       # this will use an age key that is expected already in the fs.
       keyFile = "/var/lib/sops-nix/keys.txt";
-      # keyFile = "$HOME/.config/sops/age/keys.txt";
-      # generate a new key if none is found
-      generateKey = true;
     };
 
   };
