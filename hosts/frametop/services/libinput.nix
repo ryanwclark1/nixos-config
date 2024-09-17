d46aae4f657f1ca038ab9ec1b93c29b1967357e8
Ryan Clark <ryanc@accentservices.com>
2024-09-17 16:32:01 -0500

    feat: Add libinput service configuration for frametop

---
+++ hosts/frametop/services/libinput.nix
@@ -0,0 +1,22 @@
+{
+  ...
+}:
+
+{
+  services = {
+    libinput = {
+      enable = true;
+      touchpad = {
+        accelProfile = "adaptive";
+        disableWhileTyping = false;
+        horizontalScrolling = true;
+        leftHanded = false;
+        middleEmulation = true;
+        naturalScrolling = false;
+        scrollMethod = "twofinger";
+        sendEventsMode = "disabled-on-external-mouse";
+        tapping = true;
+      };
+    };
+  };
+}
