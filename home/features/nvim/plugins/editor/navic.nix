0573c30fdf0be40e35a4e81a12283adfe26958d3
Ryan Clark <ryanc@accentservices.com>
2024-09-17 09:25:10 -0500

    Navic, restructured to settings.

--- home/features/nvim/plugins/editor/navic.nix
+++ home/features/nvim/plugins/editor/navic.nix
@@ -1,43 +1,45 @@
 _: {
   programs.nixvim.plugins.navic = {
     enable = true;
-    click = false;
-    depthLimit = 5;
-    depthLimitIndicator = "..";
-    highlight = true;
-    lazyUpdateContext = false;
-    safeOutput = true;
-    separator = "  ";
+    settings = {
+      click = false;
+      depthLimit = 5;
+      lazyUpdateContext = false;
+      depthLimitIndicator = "..";
+      safeOutput = true;
+      separator = "  ";
+      highlight = true;
+      icons = {
+        Array = "󰅪 ";
+        Boolean = "  ";
+        Class = "  ";
+        Constant = "  ";
+        Constructor = "  ";
+        Enum = " ";
+        EnumMember = " ";
+        Event = " ";
+        Field = "󰽏 ";
+        File = "󰈙 ";
+        Function = "󰊕 ";
+        Interface = " ";
+        Key = "  ";
+        Method = " ";
+        Module = "󰕳 ";
+        Namespace = " ";
+        Null = "󰟢 ";
+        Number = "󰎠 ";
+        Object = "  ";
+        Operator = "󰆕 ";
+        Package = " ";
+        Property = " ";
+        String = "󰀬 ";
+        Struct = " ";
+        TypeParameter = "󰊄 ";
+        Variable = "󰆧 ";
+      };
+    };
     lsp = {
       autoAttach = true;
-    };
-    icons = {
-      Array = "󰅪 ";
-      Boolean = "  ";
-      Class = "  ";
-      Constant = "  ";
-      Constructor = "  ";
-      Enum = " ";
-      EnumMember = " ";
-      Event = " ";
-      Field = "󰽏 ";
-      File = "󰈙 ";
-      Function = "󰊕 ";
-      Interface = " ";
-      Key = "  ";
-      Method = " ";
-      Module = "󰕳 ";
-      Namespace = " ";
-      Null = "󰟢 ";
-      Number = "󰎠 ";
-      Object = "  ";
-      Operator = "󰆕 ";
-      Package = " ";
-      Property = " ";
-      String = "󰀬 ";
-      Struct = " ";
-      TypeParameter = "󰊄 ";
-      Variable = "󰆧 ";
     };
   };
 }
