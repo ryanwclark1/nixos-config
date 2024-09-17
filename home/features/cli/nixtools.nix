645046e60c2e39b829c3392a90ecc4371aca3d0c
Ryan Clark <ryanc@accentservices.com>
2024-09-17 13:22:03 -0500

    Nixtools updated fetch index database

--- home/features/cli/nixtools.nix
+++ home/features/cli/nixtools.nix
@@ -7,9 +7,9 @@ let
     name = "fetch-nix-index-database";
     runtimeInputs = with pkgs; [ wget coreutils ];
     text = ''
-      filename="index-$(uname -m | sed 's/^arm64$/aarch64/')-$(uname | tr A-Z a-z)"
+      filename="index-$(uname -m | sed 's/^arm64$/aarch64/')-$(uname | tr '[:upper:]' '[:lower:]')"
       mkdir -p ~/.cache/nix-index && cd ~/.cache/nix-index
-      wget -q -N https://github.com/nix-community/nix-index-database/releases/latest/download/$filename
+      wget -q -N "https://github.com/nix-community/nix-index-database/releases/latest/download/$filename"
       ln -f "$filename" files
     '';
   };
