aaedffa846e64e396bb805adcb5466aa9213d3fb
Ryan Clark <ryanc@accentservices.com>
2024-09-17 16:32:26 -0500

    feat: Configure logind service for frametop

---
+++ hosts/frametop/services/logind.nix
@@ -0,0 +1,19 @@
+{
+  ...
+}:
+
+{
+  services = {
+    logind = {
+      hibernateKey = "suspend-then-hibernate";
+      hibernateKeyLongPress = "hibernate";
+      lidSwitch = "suspend-then-hibernate";
+      lidSwitchDocked = "ignore";
+      lidSwitchExternalPower = "lock";
+      powerKey = "poweroff";
+      powerKeyLongPress = "reboot";
+      suspendKey = "suspend";
+      suspendKeyLongPress = "suspend-then-hibernate"
+    };
+  };
+}
