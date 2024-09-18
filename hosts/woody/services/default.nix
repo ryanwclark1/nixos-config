73fb9328268946fae523c0049cd6924d946611b9
Ryan Clark <ryanc@accentservices.com>
2024-09-17 21:33:30 -0500

    feat: Add spice-vdagentd and vscode-server services for woody host

--- hosts/woody/services/default.nix
+++ hosts/woody/services/default.nix
@@ -5,15 +5,11 @@
 
 {
   imports = [
-    inputs.vscode-server.nixosModules.default
+    ./spice.nix
+    ./vscode-server.nix
   ];
 
   services = {
-    vscode-server = {
-      enable = true;
-    };
-    # Allows for copy/paste between host and guest.
-    spice-vdagentd.enable = true;
     xserver.videoDrivers = [ "amdgpu" ];
   };
 }
