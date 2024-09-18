feed5f8b0b5206c77cd4f2f51946d1e5eede09c7
Ryan Clark <ryanc@accentservices.com>
2024-09-18 09:07:23 -0500

    feat: Add fwupd and xserver services for woody host

--- hosts/woody/services/spice.nix
+++ hosts/woody/services/spice.nix
@@ -5,11 +5,16 @@
 
 {
   services = {
+    # Enable SPICE guest agent functionalities (clipboard sharing, resolution resizing, etc.)
     spice-vdagentd.enable = true;
+
+    # Enable WebDAV support over SPICE for file sharing between host and guest
     spice-webdavd = {
       enable = true;
-      package = pkgs.spice-webdavd;
+      package = pkgs.pkgs.phodav;
     };
+
+    # Enable automatic display configuration for SPICE sessions
     spice-autorandr = {
       enable = true;
       package = pkgs.spice-autorandr;
