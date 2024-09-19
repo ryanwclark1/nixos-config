0063e1a7af93ed9d9908635592657b9eb37fd1ee
Ryan Clark <ryanc@accentservices.com>
2024-09-19 16:34:44 -0500

    whitespace cleanup

--- home/features/desktop/hyprland/rofi/default.nix
+++ home/features/desktop/hyprland/rofi/default.nix
@@ -12,158 +12,158 @@
   ];
 
   home =
-  let
-    cliphist = lib.getExe pkgs.cliphist;
-    rofi = lib.getExe pkgs.rofi-wayland;
-    # style_dir = "${config.home.homeDirectory}/.config/rofi/style";
-  in
-  {
-    file.".config/rofi" = {
-      source = ./config;
-      recursive = true;
-    };
-    file.".config/rofi/scripts/cliphist-delete.sh" = {
-      text = ''
-        #!/usr/bin/env bash
-        sleep 0.1 && ${cliphist} list | ${rofi} -dmenu -theme ${config.home.homeDirectory}/.config/rofi/style/cliphist | ${cliphist} delete
+    let
+      cliphist = lib.getExe pkgs.cliphist;
+      rofi = lib.getExe pkgs.rofi-wayland;
+      # style_dir = "${config.home.homeDirectory}/.config/rofi/style";
+    in
+    {
+      file.".config/rofi" = {
+        source = ./config;
+        recursive = true;
+      };
+      file.".config/rofi/scripts/cliphist-delete.sh" = {
+        text = ''
+          #!/usr/bin/env bash
+          sleep 0.1 && ${cliphist} list | ${rofi} -dmenu -theme ${config.home.homeDirectory}/.config/rofi/style/cliphist | ${cliphist} delete
+          '';
+        executable = true;
+      };
+      file.".config/rofi/scripts/cliphist-copy.sh" = {
+        text = ''
+          #!/usr/bin/env bash
+          sleep 0.1 && ${cliphist} list | ${rofi} -dmenu -theme ${config.home.homeDirectory}/.config/rofi/style/cliphist | ${cliphist} delete
         '';
-      executable = true;
-    };
-    file.".config/rofi/scripts/cliphist-copy.sh" = {
-      text = ''
-        #!/usr/bin/env bash
-        sleep 0.1 && ${cliphist} list | ${rofi} -dmenu -theme ${config.home.homeDirectory}/.config/rofi/style/cliphist | ${cliphist} delete
-      '';
-      executable = true;
-    };
-    file.".config/rofi/scripts/applauncher-fullscreen.sh" = {
-      text = ''
-        #!/usr/bin/env bash
+        executable = true;
+      };
+      file.".config/rofi/scripts/applauncher-fullscreen.sh" = {
+        text = ''
+          #!/usr/bin/env bash
 
-        dir="${config.home.homeDirectory}/.config/rofi/style"
-        theme='launcher-full'
+          dir="${config.home.homeDirectory}/.config/rofi/style"
+          theme='launcher-full'
 
-        rofi_cmd() {
-          rofi -show drun \
-            -theme "$dir/$theme.rasi"
-        }
+          rofi_cmd() {
+            rofi -show drun \
+              -theme "$dir/$theme.rasi"
+          }
 
-        rofi_cmd
-      '';
-      executable = true;
-    };
-    # TODO: Fix lastlogin/uptime.  Why host?  Suspend function
-    file.".config/rofi/scripts/power-big.sh" = {
-      text = ''
-        #!/usr/bin/env bash
+          rofi_cmd
+        '';
+        executable = true;
+      };
+      # TODO: Fix lastlogin/uptime.  Why host?  Suspend function
+      file.".config/rofi/scripts/power-big.sh" = {
+        text = ''
+          #!/usr/bin/env bash
 
-        # Current Theme
-        dir="${config.home.homeDirectory}/.config/rofi/style"
-        theme='power-big'
-        background="$(hyprctl hyprpaper listloaded)"
+          # Current Theme
+          dir="${config.home.homeDirectory}/.config/rofi/style"
+          theme='power-big'
+          background="$(hyprctl hyprpaper listloaded)"
 
-        # CMDs
-        lastlogin="$(last -n 1 "$USER" | tr -s ' ' | cut -d' ' -f5-7)"
-        uptime="$(awk -F '( |,)' '{print $6, $7, $8}' <(uptime))"
-        host="$(hostname)"
+          # CMDs
+          lastlogin="$(last -n 1 "$USER" | tr -s ' ' | cut -d' ' -f5-7)"
+          uptime="$(awk -F '( |,)' '{print $6, $7, $8}' <(uptime))"
+          host="$(hostname)"
 
-        # Options
-        shutdown=' '
-        reboot=''
-        lock=''
-        suspend='󰏦'
-        logout='󰍃'
-        yes=' '
-        no=' '
+          # Options
+          shutdown=' '
+          reboot=''
+          lock=''
+          suspend='󰏦'
+          logout='󰍃'
+          yes=' '
+          no=' '
 
-        # Rofi CMD
-        rofi_cmd() {
-          rofi -dmenu \
-            -p "Goodbye $USER" \
-            -mesg "Uptime: $uptime" \
-            -theme "$dir/$theme.rasi"
-        }
+          # Rofi CMD
+          rofi_cmd() {
+            rofi -dmenu \
+              -p "Goodbye $USER" \
+              -mesg "Uptime: $uptime" \
+              -theme "$dir/$theme.rasi"
+          }
 
-        # Confirmation CMD
-        confirm_cmd() {
-          rofi -dmenu \
-            -p 'Confirmation' \
-            -mesg 'Are You Sure?' \
-            -theme "$dir/shared/confirm-big.rasi"
-        }
+          # Confirmation CMD
+          confirm_cmd() {
+            rofi -dmenu \
+              -p 'Confirmation' \
+              -mesg 'Are You Sure?' \
+              -theme "$dir/shared/confirm-big.rasi"
+          }
 
-        # Ask for confirmation
-        confirm_exit() {
-          echo -e "$yes\n$no" | confirm_cmd
-        }
+          # Ask for confirmation
+          confirm_exit() {
+            echo -e "$yes\n$no" | confirm_cmd
+          }
 
-        # Pass variables to rofi dmenu
-        run_rofi() {
-          echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
-        }
+          # Pass variables to rofi dmenu
+          run_rofi() {
+            echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+          }
 
-        # Execute Command
-        run_cmd() {
-          selected="$(confirm_exit)"
-          if [[ "$selected" == "$yes" ]]; then
-            case $1 in
-              '--shutdown')
-                systemctl poweroff
-                ;;
-              '--reboot')
-                systemctl reboot
-                ;;
-              '--suspend')
-                mpc -q pause
-                amixer set Master mute
-                systemctl suspend
-                ;;
-              '--logout')
-                case "$DESKTOP_SESSION" in
-                  hyprland)
-                    hyprctl dispatch exit
-                    ;;
-                  plasma)
-                    qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-                    ;;
-                  *)
-                    echo "Unsupported session: $DESKTOP_SESSION"
-                    ;;
-                esac
-                ;;
-            esac
-          else
-            exit 0
-          fi
-        }
-
-        # Actions
-        chosen="$(run_rofi)"
-        case $chosen in
-          $shutdown)
-            run_cmd --shutdown
-            ;;
-          $reboot)
-            run_cmd --reboot
-            ;;
-          $lock)
-            if command -v hyprlock &> /dev/null; then
-              hyprlock
+          # Execute Command
+          run_cmd() {
+            selected="$(confirm_exit)"
+            if [[ "$selected" == "$yes" ]]; then
+              case $1 in
+                '--shutdown')
+                  systemctl poweroff
+                  ;;
+                '--reboot')
+                  systemctl reboot
+                  ;;
+                '--suspend')
+                  mpc -q pause
+                  amixer set Master mute
+                  systemctl suspend
+                  ;;
+                '--logout')
+                  case "$DESKTOP_SESSION" in
+                    hyprland)
+                      hyprctl dispatch exit
+                      ;;
+                    plasma)
+                      qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+                      ;;
+                    *)
+                      echo "Unsupported session: $DESKTOP_SESSION"
+                      ;;
+                  esac
+                  ;;
+              esac
             else
-              echo "hyprlock does not exist."
+              exit 0
             fi
-            ;;
-          $suspend)
-            run_cmd --suspend
-            ;;
-          $logout)
-            run_cmd --logout
-            ;;
-        esac
-      '';
-      executable = true;
+          }
+
+          # Actions
+          chosen="$(run_rofi)"
+          case $chosen in
+            $shutdown)
+              run_cmd --shutdown
+              ;;
+            $reboot)
+              run_cmd --reboot
+              ;;
+            $lock)
+              if command -v hyprlock &> /dev/null; then
+                hyprlock
+              else
+                echo "hyprlock does not exist."
+              fi
+              ;;
+            $suspend)
+              run_cmd --suspend
+              ;;
+            $logout)
+              run_cmd --logout
+              ;;
+          esac
+        '';
+        executable = true;
+      };
     };
-  };
 
   # home.file = {
 
