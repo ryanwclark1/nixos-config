fe06a79d4a4603d33f14518673a16876bc852485
Ryan Clark <ryanc@accentservices.com>
2024-09-19 16:00:01 -0500

    Update fastfetch logo position and padding

--- home/features/fastfetch/default.nix
+++ home/features/fastfetch/default.nix
@@ -10,193 +10,151 @@
       package = pkgs.fastfetch;
       settings = {
         logo = {
+          position = "left";
           padding = {
-            top = 2;
-            width = 22;
-            height = 11;
+            top = 1;
+            right = 4;
+            left = 0;
           };
+          width = 22;
+          height = 11;
         };
         modules = [
           "break"
           {
-              type = "custom";
-              format = "\u001b[90m┌──────────────────────Hardware──────────────────────┐";
+            type = "custom";
+            format = "┌─────────────────────Hardware─────────────────────┐";
+          }
+          # {
+            # type = "command";
+          #   shell = "/bin/sh";
+          #   test = "echo $HOSTNAME";
+          #   key = " PC";
+          #   keyColor = "green";
+
+          # }
+          {
+            type = "title";
+            key = "  PC";
+            keyColor = "green";
+            format = "{host-name}";
           }
           {
-              type = "host";
-              key = " PC";
-              keyColor = "green";
+            type = "cpu";
+            key = "│ ├ ";
+            "showPeCoreCount" = true;
+            keyColor = "green";
           }
           {
-              type = "cpu";
-              key = "│ ├";
-              "showPeCoreCount" = true;
-              keyColor = "green";
+            type = "gpu";
+            key = "│ ├󰢮 ";
+            keyColor = "green";
           }
           {
-              type = "gpu";
-              key = "│ ├󰍛";
-              keyColor = "green";
+            type = "memory";
+            key = "│ ├󰑭 ";
+            keyColor = "green";
           }
           {
-              type = "memory";
-              key = "│ ├󰍛";
-              keyColor = "green";
+            type = "disk";
+            key = "│ ├󰋊 ";
+            keyColor = "green";
           }
           {
-              type = "disk";
-              key = "└ └";
-              keyColor = "green";
+            type = "display";
+            key = "└ └󰍹 ";
+            keyColor = "green";
           }
           {
-              type = "custom";
-              format = "\u001b[90m└────────────────────────────────────────────────────┘";
+            type = "custom";
+            format = "└──────────────────────────────────────────────────┘";
           }
           "break"
           {
-              type = "custom";
-              format = "\u001b[90m┌──────────────────────Software──────────────────────┐";
+            type = "custom";
+            format = "┌─────────────────────Software─────────────────────┐";
           }
           {
-              type = "os";
-              key = " OS";
-              keyColor = "yellow";
+            type = "os";
+            key = " OS";
+            keyColor = "yellow";
           }
           {
-              type = "kernel";
-              key = "│ ├";
-              keyColor = "yellow";
+            type = "kernel";
+            key = "│ ├ ";
+            keyColor = "yellow";
           }
           {
-              type = "packages";
-              key = "│ ├󰏖";
-              keyColor = "yellow";
+            type = "packages";
+            key = "│ ├󰏖 ";
+            keyColor = "yellow";
           }
           {
-              type = "shell";
-              key = "└ └";
-              keyColor = "yellow";
+            type = "shell";
+            key = "│ ├ ";
+            keyColor = "yellow";
           }
+          {
+            type = "terminal";
+            key = "└ └ ";
+            keyColor = "yellow";
+          }
+          {
+            type = "custom";
+            format = "└──────────────────────────────────────────────────┘";
+          }
+          # "break"
+          # {
+          #   type = "custom";
+          #   format = "┌────────────────────Uptime/Age────────────────────┐";
+          # }
+          # {
+          #   type = "command";
+          #   key = " 󱦟 OS Age ";
+          #   keyColor = "magenta";
+          #   text = "birth_install=$(stat -c %W /); current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days";
+          # }
+          # {
+          #   type = "uptime";
+          #   key = " 󱫐 Uptime ";
+          #   keyColor = "magenta";
+          # }
+          # {
+          #   type = "custom";
+          #   format = "└──────────────────────────────────────────────────┘";
+          # }
           "break"
           {
-              type = "de";
-              key = " DE";
-              keyColor = "blue";
+            type = "custom";
+            format = "┌─────────────────────Network─────────────────────┐";
           }
           {
-              type = "lm";
-              key = "│ ├";
-              keyColor = "blue";
+            type = "localip";
+            key = "󰩟 IP";
+            showIpv6 = false;
+            showMac = false;
+            keyColor = "red";
+            format = "{ifname}";
           }
           {
-              type = "wm";
-              key = "│ ├";
-              keyColor = "blue";
+            type = "localip";
+            key = "│ ├ ";
+            showIpv6 = false;
+            showMac = false;
+            keyColor = "red";
+            format = "{ipv4} {ifname}";
           }
           {
-              type = "wmtheme";
-              key = "│ ├󰉼";
-              keyColor = "blue";
+            type = "publicip";
+            timeout = 1000;
+            key = "└ └ ";
+            keyColor = "red";
           }
           {
-              type = "gpu";
-              key = "└ └󰍛";
-              format = "{3}";
-              keyColor = "blue";
+            type = "custom";
+            format = "└────────────────────────────────────────────────┘";
           }
-          {
-              type = "custom";
-              format = "\u001b[90m└────────────────────────────────────────────────────┘";
-          }
-          "break"
-          {
-              type = "custom";
-              format = "\u001b[90m┌────────────────────Uptime / Age────────────────────┐";
-          }
-          {
-              type = "command";
-              key = "  OS Age ";
-              keyColor = "magenta";
-              text = "birth_install=$(stat -c %W /); current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days";
-          }
-          {
-              type = "uptime";
-              key = "  Uptime ";
-              keyColor = "magenta";
-          }
-          {
-              type = "custom";
-              format = "\u001b[90m└────────────────────────────────────────────────────┘";
-          }
-          "break"
         ];
-        # modules= [
-        #   "title"
-        #   "separator"
-        #   "os"
-        #   "host"
-        #   "bios"
-        #   "bootmgr"
-        #   "board"
-        #   "chassis"
-        #   "kernel"
-        #   "initsystem"
-        #   "uptime"
-        #   "processes"
-        #   "packages"
-        #   "shell"
-        #   "editor"
-        #   "display"
-        #   "brightness"
-        #   "monitor"
-        #   "terminal"
-        #   {
-        #     type = "cpu";
-        #     showPeCoreCount = true;
-        #     temp = true;
-        #   }
-        #   "cpuusage"
-        #   {
-        #     type = "gpu";
-        #     driverSpecific = true;
-        #     temp = true;
-        #   }
-        #   "memory"
-        #   "physicalmemory"
-        #   "swap"
-        #   "disk"
-        #   "battery"
-        #   "poweradapter"
-        #   "player"
-        #   "media"
-        #   {
-        #     type = "publicip";
-        #     timeout = 1000;
-        #   }
-        #   {
-        #     type = "localip";
-        #     showIpv6 = true;
-        #     showMac = true;
-        #   }
-        #   "dns"
-        #   "wifi"
-        #   "datetime"
-        #   "locale"
-        #   "vulkan"
-        #   "opengl"
-        #   "opencl"
-        #   "users"
-        #   "bluetooth"
-        #   "bluetoothradio"
-        #   "sound"
-        #   "diskio"
-        #   {
-        #     type = "physicaldisk";
-        #     temp = true;
-        #   }
-        #   "break"
-        #   "colors"
-        # ];
       };
     };
   };
