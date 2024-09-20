ba8054c8508fddd497fb4ea05df19cb5244c8081
Ryan Clark <ryanc@accentservices.com>
2024-09-19 19:06:23 -0500

    wlogout update

--- home/features/desktop/hyprland/wlogout/default.nix
+++ home/features/desktop/hyprland/wlogout/default.nix
@@ -1,16 +1,25 @@
 {
   pkgs,
+  config,
   ...
 }:
 
+
+
+
 {
+  home.file.".config/wlogout/icons" = {
+    source = ./icons;
+    recursive = true;
+  };
+
   programs.wlogout = {
     enable = true;
     package = pkgs.wlogout;
     layout = [
       {
         label = "lock";
-        action = "hyprlock";
+        action = "sleep 1; hyprlock";
         text = "Lock";
         keybind = "l";
       }
@@ -22,87 +31,85 @@
       }
       {
         label = "logout";
-        action = "killall -9 Hyprland";
+        action = "sleep 1; hyprctl dispatch exit";
         text = "Exit";
         keybind = "e";
       }
       {
         label = "shutdown";
-        action = "systemctl poweroff";
+        action = "sleep 1; systemctl poweroff";
         text = "Shutdown";
         keybind = "s";
       }
       {
         label = "suspend";
-        action = "systemctl suspend ";
+        action = "sleep 1; systemctl suspend ";
         text = "Suspend";
         keybind = "u";
       }
       {
         label = "reboot";
-        action = "systemctl reboot";
+        action = "sleep 1; systemctl reboot";
         text = "Reboot";
         keybind = "r";
       }
     ];
     style = ''
+       * {
+        font-family: "JetBrainsMono NF", FontAwesome, sans-serif;
+      	background-image: none;
+      	transition: 20ms;
+      }
+      window {
+      	background-color: rgba(12, 12, 12, 0.1);
+      }
       button {
-        color: #FFFFFF;
+      	color: #${config.stylix.base16Scheme.base05};
         font-size:20px;
-
         background-repeat: no-repeat;
-          background-position: center;
-          background-size: 25%;
-
-          border-style: solid;
-          background-color: rgba(12, 12, 12, 0.3);
-          border: 3px solid #FFFFFF;
-
+      	background-position: center;
+      	background-size: 25%;
+      	border-style: solid;
+      	background-color: rgba(12, 12, 12, 0.3);
+      	border: 3px solid #${config.stylix.base16Scheme.base05};
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-        }
-
-        button:focus,
-        button:active,
-        button:hover {
-          color: @color11;
-          background-color: rgba(12, 12, 12, 0.5);
-          border: 3px solid @color11;
       }
-
-      /*
-      -----------------------------------------------------
-      Buttons
-      -----------------------------------------------------
-      */
-
-      #lock {
-        margin: 10px;
-        border-radius: 20px;
+      button:focus,
+      button:active,
+      button:hover {
+        color: #${config.stylix.base16Scheme.base0B};
+        background-color: rgba(12, 12, 12, 0.5);
+        border: 3px solid #${config.stylix.base16Scheme.base0B};
       }
-
       #logout {
-        margin: 10px;
-        border-radius: 20px;
+      	margin: 10px;
+      	border-radius: 20px;
+      	background-image: image(url("icons/logout.png"));
       }
-
       #suspend {
-        margin: 10px;
-        border-radius: 20px;
+      	margin: 10px;
+      	border-radius: 20px;
+      	background-image: image(url("icons/suspend.png"));
       }
-
-      #hibernate {
-        margin: 10px;
-        border-radius: 20px;
-      }
-
       #shutdown {
-        margin: 10px;
-        border-radius: 20px;
+      	margin: 10px;
+      	border-radius: 20px;
+      	background-image: image(url("icons/shutdown.png"));
       }
-
       #reboot {
-        margin: 10px;
-        border-radius: 20px;
+      	margin: 10px;
+      	border-radius: 20px;
+      	background-image: image(url("icons/reboot.png"));
+      }
+      #lock {
+      	margin: 10px;
+      	border-radius: 20px;
+      	background-image: image(url("icons/lock.png"));
+      }
+      #hibernate {
+      	margin: 10px;
+      	border-radius: 20px;
+      	background-image: image(url("icons/hibernate.png"));
       }
     '';
   };
