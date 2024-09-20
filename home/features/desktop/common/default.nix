9953ec664ad989c1f8436ffd810cef27e8dba523
Ryan Clark <ryanc@accentservices.com>
2024-09-20 12:31:07 -0500

    Update default.nix with new package dependencies

--- home/features/desktop/common/default.nix
+++ home/features/desktop/common/default.nix
@@ -13,19 +13,20 @@
   home.packages = with pkgs; [
     d-spy # Dbus debugger
     ventoy-full #balena type tool
-    libnotify
-    wl-clipboard
-    grim
-    mimeo
+    libnotify # Notification library
+    wl-clipboard # Wayland clipboard
+    grim # Screenshot tool,
+    mimeo # Open files with the right program
     slurp # Screenshot tool, select area
-    mediainfo
+    mediainfo # Media info
     bluez-tools # bt-adapter
     czkawka # Duplicate file finder
-    gpu-viewer
+    gpu-viewer # GPU info
     waypipe # Network proxy for Wayland clients (applications)
     wf-recorder # Utility program for screen recording of wlroots-based compositors
     wl-mirror # Simple Wayland output mirror client
-    ydotool # Command-line tool for emulating input devices
+    ydotool # Command-line tool for automation which emulates input devices
+    file-roller # Archive manager
   ];
 
 }
