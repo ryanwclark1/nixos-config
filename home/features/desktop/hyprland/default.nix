77b5b5f1125452aad655a4ce53f717c8bfa96b64
Ryan Clark <ryanc@accentservices.com>
2024-09-20 12:31:14 -0500

    Update default.nix with new package dependencies and add file-chooser settings

--- home/features/desktop/hyprland/default.nix
+++ home/features/desktop/hyprland/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   pkgs,
   ...
 }:
@@ -23,17 +24,33 @@
     file."Pictures/wallpapers" = {
       source = ../../../../hosts/common/wallpaper;
       recursive = true;
+
+    file.".config/swappy/config".text = ''
+      [Default]
+      save_dir="${config.home.homeDirectory}/Pictures/Screenshots"
+      save_filename_format=swappy-%Y%m%d-%H%M%S.png
+      show_panel=false
+      line_size=5
+      text_size=20
+      text_font=Ubuntu
+      paint_mode=brush
+      early_exit=true
+      fill_shape=false
+    '';
     };
 
     packages = with pkgs; [
-      wf-recorder
-      wl-clipboard
-      grim
-      slurp
+      wf-recorder # Utility program for screen recording of wlroots-based compositors
+      wl-clipboard # Wayland clipboard
+      grim # Screenshot tool,
+      slurp # Screenshot tool, select area
       grimblast
       hyprpicker
-      swww
-      yad
+      swww # Sway window switcher
+      yad # Yet another dialog
+      brightnessctl # Adjust screen brightness
+      swappy # Wayland native snapshot editing tool, inspired by Snappy on macOS
     ];
   };
 }
+
