d6d557ad69f8e8be2e6c43de29af5b2a91c89d41
Ryan Clark <ryanc@accentservices.com>
2024-09-21 16:38:48 -0500

    Refactor music.nix: Comment out unused packages and enable Spotify

--- home/features/media/music.nix
+++ home/features/media/music.nix
@@ -4,26 +4,14 @@
 }:
 
 {
-  services.mpd = {
-    enable = false;
-    musicDirectory = ''$HOME/Music'';
-  };
 
-  services.fluidsynth = {
-    enable = true;
-    soundService = "pipewire-pulse";
-    extraOptions = [
-      "-g 2"
-    ];
-  };
-
-  programs.ncmpcpp = {
-    package = pkgs.ncmpcpp.override { visualizerSupport = true; };
-    enable = false;
-  };
+  # programs.ncmpcpp = {
+  #   package = pkgs.ncmpcpp.override { visualizerSupport = true; };
+  #   enable = false;
+  # };
 
   home.packages = with pkgs; [
     termusic
-    alsa-utils
+    spotify
   ];
 }
