ddcccdfeb54f10ceb53c018584cfba8662b1bf4b
Ryan Clark <ryanc@accentservices.com>
2024-09-20 19:53:21 -0500

    Refactor wlogout style: Update font sizes and transitions

--- home/features/desktop/hyprland/wlogout/default.nix
+++ home/features/desktop/hyprland/wlogout/default.nix
@@ -1,11 +1,11 @@
 {
   pkgs,
   config,
+
   ...
 }:
 
-
-
+with config.lib.stylix.colors.withHashtag;
 
 {
   home.file.".config/wlogout/icons" = {
@@ -55,7 +55,7 @@
       }
     ];
     style = ''
-       * {
+      * {
         font-family: "JetBrainsMono NF", FontAwesome, sans-serif;
       	background-image: none;
       	transition: 20ms;
@@ -64,22 +64,22 @@
       	background-color: rgba(12, 12, 12, 0.1);
       }
       button {
-      	color: #${config.stylix.base16Scheme.base05};
+      	color: #${base05};
         font-size:20px;
         background-repeat: no-repeat;
       	background-position: center;
       	background-size: 25%;
       	border-style: solid;
       	background-color: rgba(12, 12, 12, 0.3);
-      	border: 3px solid #${config.stylix.base16Scheme.base05};
+      	border: 3px solid #${base05};
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
       }
       button:focus,
       button:active,
       button:hover {
-        color: #${config.stylix.base16Scheme.base0B};
+        color: #${base0B};
         background-color: rgba(12, 12, 12, 0.5);
-        border: 3px solid #${config.stylix.base16Scheme.base0B};
+        border: 3px solid #${base0B};
       }
       #logout {
       	margin: 10px;
