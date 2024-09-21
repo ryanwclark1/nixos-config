72ec76085cce039780c81c94bfef2813e2780e1a
Ryan Clark <ryanc@accentservices.com>
2024-09-21 13:28:08 -0500

    Refactor Firefox configuration: Enable loading tabs in background

--- home/features/firefox/default.nix
+++ home/features/firefox/default.nix
@@ -22,6 +22,7 @@
           "shell.checkDefaultBrowser" = false;
           "shell.defaultBrowserCheckCount" = 1;
           "startup.homepage" = "https://start.duckduckgo.com";
+          "tabs.loadInBackground" = true;
           "uiCustomization.state" = ''
             {"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","library-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","widget-overflow-fixed-list"],"currentVersion":18,"newElementCount":4}
           '';
