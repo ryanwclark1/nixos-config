eb061053b44442ba5a525dd71281d92d190de39a
Ryan Clark <ryanc@accentservices.com>
2024-09-20 19:53:39 -0500

    Refactor hyprland hypr scripts: Remove unused variables and update waybar style

--- home/features/desktop/hyprland/hypr/default.nix
+++ home/features/desktop/hyprland/hypr/default.nix
@@ -12,13 +12,10 @@
     # ./hyprbars.nix
   ];
 
-
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
     systemd.enable = true;
-
-
     settings =
     let
       waybar = lib.getExe pkgs.waybar;
@@ -27,12 +24,9 @@
       eww = lib.getExe config.programs.eww.package;
       wl-paste = "${pkgs.wl-clipboard}/bin/wl-paste";
       cliphist = lib.getExe config.services.cliphist.package;
-      thunar = "thunar";
       steam = "${pkgs.steam}/bin/steam";
-      dunst = lib.getExe config.services.dunst.package;
       swaync = "${pkgs.swaynotificationcenter}/bin/swaync";
       swww = "${pkgs.swww}/bin/swww";
-      nm-applet = "${pkgs.networkmanagerapplet}/bin/nm-applet";
     in
     {
       env = [
@@ -48,11 +42,12 @@
         "SDL_VIDEODRIVER, x11"
         "MOZ_ENABLE_WAYLAND, 1"
       ];
+
       monitor = [
         ",highres,auto,1"
       ];
-      "exec-once" =
-      [
+
+      "exec-once" =[
         "dbus-update-activation-environment --systemd --all"
         "systemctl --user import-environment QT_QPA_PLATFORMTHEME WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "${hypridle} &"
@@ -259,36 +254,21 @@
       };
 
       windowrule = [
+        "noborder, ^(${rofi})$"
         # "float, ^(${thunar})$"
         # "center, ^(${thunar})$"
         # "size 1080 900, ^(${thunar})$"
-        "float, ^(${steam})$"
-        "center, ^(${steam})$"
-        "size 1080 900, ^(${steam})$"
+        # "float, ^(${steam})$"
+        # "center, ^(${steam})$"
+        # "size 1080 900, ^(${steam})$"
       ];
 
-      # windowrulev2 = let
-      #   sweethome3d-tooltips = "title:^(win[0-9])$,class:^(com-eteks-sweethome3d-SweetHome3DBootstrap)$";
-      #   steam = "title:^()$,class:^(steam)$";
-      #   kdeconnect-pointer = "class:^(kdeconnect.daemon)$";
-      # in [
-      #   "nofocus, ${sweethome3d-tooltips}"
+      windowrulev2 = [
+        "stayfocused, title:^()$,class:^(${steam})$"
+        "minsize 1 1, title:^()$,class:^(${steam})$"
 
-      #   "stayfocused, ${steam}"
-      #   "minsize 1 1, ${steam}"
+      ];
 
-      #   "size 100% 110%, ${kdeconnect-pointer}"
-      #   "center, ${kdeconnect-pointer}"
-      #   "nofocus, ${kdeconnect-pointer}"
-      #   "noblur, ${kdeconnect-pointer}"
-      #   "noanim, ${kdeconnect-pointer}"
-      #   "noshadow, ${kdeconnect-pointer}"
-      #   "noborder, ${kdeconnect-pointer}"
-      #   "suppressevent fullscreen, ${kdeconnect-pointer}"
-      # ];
-      # ++ (lib.mapAttrsToList (name: colors:
-      #   "bordercolor ${rgba colors.primary "aa"} ${rgba colors.primary_container "aa"}, title:^(\\[${name}\\])"
-      # ) remoteColorschemes);
       layerrule = [
         "animation fade,hyprpicker"
         "animation fade,selection"
@@ -384,142 +364,6 @@
       #       "specialWorkspace,1,3,md3_decel,slidevert"
       #   ];
       # };
-
-      bind =
-      let
-        rofi = lib.getExe config.programs.rofi.package;
-        grimblast = lib.getExe pkgs.grimblast;
-        tesseract = lib.getExe pkgs.tesseract;
-        wpctl = "${pkgs.wireplumber}/bin/wpctl";
-        notify-send = lib.getExe' pkgs.libnotify "notify-send";
-        terminal = lib.getExe pkgs.alacritty;
-        files = "${pkgs.kdePackages.dolphin}/bin/dolphin";
-        defaultApp = type: "${lib.getExe pkgs.handlr-regex} launch ${type}";
-        hyprlock = lib.getExe pkgs.hyprlock;
-        browser = "${pkgs.google-chrome}/bin/google-chrome-stable";
-        remote = lib.getExe (pkgs.writeShellScriptBin "remote" ''
-          socket="$(basename "$(find ~/.ssh -name 'administrator@*' | head -1 | cut -d ':' -f1)")"
-          host="''${socket#master-}"
-          ssh "$host" "$@"
-        '');
-      in
-        [
-          # Program bindings
-          "SUPER,Return,exec,${terminal}"
-          "SUPERSHIFT,Return,exec,${rofi} -show drun"
-          "SUPERSHIFT,W,exec,${config.home.homeDirectory}/.config/hypr/scripts/web-search.sh"
-          "SUPER,W,exec,${browser}"
-          "SUPER,S,exec,${config.home.homeDirectory}/.config/hypr/scripts/screenshooting.sh"
-
-          "SUPER,e,exec,${defaultApp "text/plain"}"
-          "SUPER,b,exec,${defaultApp "x-scheme-handler/https"}"
-          # "$mod, Space, exec, $menu --show drun"
-          "SUPER ALT,space,exec,${files}"
-          # "SUPER ALT,Return,exec,${remote} ${defaultApp "x-scheme-handler/terminal"}"
-          # "SUPER ALT,e,exec,${remote} ${defaultApp "text/plain"}"
-          # "SUPER ALT,b,exec,${remote} ${defaultApp "x-scheme-handler/https"}"
-
-          # Brightness control (only works if the system has lightd)
-          ",XF86MonBrightnessUp,exec,light -A 10"
-          ",XF86MonBrightnessDown,exec,light -U 10"
-
-          # Volume
-          ",XF86AudioRaiseVolume,exec,${wpctl} set-mute @DEFAULT_AUDIO_SINK@ 0 && ${wpctl} set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
-          ",XF86AudioLowerVolume,exec,${wpctl} set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-          ",XF86AudioMute,exec,${wpctl} set-mute @DEFAULT_AUDIO_SINK@ toggle"
-          "SHIFT,XF86AudioRaiseVolume,exec,${wpctl} set-volume -l 1 @DEFAULT_AUDIO_SOURCE@ 5%+"
-          "SHIFT,XF86AudioLowerVolume,exec,${wpctl} set-volume @DEFAULT_AUDIO_SOURCE@ 5%-"
-          "SHIFT,XF86AudioMute,exec,${wpctl} set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-          ",XF86AudioMicMute,exec,${wpctl} set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-
-          # Screenshotting
-          ",Print,exec,${grimblast} --notify --freeze copy area"
-          "SHIFT,Print,exec,${grimblast} --notify --freeze copy output"
-
-          # To OCR
-          "ALT,Print,exec,${grimblast} --freeze save area - | ${tesseract} - - | wl-copy && ${notify-send} -t 3000 'OCR result copied to buffer'"
-
-          "ALT,Tab,cyclenext"
-          "ALT,Tab,bringactivetotop"
-
-
-        ]
-        ++
-        (
-          let
-            playerctl = lib.getExe' config.services.playerctld.package "playerctl";
-            playerctld = lib.getExe' config.services.playerctld.package "playerctld";
-          in
-            lib.optionals config.services.playerctld.enable [
-              # Media control
-              ",XF86AudioNext,exec,${playerctl} next"
-              ",XF86AudioPrev,exec,${playerctl} previous"
-              ",XF86AudioPlay,exec,${playerctl} play-pause"
-              ",XF86AudioStop,exec,${playerctl} stop"
-              "SHIFT,XF86AudioNext,exec,${playerctld} shift"
-              "SHIFT,XF86AudioPrev,exec,${playerctld} unshift"
-              "SHIFT,XF86AudioPlay,exec,systemctl --user restart playerctld"
-            ]
-        )
-        ++
-        # Screen lock
-        (
-          let
-            hyprlock = lib.getExe config.programs.hyprlock.package;
-          in
-          lib.optionals config.programs.hyprlock.enable [
-            "SUPER,backspace,exec,${config.home.homeDirectory}/.config/rofi/scripts/powermenu_t4"
-            "SUPER,XF86Calculator,exec,${config.home.homeDirectory}/.config/rofi/scripts/powermenu_t4"
-          ]
-        )
-        ++
-        # Notification manager
-        (
-          let
-            makoctl = lib.getExe' config.services.mako.package "makoctl";
-          in
-            lib.optionals config.services.mako.enable [
-              "SUPER,w,exec,${makoctl} dismiss"
-              "SUPER SHIFT,w,exec,${makoctl} restore"
-            ]
-        )
-        ++
-        # Launcher
-        (
-          lib.optionals config.programs.rofi.enable [
-            "SUPER,x,exec,${rofi} -show drun -theme ${config.home.homeDirectory}/.config/rofi/style/launcher-center.rasi"
-            "SUPER,s,exec,${rofi} -show drun -theme ${config.home.homeDirectory}/.config/rofi/style/launcher-full.rasi"
-          ]
-          # ++
-          # (
-          #   let
-          #     pass-wofi = lib.getExe (pkgs.pass-wofi.override {pass = config.programs.password-store.package;});
-          #   in
-          #     lib.optionals config.programs.password-store.enable [
-          #       ",XF86Calculator,exec,${pass-wofi}"
-          #       "SHIFT,XF86Calculator,exec,${pass-wofi} fill"
-
-          #       "SUPER,semicolon,exec,${pass-wofi}"
-          #       "SUPER SHIFT,semicolon,exec,${pass-wofi} fill"
-          #     ]
-          # )
-          # ++
-          # (
-          #   let
-          #     cliphist = lib.getExe config.services.cliphist.package;
-          #   in
-          #   lib.optionals config.services.cliphist.enable [
-          #     ''SUPER,c,exec,selected=$(${cliphist} list | ${rofi} -S dmenu) && echo "$selected" | ${cliphist} decode | wl-copy''
-          #   ]
-          # )
-        );
     };
-    extraConfig = ''
-      # Passthrough mode (e.g. for VNC)
-      bind=SUPER,P,submap,passthrough
-      submap=passthrough
-      bind=SUPER,P,submap,reset
-      submap=reset
-    '';
   };
 }
