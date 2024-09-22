258a173a9cf889a5ef8b3530f6de2891a673dee6
Ryan Clark <ryanc@accentservices.com>
2024-09-22 12:28:03 -0500

    Refactor hyprland basic-binds.nix: Update keybindings and add new commands

--- home/features/desktop/hyprland/hypr/basic-binds.nix
+++ home/features/desktop/hyprland/hypr/basic-binds.nix
@@ -4,7 +4,17 @@
   pkgs,
   ...
 }:
-
+let
+  terminal = lib.getExe pkgs.alacritty;
+  files = "${pkgs.nautilus}/bin/nautilus";
+  defaultApp = type: "${lib.getExe pkgs.handlr-regex} launch ${type}";
+  browser = "${pkgs.google-chrome}/bin/google-chrome-stable";
+  remote = lib.getExe (pkgs.writeShellScriptBin "remote" ''
+    socket="$(basename "$(find ~/.ssh -name 'administrator@*' | head -1 | cut -d ':' -f1)")"
+    host="''${socket#master-}"
+    ssh "$host" "$@"
+  '');
+in
 {
   wayland.windowManager.hyprland.settings = {
     bindm = [
@@ -14,55 +24,45 @@
 
     bind =
       [
-        "SUPER,q,killactive"
-        "ALT,F4,killactive"
-        "SUPER SHIFT,e,exit"
+        "SUPER, q,killactive"
+        "ALT, F4,killactive"
+        "SUPER SHIFT, e,exit"
 
-        "SUPER,f,fullscreen,1"
-        "SUPER SHIFT,f,fullscreen,0"
-        "SUPER SHIFT,space,togglefloating"
+        "SUPER, f,fullscreen,1"
+        "SUPER SHIFT, f,fullscreen,0"
+        "SUPER SHIFT, space,togglefloating"
 
-        "SUPER,minus,splitratio,-0.25"
-        "SUPER SHIFT,minus,splitratio,-0.3333333"
+        "SUPER, minus,splitratio,-0.25"
+        "SUPER SHIFT, minus,splitratio,-0.3333333"
 
-        "SUPER,equal,splitratio,0.25"
-        "SUPER SHIFT,equal,splitratio,0.3333333"
+        "SUPER,equal, splitratio,0.25"
+        "SUPER SHIFT, equal,splitratio,0.3333333"
 
-        "SUPER,g,togglegroup"
-        "SUPER,t,lockactivegroup,toggle"
+        "SUPER, g,togglegroup"
+        "SUPER, t,lockactivegroup,toggle"
 
-        "SUPER,apostrophe,workspace,previous"
-        "SUPER SHIFT,apostrophe,workspace,next"
+        "SUPER, apostrophe,workspace,previous"
+        "SUPER SHIFT, apostrophe,workspace,next"
 
-        "SUPER,u,togglespecialworkspace"
-        "SUPER SHIFT,u,movetoworkspacesilent,special"
-        "SUPER,i,pseudo"
+        "SUPER, u,togglespecialworkspace"
+        "SUPER SHIFT, u,movetoworkspacesilent,special"
+        "SUPER, i,pseudo"
 
-        "ALT,Tab,cyclenext"
-        "ALT,Tab,bringactivetotop"
+        # "ALT, Tab,cyclenext"
+        # "ALT, Tab,bringactivetotop"
       ]
       ++
       (
-        let
-          terminal = lib.getExe pkgs.alacritty;
-          files = "${pkgs.nautilus}/bin/nautilus";
-          defaultApp = type: "${lib.getExe pkgs.handlr-regex} launch ${type}";
-          browser = "${pkgs.google-chrome}/bin/google-chrome-stable";
-          remote = lib.getExe (pkgs.writeShellScriptBin "remote" ''
-            socket="$(basename "$(find ~/.ssh -name 'administrator@*' | head -1 | cut -d ':' -f1)")"
-            host="''${socket#master-}"
-            ssh "$host" "$@"
-          '');
-        in
+
         [
           # Program bindings
-          "SUPER,Return,exec,${terminal}"
-          "SUPER,W,exec,${browser}"
-          "SUPER,S,exec,screenshooting"
-          "SUPER,e,exec,${defaultApp "text/plain"}"
-          "SUPER,b,exec,${defaultApp "x-scheme-handler/https"}"
-          "SUPER ALT,space,exec,${files}"
-          "SUPER,backspace,exec,${pkgs.wlogout}/bin/wlogout"
+          # "SUPER, Return,exec,${terminal}"
+          # "SUPER, W,exec,${browser}"
+          "SUPER, S,exec,screenshooting"
+          "SUPER, e,exec,${defaultApp "text/plain"}"
+          "SUPER, b,exec,${defaultApp "x-scheme-handler/https"}"
+          "SUPER ALT, space,exec,${files}"
+          "SUPER, backspace,exec,${pkgs.wlogout}/bin/wlogout"
           # "SUPER ALT,Return,exec,${remote} ${defaultApp "x-scheme-handler/terminal"}"
           # "SUPER ALT,e,exec,${remote} ${defaultApp "text/plain"}"
           # "SUPER ALT,b,exec,${remote} ${defaultApp "x-scheme-handler/https"}"
@@ -96,6 +96,8 @@
 
           ",XF86MonBrightnessUp,exec,${brightnessctl} -q set +10%"
           ",XF86MonBrightnessDown,exec,${brightnessctl} -q set 10%-"
+          ",XF86KbdBrightnessUp,   exec, ${brightnessctl} -d asus::kbd_backlight set +1"
+          ",XF86KbdBrightnessDown, exec, ${brightnessctl} -d asus::kbd_backlight set  1-"
         ]
       )
       ++
@@ -116,37 +118,37 @@
       )
       ++
       # Screen lock
-      (
-        let
-          hyprlock = lib.getExe config.programs.hyprlock.package;
-        in
-        lib.optionals config.programs.hyprlock.enable [
-          "SUPER,backspace,exec,${config.home.homeDirectory}/.config/rofi/scripts/powermenu_t4"
-          "SUPER,XF86Calculator,exec,${config.home.homeDirectory}/.config/rofi/scripts/powermenu_t4"
-        ]
-      )
-      ++
+      # (
+      #   let
+      #     hyprlock = lib.getExe config.programs.hyprlock.package;
+      #   in
+      #   lib.optionals config.programs.hyprlock.enable [
+      #     "SUPER,backspace,exec,${config.home.homeDirectory}/.config/rofi/scripts/powermenu_t4"
+      #     "SUPER,XF86Calculator,exec,${config.home.homeDirectory}/.config/rofi/scripts/powermenu_t4"
+      #   ]
+      # )
+      # ++
       # Notification manager
-      (
-        let
-          makoctl = lib.getExe' config.services.mako.package "makoctl";
-        in
-          lib.optionals config.services.mako.enable [
-            "SUPER,w,exec,${makoctl} dismiss"
-            "SUPER SHIFT,w,exec,${makoctl} restore"
-          ]
-      )
-      ++
+      # (
+      #   let
+      #     makoctl = lib.getExe' config.services.mako.package "makoctl";
+      #   in
+      #     lib.optionals config.services.mako.enable [
+      #       "SUPER,w,exec,${makoctl} dismiss"
+      #       "SUPER SHIFT,w,exec,${makoctl} restore"
+      #     ]
+      # )
+      # ++
       # Launcher
       (
         let
           rofi = lib.getExe config.programs.rofi.package;
         in
         lib.optionals config.programs.rofi.enable [
-          "SUPER,x,exec,${rofi} -show drun -theme ${config.home.homeDirectory}/.config/rofi/style/launcher-center.rasi"
-          "SUPER,s,exec,${rofi} -show drun -theme ${config.home.homeDirectory}/.config/rofi/style/launcher-full.rasi"
+          "SUPER, x,exec,${rofi} -show drun -theme ${config.home.homeDirectory}/.config/rofi/style/launcher-center.rasi"
+          # "SUPER, s,exec,${rofi} -show drun -theme ${config.home.homeDirectory}/.config/rofi/style/launcher-full.rasi"
         # "$mod, Space, exec, $menu --show drun"
-          "SUPERSHIFT,W,exec,web-search"
+          "SUPERSHIFT, W,exec,web-search"
         ]
         ++
         (
@@ -154,7 +156,7 @@
             cliphist = lib.getExe config.services.cliphist.package;
           in
           lib.optionals config.services.cliphist.enable [
-            ''SUPER,c,exec,selected=$(${cliphist} list | ${rofi} -S dmenu) && echo "$selected" | ${cliphist} decode | wl-copy''
+            ''SUPER, c,exec,selected=$(${cliphist} list | ${rofi} -S dmenu) && echo "$selected" | ${cliphist} decode | wl-copy''
           ]
         )
       )
@@ -173,42 +175,103 @@
         ]
       )
       ++
-      # Bindings for moving windows and workspaces
       (
         let
-          workspaces = [ "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" ];
-          # Map keys (arrows and hjkl) to hyprland directions (l, r, u, d)
-          directions = rec {
-              left = "l";
-              right = "r";
-              up = "u";
-              down = "d";
-              h = left;
-              l = right;
-              k = up;
-              j = down;
-          };
-        in
-        # Change workspace
-        (map (n: "SUPER,${n},workspace,name:${n}") workspaces)
-        ++
-        # Move window to workspace
-        (map (n: "SUPER SHIFT,${n},movetoworkspacesilent,name:${n}") workspaces)
-        ++
-        # Move focus
-        (lib.mapAttrsToList (key: direction: "SUPER,${key},movefocus,${direction}") directions)
-        ++
-        # Swap windows
-        (lib.mapAttrsToList (key: direction: "SUPER SHIFT,${key},swapwindow,${direction}") directions)
-        ++
-        # Move windows
-        (lib.mapAttrsToList (key: direction: "SUPER CONTROL,${key},movewindoworgroup,${direction}") directions)
-        ++
-        # Move monitor focus
-        (lib.mapAttrsToList (key: direction: "SUPER ALT,${key},focusmonitor,${direction}") directions)
-        ++
-        # Move workspace to other monitor
-        (lib.mapAttrsToList (key: direction: "SUPER ALT SHIFT,${key},movecurrentworkspacetomonitor,${direction}") directions)
+        yt = pkgs.writeShellScript "yt" ''
+          notify-send "Opening video" "$(wl-paste)"
+          mpv "$(wl-paste)"
+        '';
+        screenshot = "screenshooting";
+        binding = mod: cmd: key: arg: "${mod}, ${key}, ${cmd}, ${arg}";
+        mvfocus = binding "SUPER" "movefocus";
+        ws = binding "SUPER" "workspace";
+        resizeactive = binding "SUPER CTRL" "resizeactive";
+        mvactive = binding "SUPER ALT" "moveactive";
+        mvtows = binding "SUPER SHIFT" "movetoworkspace";
+        e = "exec, ags -b hypr";
+        arr = [1 2 3 4 5 6 7];
+      in
+        [
+          "CTRL SHIFT, R,  ${e} quit; ags -b hypr"
+          "SUPER, R,       ${e} -t launcher"
+          "SUPER, Tab,     ${e} -t overview"
+          ",XF86PowerOff,  ${e} -r 'powermenu.shutdown()'"
+          ",XF86Launch4,   ${e} -r 'recorder.start()'"
+          ",Print,         exec, ${screenshot}"
+          "SHIFT,Print,    exec, ${screenshot} --full"
+          "SUPER, Return, exec, xterm" # xterm is a symlink, not actually xterm
+          "SUPER, W, exec, firefox"
+          # "SUPER, E, exec, wezterm -e lf"
+
+          # youtube
+          ", XF86Launch1,  exec, ${yt}"
+
+          "ALT, Tab, focuscurrentorlast"
+          "CTRL ALT, Delete, exit"
+          "ALT, Q, killactive"
+          # "SUPER, F, togglefloating"
+          # "SUPER, G, fullscreen"
+          # "SUPER, O, fakefullscreen"
+          "SUPER, P, togglesplit"
+
+          (mvfocus "k" "u")
+          (mvfocus "j" "d")
+          (mvfocus "l" "r")
+          (mvfocus "h" "l")
+          (ws "left" "e-1")
+          (ws "right" "e+1")
+          (mvtows "left" "e-1")
+          (mvtows "right" "e+1")
+          (resizeactive "k" "0 -20")
+          (resizeactive "j" "0 20")
+          (resizeactive "l" "20 0")
+          (resizeactive "h" "-20 0")
+          (mvactive "k" "0 -20")
+          (mvactive "j" "0 20")
+          (mvactive "l" "20 0")
+          (mvactive "h" "-20 0")
+        ]
+        ++ (map (i: ws (toString i) (toString i)) arr)
+        ++ (map (i: mvtows (toString i) (toString i)) arr)
+
       );
+      # ++
+      # # Bindings for moving windows and workspaces
+      # (
+      #   let
+      #     workspaces = [ "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" ];
+      #     # Map keys (arrows and hjkl) to hyprland directions (l, r, u, d)
+      #     directions = rec {
+      #         left = "l";
+      #         right = "r";
+      #         up = "u";
+      #         down = "d";
+      #         h = left;
+      #         l = right;
+      #         k = up;
+      #         j = down;
+      #     };
+      #   in
+      #   # Change workspace
+      #   (map (n: "SUPER,${n},workspace,name:${n}") workspaces)
+      #   ++
+      #   # Move window to workspace
+      #   (map (n: "SUPER SHIFT,${n},movetoworkspacesilent,name:${n}") workspaces)
+      #   ++
+      #   # Move focus
+      #   (lib.mapAttrsToList (key: direction: "SUPER,${key},movefocus,${direction}") directions)
+      #   ++
+      #   # Swap windows
+      #   (lib.mapAttrsToList (key: direction: "SUPER SHIFT,${key},swapwindow,${direction}") directions)
+      #   ++
+      #   # Move windows
+      #   (lib.mapAttrsToList (key: direction: "SUPER CONTROL,${key},movewindoworgroup,${direction}") directions)
+      #   ++
+      #   # Move monitor focus
+      #   (lib.mapAttrsToList (key: direction: "SUPER ALT,${key},focusmonitor,${direction}") directions)
+      #   ++
+      #   # Move workspace to other monitor
+      #   (lib.mapAttrsToList (key: direction: "SUPER ALT SHIFT,${key},movecurrentworkspacetomonitor,${direction}") directions)
+      # );
   };
 }
