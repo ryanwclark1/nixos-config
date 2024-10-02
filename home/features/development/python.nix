bd0a5087e60082fa05e5648562da9a4b06ee4041
Ryan Clark <ryanc@accentservices.com>
2024-10-02 09:50:28 -0500

    Refactor development default.nix: Remove unused poetry.nix and add cachix

--- home/features/development/python.nix
+++ home/features/development/python.nix
@@ -6,13 +6,18 @@
 {
   home.packages = with pkgs; [
     python312
-    python312Packages.poetry-core
-    python312Packages.pdm-backend
     python312Packages.pip
-    # python312Packages.pipx
-    # poetry
     functiontrace-server
-    # memray
   ];
-  # home.sessionPath = [ "$HOME/.python/bin" ];
+
+  programs = {
+    poetry = {
+      enable = true;
+      package = pkgs.poetry.withPlugins (ps: with ps; [ poetry-plugin-up ]);
+      settings = {
+        virtualenvs.create = true;
+        virtualenvs.in-project = true;
+      };
+    };
+  };
 }
