d3b6350d8fccd9044b93ff523c44fd8b889b90de
Ryan Clark <ryanc@accentservices.com>
2024-10-03 09:12:05 -0500

    Refactor go.nix: Update go packages and add new dependencies

--- home/features/development/go.nix
+++ home/features/development/go.nix
@@ -8,13 +8,30 @@
     enable = true;
     package = pkgs.go_1_23;
     goPath = "go";
+    goBin = "go/bin";
+    packages = {
+      # "golang.org/x/text" = builtins.fetchGit "https://go.googlesource.com/text";
+      # "github.com/air-verse/air" = builtins.fetchGit { url = "https://github.com/air-verse/air"; ref = "master"; };
+      # "github.com/mvdan/gofumpt" = builtins.fetchGit "https://https://github.com/mvdan/gofumpt";
+      # "github.com/a-h/templ" = builtins.fetchGit "https://github.com/a-h/templ";
+      # "golang.org/x/tools/gopls" = builtins.fetchGit "https://golang.org/x/tools/gopls";
+      # "golang.org/x/tools/cmd/goimports" = builtins.fetchGit "https://golang.org/x/tools/cmd/goimports";
+      # "github.com/go-delve/delve/cmd/dlv" = builtins.fetchGit "https://github.com/go-delve/delve";
+      # "gotest.tools/gotestsum" = builtins.fetchGit "https://github.com/gotestyourself/gotestsum";
+    };
   };
 
   home.packages = with pkgs; [
+    air       # Live reload for Go apps
+    templ     # A language for writing HTML user interfaces in Go.
     golangci-lint-langserver
     gopls
     errcheck
-    templ
+    delve # debugger for the Go programming language
+    gofumpt
+    gotestsum
+    golangci-lint
+    go-callvis
   ];
-  # home.sessionPath = [ "$HOME/go/bin" ];
+
 }
