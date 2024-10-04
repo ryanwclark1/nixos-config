fa032dae189173c73904c51c74887782405fb495
Ryan Clark <ryanc@accentservices.com>
2024-10-04 18:04:43 -0500

    Refactor starship plugin: Update vimcmd_symbol
    Update the vimcmd_symbol in the starship plugin to use a new icon.

--- home/features/starship/default.nix
+++ home/features/starship/default.nix
@@ -286,7 +286,7 @@ with config.lib.stylix.colors.withHashtag;
       character = {
         error_symbol = "[~>](bold ${base08})";
         success_symbol = "[](bold ${base0B})";
-        vimcmd_symbol = "[](bold ${base0A})";
+        vimcmd_symbol = "[](bold ${base0A})";
         vimcmd_visual_symbol = "[](bold ${base0C})";
         vimcmd_replace_symbol = "[](bold ${base0E})";
         vimcmd_replace_one_symbol = "[](bold ${base0E})";
