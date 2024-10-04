c2617f260c8f1b4a85946511344e649cd1fea4fb
Ryan Clark <ryanc@accentservices.com>
2024-10-03 19:53:48 -0500

    Refactor noti.nix: Add espeak voice settings and shell alias

--- home/features/cli/noti.nix
+++ home/features/cli/noti.nix
@@ -1,10 +1,24 @@
 # Monitor a process and trigger a notification.
 {
+  pkgs,
   ...
 }:
 
 {
   programs.noti = {
     enable = true;
+    settings = {
+      espeak = {
+        voiceName = "english-us";
+      };
+    };
+  };
+
+  home.packages = with pkgs; [
+    espeak-classic
+  ];
+
+  home.shellAliases = {
+    espeak = "espeak-ng";
   };
 }
