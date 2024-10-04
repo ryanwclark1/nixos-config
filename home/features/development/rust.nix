3da15aeb6771eede7dc987ae343fde400e605b33
Ryan Clark <ryanc@accentservices.com>
2024-10-03 21:35:21 -0500

    Refactor rust.nix: Comment out unused cargo subcommands, external libraries, and utilities

--- home/features/development/rust.nix
+++ home/features/development/rust.nix
@@ -6,41 +6,41 @@
 
 {
   home.packages = with pkgs; let
-    cargoSubCommands = [
-      cargo-cross
-      cargo-clone
-      cargo-release
-      cargo-vet
-      cargo-shuttle
-      cargo-temp
-      cargo-shuttle
-      cargo-sort
-      cargo-sweep
-      cargo-audit
-      cargo-tarpaulin
-      cargo-semver-checks
-      cargo-udeps
-      cargo-watch
-      cargo-crev
-      cargo-deny
-      cargo-diet
-      cargo-edit
-      cargo-bloat
-      cargo-about
-      cargo-update
-      cargo-readme
-      cargo-release
-      cargo-modules
-      cargo-profiler
-      cargo-outdated
-      cargo-generate
-      cargo-workspaces
-      cargo-public-api
-      cargo-supply-chain
-      cargo-unused-features
-      cargo-nextest
-      cargo-leptos
-    ];
+    # cargoSubCommands = [
+    #   cargo-cross
+    #   cargo-clone
+    #   cargo-release
+    #   cargo-vet
+    #   cargo-shuttle
+    #   cargo-temp
+    #   cargo-shuttle
+    #   cargo-sort
+    #   cargo-sweep
+    #   cargo-audit
+    #   cargo-tarpaulin
+    #   cargo-semver-checks
+    #   cargo-udeps
+    #   cargo-watch
+    #   cargo-crev
+    #   cargo-deny
+    #   cargo-diet
+    #   cargo-edit
+    #   cargo-bloat
+    #   cargo-about
+    #   cargo-update
+    #   cargo-readme
+    #   cargo-release
+    #   cargo-modules
+    #   cargo-profiler
+    #   cargo-outdated
+    #   cargo-generate
+    #   cargo-workspaces
+    #   cargo-public-api
+    #   cargo-supply-chain
+    #   cargo-unused-features
+    #   cargo-nextest
+    #   cargo-leptos
+    # ];
 
     rustBasics = [
       # llvmPackages_latest.bintools
@@ -50,34 +50,34 @@
       cargo-binutils
     ];
 
-    externalLibs = [
-      # qemu
-      clang
-      glib
-      grub2
-      openssl
-      pkg-config
-      xorriso
-      zlib.out
-    ];
+    # externalLibs = [
+    #   # qemu
+    #   clang
+    #   glib
+    #   grub2
+    #   openssl
+    #   pkg-config
+    #   xorriso
+    #   zlib.out
+    # ];
 
-    utils = [
-      # rust-analyzer
-      bunyan-rs
-      lapce
-      lldb
-      mdbook
-      rust-audit-info
-      rust-code-analysis
-      rust-script
-      rusty-man
-      sea-orm-cli
-      sqlite
-      sqlx-cli
-      taplo
-      trunk # for wasm
-      valgrind
-    ];
+    # utils = [
+    #   # rust-analyzer
+    #   bunyan-rs
+    #   lapce
+    #   lldb
+    #   mdbook
+    #   rust-audit-info
+    #   rust-code-analysis
+    #   rust-script
+    #   rusty-man
+    #   sea-orm-cli
+    #   sqlite
+    #   sqlx-cli
+    #   taplo
+    #   trunk # for wasm
+    #   valgrind
+    # ];
   in
   cargoSubCommands
   ++ rustBasics
