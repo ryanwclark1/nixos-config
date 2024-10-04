660a1afdd77cc811ddcd834a48ed78d0fa395c69
Ryan Clark <ryanc@accentservices.com>
2024-10-04 16:29:26 -0500

    Refactor yazi plugin and add folder-rules and eza-preview plugins

--- home/features/yazi/init.lua
+++ home/features/yazi/init.lua
@@ -2,9 +2,21 @@
 -- 	type = ui.Border.ROUNDED,
 -- }
 
--- require("smart-paste").entry{}
--- require("smart-enter").entry{}
--- require("max-preview"):entry{}
-require("starship"):setup{}
+-- require("starship"):setup{}
+require("folder-rules"):setup{}
+require("eza-preview"):setup()
 
+-- Show symlink in status bar
+function Status:name()
+  local h = self._tab.current.hovered
+  if not h then
+    return ui.Line {}
+  end
+
+  local linked = ""
+  if h.link_to ~= nil then
+    linked = " -> " .. tostring(h.link_to)
+  end
+  return ui.Line(" " .. h.name .. linked)
+end
 
