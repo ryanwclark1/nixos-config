cc7887366c38648bf83c1229a77b6c538b153da2
Ryan Clark <ryanc@accentservices.com>
2024-10-07 16:05:50 -0500

    Moved shell integrations to applications

--- home/features/shell/zsh.nix
+++ home/features/shell/zsh.nix
@@ -1,6 +1,5 @@
 {
-  config,
-  lib,
+
   pkgs,
   ...
 }:
@@ -19,16 +18,5 @@
       enable = true;
     };
   };
-
-  programs.atuin.enableZshIntegration = lib.mkIf config.programs.atuin.enable true;
-  programs.eza.enableZshIntegration = lib.mkIf config.programs.eza.enable true;
-  programs.fzf.enableZshIntegration = lib.mkIf config.programs.fzf.enable true;
-  programs.kitty.shellIntegration.enableZshIntegration = lib.mkIf config.programs.kitty.enable true;
-  programs.navi.enableZshIntegration = lib.mkIf config.programs.navi.enable true;
-  programs.nix-index.enableZshIntegration = lib.mkIf config.programs.nix-index.enable true;
-  programs.starship.enableZshIntegration = lib.mkIf config.programs.starship.enable true;
-  programs.zoxide.enableZshIntegration = lib.mkIf config.programs.zoxide.enable true;
-  programs.yazi.enableZshIntegration = lib.mkIf config.programs.yazi.enable true;
-  programs.zellij.enableZshIntegration = lib.mkIf config.programs.zellij.enable false;
 
 }
