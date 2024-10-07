90a7e809e2ff702dff321b6af047ec24de5d6aba
Ryan Clark <ryanc@accentservices.com>
2024-10-07 12:46:09 -0500

    Refactor navi.nix: Update navi configuration options

--- home/features/cli/navi.nix
+++ home/features/cli/navi.nix
@@ -10,42 +10,43 @@
   programs.navi = {
     enable = true;
     package = pkgs.navi;
-    enableBashIntegration = lib.mkIf config.programs.bash.enable true;
-    enableFishIntegration = lib.mkIf config.programs.fish.enable true;
-    enableZshIntegration = lib.mkIf config.programs.zsh.enable true;
+    # conflicts with Control G -> GH
+    # enableBashIntegration = lib.mkIf config.programs.bash.enable true;
+    # enableFishIntegration = lib.mkIf config.programs.fish.enable true;
+    # enableZshIntegration = lib.mkIf config.programs.zsh.enable true;
     settings = {
-      style = {
-        tag = {
-          color = "cyan";
-          width_percentage = 26;
-          min_width = 20;
-        };
-        comment = {
-          color = "blue";
-          width_percentage = 42;
-          min_width = 45;
-        };
-        snippet = {
-          color = "white";
-        };
-      };
-      finder = {
-        command = "fzf";
-        # overrides: --tac # equivalent to the --fzf-overrides option
-        # overrides_var: --tac # equivalent to the --fzf-overrides-var option
-      };
-      cheats = {
-        paths = [];
-      };
+      # style = {
+      #   tag = {
+      #     color = "cyan";
+      #     width_percentage = 26;
+      #     min_width = 20;
+      #   };
+      #   comment = {
+      #     color = "blue";
+      #     width_percentage = 42;
+      #     min_width = 45;
+      #   };
+      #   snippet = {
+      #     color = "white";
+      #   };
+      # };
+      # finder = {
+      #   command = "fzf";
+      #   # overrides: --tac # equivalent to the --fzf-overrides option
+      #   # overrides_var: --tac # equivalent to the --fzf-overrides-var option
+      # };
+      # cheats = {
+      #   paths = [];
+      # };
       # search:
       #   tags: git,!checkout # equivalent to the --tag-rules option
       client = {
         tealdeer = true;
       };
-      shell = {
-        command = "bash";
-        finder_command = "bash";
-      };
+      # shell = {
+      #   command = "bash";
+      #   finder_command = "bash";
+      # };
     };
   };
 }
