931e78e5563968b05868642a67d8d8b995cd0a4b
Ryan Clark <ryanc@accentservices.com>
2024-10-08 15:25:20 -0500

    Add bat.nix configuration for bat package and shell aliases
    This commit adds the configuration for the bat package, including the theme and additional packages. It also adds shell aliases for `cat` and `less` commands to use `bat` with custom options.

---
+++ home/features/bat/bat.nix
@@ -0,0 +1,32 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  home.file.".config/bat/themes/theme.tmTheme".source = ./theme.tmTheme;
+
+  programs.bat = {
+    enable = true;
+    package = pkgs.bat;
+    extraPackages = with pkgs.bat-extras; [
+      batdiff
+      batgrep
+      batman
+      batpipe
+      batwatch
+      prettybat
+    ];
+    config = {
+      pager = "less -FR";
+      theme = "theme";
+    }
+  };
+
+  home.shellAliases = {
+    cat = "bat --plain --color=always";
+    less = "bat --pager 'less RF' --style=numbers --color=always";
+  };
+
+
+}
