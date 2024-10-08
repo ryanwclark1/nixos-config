fb59b174d7a4a5459234c8dacd74a9c88c6066a0
Ryan Clark <ryanc@accentservices.com>
2024-10-07 21:01:18 -0500

    Refactor zsh default.nix: Add shell integration for fzf

--- home/features/shell/zsh.nix
+++ home/features/shell/zsh.nix
@@ -17,6 +17,20 @@
     autosuggestion = {
       enable = true;
     };
+    initExtra = ''
+    _fzf_comprun() {
+      local command=$1
+      shift
+
+      case "$command" in
+        cd)           fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
+        export|unset) fzf --preview "eval 'echo \''\${}'"         "$@" ;;
+        ssh)          fzf --preview 'dig {}'                   "$@" ;;
+        *)            fzf --preview "$show_file_or_dir_preview" "$@" ;;
+      esac
+    }
+
+    '';
   };
 
 }
