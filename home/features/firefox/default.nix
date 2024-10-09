12a4c07f32d077bb065d494715e0bf641adecbda
Ryan Clark <ryanc@accentservices.com>
2024-10-08 21:30:15 -0500

    Refactor firefox default.nix: Remove default applications for mime types

--- home/features/firefox/default.nix
+++ home/features/firefox/default.nix
@@ -34,11 +34,4 @@
       };
     };
   };
-
-  xdg.mimeApps.defaultApplications = {
-    "text/html" = [ "firefox.desktop" ];
-    "text/xml" = [ "firefox.desktop" ];
-    "x-scheme-handler/http" = [ "firefox.desktop" ];
-    "x-scheme-handler/https" = [ "firefox.desktop" ];
-  };
 }
