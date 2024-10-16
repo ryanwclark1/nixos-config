67392e22b759deda465879d350f8e0bc83f457b4
Ryan Clark <ryanc@accentservices.com>
2024-10-16 14:11:01 -0500

    Add new desktop packages for development tools and utilities

--- home/features/desktop/common/default.nix
+++ home/features/desktop/common/default.nix
@@ -32,7 +32,12 @@
     libsoup
     fragments # Torrent client
     seabird # Kubernetes native desktop app that simplifies working with Kubernetes.
-
+    insomnia # API Client for REST, GraphQL, GRPC and OpenAPI design tool for developers.
+    devpod-desktop # Codespaces but open-source, client-only and unopinionated: Works with any IDE
+    tilt # Local Kubernetes development Local to manage your developer instance when your team deploys to Kubernetes in production.
+    postman # API Development Environment
+    dbeaver-bin # Universal Database Tool
+    sqlitebrowser # Visual tool to create, design, and edit database files compatible with SQLite
   ];
 
 }
