2e47c1d99e54123a7ef86decccdf957507c668ec
Ryan Clark <ryanc@accentservices.com>
2024-10-16 12:53:26 -0500

    Adding accent machine

---
+++ home/accent.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  ...
+}:
+
+{
+  imports = [
+    ./global
+
+    ./features/alacritty
+    ./features/atuin
+    ./features/bat
+    ./features/cli
+    ./features/compression
+    ./features/eza
+    ./features/fastfetch
+    ./features/fd
+    ./features/fzf
+    ./features/git
+    ./features/kitty
+    ./features/kubernetes
+    ./features/lazygit
+    ./features/networking-utils
+    ./features/nixvim
+    ./features/ripgrep
+    ./features/shell
+    ./features/starship
+    ./features/sys-stats
+    ./features/tmux
+    ./features/virtviewer
+    ./features/yazi
+    ./features/zellij
+    ./features/zoxide
+  ];
+
+  # Disable impermanence
+  # home.persistence = lib.mkForce {};
+}
