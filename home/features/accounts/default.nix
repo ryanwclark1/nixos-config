ed99fa235b9b29953d57de42b85998c1b84850e1
Ryan Clark <ryanc@accentservices.com>
2024-10-25 18:26:12 -0500

    Refactor .gitignore and home-manager.md: Update email configuration and add neomutt feature

--- home/features/accounts/default.nix
+++ home/features/accounts/default.nix
@@ -1,61 +1,38 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }:
 
 {
-  sops.secrets = {
-    accent-email-address = {
-      sopsFile = ../../../secrets/secrets.yaml;
-    };
-    accent-email-username = {
-      sopsFile = ../../../secrets/secrets.yaml;
-    };
-    accent-email-name = {
-      sopsFile = ../../../secrets/secrets.yaml;
-    };
-    accent-email-realname = {
-      sopsFile = ../../../secrets/secrets.yaml;
-    };
-    accent-email-password = {
-      sopsFile = ../../../secrets/secrets.yaml;
-    };
-    accent-email-imap-host = {
-      sopsFile = ../../../secrets/secrets.yaml;
-    };
-    accent-email-imap-port = {
-      sopsFile = ../../../secrets/secrets.yaml;
-    };
-    accent-email-smtp-host = {
-      sopsFile = ../../../secrets/secrets.yaml;
-    };
-    accent-email-smtp-port = {
-      sopsFile = ../../../secrets/secrets.yaml;
-    };
-    accent-email-flavor = {
-      sopsFile = ../../../secrets/secrets.yaml;
-    };
-  };
+
   accounts = {
     email = {
       maildirBasePath = "Mail";
       accounts = {
-        "accent" = {
-          address = config.sops.secrets.accent-email-address.path;
-          flavor = config.sops.secrets.accent-email-flavor.path;
-          name = config.sops.secrets.accent-email-name.path;
-          realName = config.sops.secrets.accent-email-realname.path;
-          username = config.sops.secrets.accent-email-username.path;
-          # passwordCommand = "";
-          imap = {
-            host = config.sops.secrets.accent-email-imap-host.path;
-            port = config.sops.secrets.accent-email-imap-port.path;
-          };
-          smtp = {
-            host = config.sops.secrets.accent-email-smtp-host.path;
-            port = config.sops.secrets.accent-email-smtp-port.path;
-          };
+        "accent" =
+          let
+            accent_email = "$(${pkgs.coreutils}/bin/cat ${config.sops.secrets."accent-email/accent-email-address".path})";
+          in
+          {
+          address = accent_email;
+          flavor = "gmail.com";
+          # name = config.sops.secrets.accent-email-name.path;
+          realName = "Ryan Clark";
+          # userName = config.sops.secrets."accent-email/accent-email-username".path;
+          # username = "ryanc@accentservices.com";
+          passwordCommand = ''${pkgs.coreutils}/bin/cat ${config.sops.secrets."accent-email/accent-email-password".path}'';
+          primary = true;
+          # imap = {
+            # host = builtins.readFile config.sops.secrets."accent-email/accent-email-imap-host".path;
+            # port = 993;
+            # port = builtins.readFile config.sops.secrets."accent-email/accent-email-imap-port".path;
+          # };
+          # smtp = {
+          #   host = config.sops.secrets."accent-email/accent-email-smtp-host".path;
+          #   port = 465;
+          # };
           neomutt = {
             enable = true;
             mailboxType = "imap";
@@ -70,5 +47,15 @@
     package = pkgs.neomutt;
     editor = "$EDITOR";
     vimKeys = true;
+  };
+
+  home.file = {
+    ".config/accent-email" = {
+      text =
+        ''
+          password = ''$(${pkgs.coreutils}/bin/cat ${config.sops.secrets."accent-email/accent-email-password".path}')'
+          username = ${config.sops.secrets."accent-email/accent-email-username".key}
+      '';
+    };
   };
 }
