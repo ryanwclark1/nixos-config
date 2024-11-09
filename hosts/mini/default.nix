0085a2d8e7846cfac595c25009b378d4b2035607
Ryan Clark <ryanc@accentservices.com>
2024-11-09 15:34:06 -0600

    Refactor mini/default.nix: Comment out unused code and update system state version

--- hosts/mini/default.nix
+++ hosts/mini/default.nix
@@ -84,11 +84,11 @@
   # programs.fish.enable = true;
 
   # Set Git commit hash for darwin-version.
-  system.configurationRevision = self.rev or self.dirtyRev or null;
+  # system.configurationRevision = self.rev or self.dirtyRev or null;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
-  system.stateVersion = 5;
+  # system.stateVersion = 5;
 
   # The platform the configuration will be used on.
   nixpkgs.hostPlatform = "aarch64-darwin";
