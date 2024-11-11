54e70488c9246c7572d26b3812cbdf6757fde5f6
Ryan Clark <ryanc@accentservices.com>
2024-11-11 11:32:52 -0600

    Refactor global configuration in default.nix and add nh.nix
    - Remove prometheus-node-exporter.nix and its associated configuration
    - Add nh.nix to enable nh, another Nix CLI helper
    - Update nix.nix to remove nh configuration

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -26,15 +26,4 @@
       ];
     };
   };
-
-  programs.nh = {
-    enable = true;
-    package = pkgs.nh;
-    clean = {
-      enable = true;
-      dates = "weekly";
-      extraArgs = "--keep-since 4d --keep 3";
-    };
-    flake = "/home/administrator/nixos-config";
-  };
 }
