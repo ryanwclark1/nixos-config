e562efebc5984a961c741d9612b514a4c1862a30
Ryan Clark <ryanc@accentservices.com>
2024-11-15 11:28:38 -0600

    Refactor home configuration: Move settings to home.nix and clean up default.nix

--- home/global/default.nix
+++ home/global/default.nix
@@ -1,9 +1,6 @@
 {
-  config,
   inputs,
-  lib,
   outputs,
-  pkgs,
   ...
 }:
 
@@ -12,85 +9,9 @@
     inputs.impermanence.nixosModules.home-manager.impermanence
     ./style.nix
     ./sops.nix
+    ./home.nix
   ]
   ++ (builtins.attrValues outputs.homeManagerModules);
-
-  # duplicated in host/common/global/nix.nix
-  nix = {
-    package = lib.mkDefault pkgs.nixVersions.latest;
-    settings = {
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      warn-dirty = false;
-    };
-  };
-
-  systemd.user.startServices = "sd-switch";
-
-  programs = {
-    home-manager.enable = true;
-    git.enable = true;
-  };
-  
-  home =
-  let
-    editor = lib.getExe config.programs.nixvim.package;
-  in
-  {
-    username = lib.mkDefault "administrator";
-    homeDirectory = lib.mkDefault "/home/${config.home.username}";
-    stateVersion = lib.mkDefault "24.11";
-    sessionPath = [ "${config.home.homeDirectory}/.local/bin" ];
-    sessionVariables = {
-      FLAKE = lib.mkDefault "${config.home.homeDirectory}/nixos-config";
-      EDITOR = lib.mkDefault "${editor}";
-      VISUAL = lib.mkDefault "${editor}";
-      MANPAGER = lib.mkDefault "sh -c 'col -bx | bat -l man -p'";
-      MANROFFOPT="-c";
-    };
-    shellAliases = rec{
-      jqless = "jq -C | bat --pager 'less RF' --style=numbers --color=always";
-
-      n = "nix";
-      nd = "nix develop -c $SHELL";
-      ns = "nix shell";
-      nsn = "nix shell nixpkgs#";
-      nb = "nix build";
-      nbn = "nix build nixpkgs#";
-      nf = "nix flake";
-
-      nr = "nixos-rebuild --flake .";
-      nrs = "nixos-rebuild --flake . switch";
-      snr = "sudo nixos-rebuild --flake .";
-      snrs = "sudo nixos-rebuild --flake . switch";
-      hm = "home-manager --flake .";
-      hms = "home-manager --flake . switch";
-
-      wifi = "nmtui";
-
-      cik = lib.mkIf config.programs.kitty.enable "clone-in-kitty --type os-window";
-      ck = cik;
-    };
-
-
-    # persistence = {};
-    # persistence = {
-    #   "/persist${config.home.homeDirectory}" = {
-    #     defaultDirectoryMethod = "symlink";
-    #     directories = [
-    #       "documents"
-    #       "downloads"
-    #       "pictures"
-    #       "videos"
-    #       ".local/bin"
-    #       ".local/share/nix" # trusted settings and repl history
-    #     ];
-    #     allowOther = true;
-    #   };
-    # };
-  };
 
   # home.file = {
   #   ".colorscheme".text = config.colorscheme.slug;
