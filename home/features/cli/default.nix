d66af0f73d12fd851aa9d2f7f83a61e3abfadba5
Ryan Clark <ryanc@accentservices.com>
2024-11-16 16:22:57 -0600

    Remove httpie and jdupes from home packages in default.nix

--- home/features/cli/default.nix
+++ home/features/cli/default.nix
@@ -19,11 +19,9 @@
   ];
   home.packages = with pkgs; [
     distrobox # Nice escape hatch, integrates docker images with my environment
-    httpie # Better curl
     scrot # A command-line screen capture utility
     killall # A command-line tool to kill processes by name
     duf # Disk Usage/Free Utility
-    jdupes # Find duplicate files
     ncdu # TUI disk usage
     lazydocker # A simple terminal UI for both docker and docker-compose
     dive # A tool for exploring each layer in a docker image.
