879002e49e07912e875690eca53af09f1f94195a
Ryan Clark <ryanc@accentservices.com>
2024-11-23 16:11:34 -0600

    Refactor Tailscale configuration: Update flags and parameters for improved functionality and security

--- hosts/woody/services/tailscale.nix
+++ hosts/woody/services/tailscale.nix
@@ -10,16 +10,19 @@
     useRoutingFeatures = lib.mkDefault "client";
     openFirewall = true;
     port = 41641;
-    extraSetFlags = [
-      "--operator=administrator"
-    ];
+    # extraSetFlags = [
+    # ];
     extraUpFlags = [
-      "---exit-node=homeassistant"
+      "--exit-node=homeassistant"
       "--exit-node-allow-lan-access"
+      "--operator=administrator"
+      "--advertise-tags=tag:client,tag:desktop,tag:no-exit"
+      "--accept-routes=true"
     ];
   };
 
+  # Prometheus Service Discovery for Tailscale.
   environment.systemPackages = [
-    pkgs.tailscaled
+    pkgs.tailscalesd
   ];
 }
