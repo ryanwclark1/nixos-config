806eab68e9609151831a4ebbe086452c6dde7b34
Ryan Clark <ryanc@accentservices.com>
2024-12-06 11:13:38 -0600

    Add default font configuration: Include a comprehensive list of Nerd Fonts and other font packages for enhanced typography support

---
+++ home/features/fonts/defaut.nix
@@ -0,0 +1,26 @@
+{
+  ...,
+  pkgs,
+}:
+
+{
+  home.packages = [
+    # Fonts
+    nerd-fonts.dejavu-sans-mono
+    nerd-fonts.droid-sans-mono
+    nerd-fonts.fira-code
+    nerd-fonts.fira-mono
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.monaspace
+    nerd-fonts.mplus
+    nerd-fonts.noto
+    nerd-fonts.ubuntu
+    nerd-fonts.ubuntu-mono
+    nerd-fonts.ubuntu-sans
+    nerd-fonts.symbols-only
+    noto-fonts
+    noto-fonts-emoji
+    liberation_ttf
+    powerline-symbols
+  ];
+}
