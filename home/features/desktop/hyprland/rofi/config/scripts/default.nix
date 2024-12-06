f929dfcd1fc952e64b31e26344b2e79ffb1e3024
Ryan Clark <ryanc@accentservices.com>
2024-12-06 14:09:10 -0600

    Refactor Rofi configuration: Restructure and consolidate style and shared files for improved organization and clarity

---
+++ home/features/desktop/hyprland/rofi/config/scripts/default.nix
@@ -0,0 +1,18 @@
+{
+  ...
+}:
+
+{
+  imports = [
+    ./appasroot.nix
+    ./apps.nix
+    ./battery.nix
+    ./brightness.nix
+    ./mpd.nix
+    ./power-big.nix
+    ./powermenu.nix
+    ./quicklinks.nix
+    ./screenshot.nix
+    ./volume.nix
+  ];
+}
