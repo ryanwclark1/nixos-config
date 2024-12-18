c5477e55f7abd5198f56159bb53aced1e1e5dfe8
Ryan Clark <ryanc@accentservices.com>
2024-12-18 10:35:12 -0600

    Update script file path in default.nix for nix-prefetch-git

--- home/features/desktop/scripts/default.nix
+++ home/features/desktop/scripts/default.nix
@@ -4,7 +4,7 @@
 }:
 
 let
-  scriptFile = ./nix-prefetch-git.sh; # Path to your shell script file
+  scriptFile = ./nix-prefetch-git; # Path to your shell script file
   myBinary = pkgs.writeShellScriptBin "nix-prefetch-git" (pkgs.readFile scriptFile);
 in
 
