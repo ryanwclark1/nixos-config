6872c6613e940e47d3fdbc2598e613f043bd6093
Ryan Clark <ryanc@accentservices.com>
2024-12-22 16:06:28 -0600

    Refactor EWW configuration and scripts for improved organization and functionality

--- home/features/desktop/hyprland/eww/default.nix
+++ home/features/desktop/hyprland/eww/default.nix
@@ -14,31 +14,27 @@
     ./launch_bar.nix
   ];
 
-  programs = {
-    eww = {
-      enable = true;
-      package = pkgs.eww;
-      enableZshIntegration = true;
-      enableBashIntegration = true;
-      enableFishIntegration = true;
-      # configDir = "${config.home.homeDirectory}/.config/eww";
-    };
-  };
+  home.packages = with pkgs; [
+    pkgs.eww
+  ];
 
-
-
-  # configuration
+  # # configuration
   home.file.".config/eww/eww.scss".source = ./eww.scss;
   home.file.".config/eww/eww.yuck".source = ./eww.yuck;
   home.file.".config/eww/colors.scss".source = ./colors.scss;
-  home.file.".config/eww/eww_windows.yuck".source = ./eww_windows.yuck;
-  home.file.".config/eww/eww_widgets.yuck".source = ./eww_widgets.yuck;
   home.file.".config/eww/eww_variables.yuck".source = ./eww_variables.yuck;
-    home.file.".config/eww/variables.yuck".source = ./variables.yuck;
+  home.file.".config/eww/eww_widgets.yuck".source = ./eww_widgets.yuck;
+  home.file.".config/eww/eww_windows.yuck".source = ./eww_windows.yuck;
+  home.file.".config/eww/variables.yuck".source = ./variables.yuck;
 
   # scripts
   home.file.".config/eww/actions" = {
     source = ./actions;
+    recursive = true;
+  };
+
+  home.file.".config/eww/assets" = {
+    source = ./assets;
     recursive = true;
   };
 
@@ -52,28 +48,24 @@
     recursive = true;
   };
 
+  home.file.".config/eww/images" = {
+    source = ./images;
+    recursive = true;
+  };
+
   home.file.".config/eww/powermenu" = {
     source = ./powermenu;
     recursive = true;
   };
-
-  # home.file.".config/eww/scripts/wifi.sh" = {
-  #   source = ./scripts/wifi.sh;
-  #   executable = true;
-  # };
-
-  # home.file.".config/eww/scripts/brightness.sh" = {
-  #   source = ./scripts/brightness.sh;
-  #   executable = true;
-  # };
-
-  # home.file.".config/eww/scripts/workspaces.sh" = {
-  #   source = ./scripts/workspaces.sh;
-  #   executable = true;
-  # };
-
-  # home.file.".config/eww/scripts/workspaces.lua" = {
-  #   source = ./scripts/workspaces.lua;
-  #   executable = true;
-  # };
 }
+
+  # programs = {
+  #   eww = {
+  #     enable = true;
+  #     package = pkgs.eww-wayland;
+  #     enableZshIntegration = true;
+  #     enableBashIntegration = true;
+  #     enableFishIntegration = true;
+  #     configDir = "${config.home.homeDirectory}/.config/eww";
+  #   };
+  # };
--- home/features/desktop/hyprland/eww/scripts/workspaces.nix
+++ home/features/desktop/hyprland/eww/scripts/workspaces.nix
@@ -8,6 +8,15 @@
 {
   home.file.".config/eww/scripts/workspaces.sh" = {
     text = ''
+      #!/bin/sh
+      
+      workspaces() {
+      ./scripts/workspaces.lua
+      }
+      workspaces
+      tail -f /tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/hyprland.log | grep --line-buffered "Changed to workspace" | while read -r; do
+      workspaces
+      done
     '';
     executable = true;
   };
