a46886dcbdb999c385e035a988df34221dc5be7a
Ryan Clark <ryanc@accentservices.com>
2024-12-25 21:35:08 -0600

    Add new EWW scripts for memory, battery, wifi, brightness, and volume management; remove deprecated scripts

--- home/features/desktop/hyprland/eww/scripts/default.nix
+++ home/features/desktop/hyprland/eww/scripts/default.nix
@@ -4,24 +4,106 @@
 
 {
   imports = [
-    ./airplane_mode.nix
+    # ./airplane_mode.nix
     # ./appname.nix
-    ./battery.nix
-    ./battery2.nix
-    ./brightness.nix
-    ./mem-ad.nix
-    ./memory.nix
-    ./music_info.nix
-    ./notifications.nix
-    ./pop.nix
-    ./screenshot.nix
-    ./search.nix
-    ./volume.nix
-    ./weather.nix
-    ./wifi.nix
-    ./wifi2.nix
+    # ./battery.nix
+    # ./battery2.nix
+    # ./brightness.nix
+    # ./mem-ad.nix
+    # ./memory.nix
+    # ./music_info.nix
+    # ./notifications.nix
+    # ./pop.nix
+    # ./screenshot.nix
+    # ./search.nix
+    # ./volume.nix
+    # ./weather.nix
+    # ./wifi.nix
+    # ./wifi2.nix
     ./workspace_code.nix
     ./workspace.nix
     ./workspaces.nix
   ];
+
+  home.file.".config/eww/scripts/airplane_mode.sh" = {
+    source = ./airplane_mode.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/appname.sh" = {
+    source = ./appname.sh;
+		executable = true;
+	};
+
+  home.file.".config/eww/scripts/battery.sh" = {
+    source = ./battery.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/battery2.sh" = {
+    source = ./battery2.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/brightness.sh" = {
+    source = ./brightness.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/mem-ad.sh" = {
+    source = ./mem-ad.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/memory.sh" = {
+    source = ./memory.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/music_info.sh" = {
+    source = ./music_info.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/notifications.sh" = {
+    source = ./notifications.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/pop.sh" = {
+    source = ./pop.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/screenshot.sh" = {
+    source = ./screenshot.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/search.sh" = {
+    source = ./search.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/volume.sh" = {
+    source = ./volume.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/weather.sh" = {
+    source = ./weather.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/wifi.sh" = {
+    source = ./wifi.sh;
+    executable = true;
+  };
+
+  home.file.".config/eww/scripts/wifi2.sh" = {
+    source = ./wifi2.sh;
+    executable = true;
+  };
+
+
 }
---
+++ home/features/desktop/hyprland/eww/scripts/memory.sh
@@ -0,0 +1,5 @@
+
+#!/bin/sh
+
+printf "%.0f\n" $(free -m | grep Mem | awk '{print ($3/$2)*100}')
+
