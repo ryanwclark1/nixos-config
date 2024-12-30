b4b7a5e45961a0a21c106abb3ddd3f53463669bf
Ryan Clark <ryanc@accentservices.com>
2024-12-30 09:30:37 -0600

    Add overlay to autofs5 for patching incompatible function pointer types in cyrus-sasl module

--- hosts/common/optional/nfs.nix
+++ hosts/common/optional/nfs.nix
@@ -5,6 +5,23 @@
 }:
 
 {
+  nixpkgs.overlays = [
+    (
+      final: prev: {
+        autofs5 = prev.autofs5.overrideAttrs (_: pattr: {
+          patches =
+            pattr.patches
+            ++ [
+              (prev.fetchpatch {
+                url = "mirror://kernel/linux/daemons/autofs/v5/patches-5.2.0/autofs-5.1.9-Fix-incompatible-function-pointer-types-in-cyrus-sasl-module.patch";
+                hash = "sha256-erLlqZtVmYqUOsk3S7S50yA0VB8Gzibsv+X50+gcA58=";
+              })
+            ];
+        });
+      }
+    )
+  ];
+
   environment.systemPackages = with pkgs; [
     nfs-utils
   ];
