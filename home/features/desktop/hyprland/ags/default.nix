8ca3dc1ef277ce926c4e02ed5be1a7eb89d880f7
Ryan Clark <ryanc@accentservices.com>
2024-12-30 21:28:32 -0600

    Refactor AGS configuration in Hyprland to include additional packages and clean up unused imports

--- home/features/desktop/hyprland/ags/default.nix
+++ home/features/desktop/hyprland/ags/default.nix
@@ -1,14 +1,13 @@
 {
   config,
-  inputs,
   pkgs,
+  inputs,
   ...
 }:
 
 {
   imports = [
     inputs.ags.homeManagerModules.default
-    # inputs.matugen.nixosModules.default
     # inputs.astal.nixosModules.default
   ];
 
@@ -19,15 +18,29 @@
 
   programs.ags = {
     enable = true;
-
-      # ags.packages.x86_64-linux.battery
-      # ags.packages.x86_64-linux.hyperland
-    # ];
-    # extraPackages = with pkgs; [
-    #   gtksourceview
-    #   webkitgtk_6_0
-    #   accountsservice
-    # ];
+    extraPackages = with pkgs; [
+      inputs.ags.packages.x86_64-linux.battery
+      inputs.ags.packages.x86_64-linux.hyprland
+      inputs.ags.packages.x86_64-linux.tray
+      inputs.ags.packages.x86_64-linux.apps
+      inputs.ags.packages.x86_64-linux.battery
+      inputs.ags.packages.x86_64-linux.bluetooth
+      inputs.ags.packages.x86_64-linux.mpris
+      inputs.ags.packages.x86_64-linux.network
+      inputs.ags.packages.x86_64-linux.notifd
+      inputs.ags.packages.x86_64-linux.powerprofiles
+      inputs.ags.packages.x86_64-linux.wireplumber
+    ] ++ (with pkgs; [
+      typescript
+      dart-sass
+      gobject-introspection
+      (python3.withPackages (ps:
+        with ps; [
+          gpustat
+          dbus-python
+          pygobject3
+        ]))
+    ]);
   };
   # programs.matugen = {
   #   enable = true;
