265799e36d19c3fd5342eeaaf0bcffd96f3e1da0
Ryan Clark <ryanc@accentservices.com>
2024-12-31 12:42:56 -0600

    Remove swww references from Hyprland configuration for cleaner startup

--- home/features/desktop/hyprland/hypr/default.nix
+++ home/features/desktop/hyprland/hypr/default.nix
@@ -12,7 +12,6 @@ let
   cliphist = lib.getExe config.services.cliphist.package;
   steam = "${pkgs.steam}/bin/steam";
   swaync = "${pkgs.swaynotificationcenter}/bin/swaync";
-  swww = "${pkgs.swww}/bin/swww";
   # ags = lib.getExe config.programs.ags.package;
   pwvucontrol = "${pkgs.pwvucontrol}/bin/pwvucontrol";
   nm-connection-editor = "${pkgs.networkmanagerapplet}/bin/nm-connection-editor";
@@ -51,11 +50,9 @@ in
         "dbus-update-activation-environment --systemd --all"
         "systemctl --user import-environment QT_QPA_PLATFORMTHEME WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "${hypridle} &"
-        "killall -q ${swww};sleep .5 && ${swww} init"
         # "killall -q ${ags}; sleep .5 && ${ags} -b hypr"
         "nm-applet --indicator &"
         "${wl-paste} --watch ${cliphist} store"
-        # "sleep 1.5 && swww img /home/${username}/Pictures/Wallpapers/xyz.jpg"
         # "systemctl --user start waybar"
       ];
 
