25b1d13dc724caec6e3182cede1f30d337a8ce12
Ryan Clark <ryanc@accentservices.com>
2025-01-04 15:46:31 -0600

    Update hypridle default.nix to modify timeout settings and change on-timeout actions

--- home/features/desktop/hyprland/hypridle/default.nix
+++ home/features/desktop/hyprland/hypridle/default.nix
@@ -1,4 +1,7 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  ...
+}:
 
 {
   services = {
@@ -16,12 +19,18 @@
 
         listener = [
           {
-            timeout = 900;
-            on-timeout = "hyprlock";
+            timeout = 975;                            # 9 min
+            on-timeout = notify-send "You are idle!"; # command to run when timeout has passed
+            on-resume = notify-send "Welcome back!"; # command to run when user is back
+
+          }
+          {
+            timeout = 1000;
+            on-timeout = "loginctl lock-session";
           }
           # dpms
           {
-            timeout = 1200;
+            timeout = 2000;
             on-timeout = "hyprctl dispatch dpms off";
             on-resume = "hyprctl dispatch dpms on";
           }
