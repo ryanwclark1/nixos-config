e5595c76c97a125c8a32d064d2bd39fee20e7d11
Ryan Clark <ryanc@accentservices.com>
2025-01-04 14:00:24 -0600

    Moved kubernetes tools to kubernetes dir from common

--- home/features/kubernetes/default.nix
+++ home/features/kubernetes/default.nix
@@ -12,5 +12,9 @@
     kubernetes-helm
     minikube
     talosctl
+    kubeshark # Kubernetes packet capture tool
+    seabird # Kubernetes native desktop app that simplifies working with Kubernetes.
+    weave-gitops # GitOps for Kubernetes
+    tilt # Local Kubernetes development Local to manage your developer instance when your team deploys to Kubernetes in production.
   ];
 }
