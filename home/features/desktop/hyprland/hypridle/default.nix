fa5e7e3ed6a6dd70f8cf0ff436f508af1d2dfb6a
Ryan Clark <ryanc@accentservices.com>
2025-01-04 16:02:31 -0600

    Fix command syntax for on-timeout and on-resume actions in default.nix

--- home/features/desktop/hyprland/hypridle/default.nix
+++ home/features/desktop/hyprland/hypridle/default.nix
@@ -20,8 +20,8 @@
         listener = [
           {
             timeout = 975;                            # 9 min
-            on-timeout = notify-send "You are idle!"; # command to run when timeout has passed
-            on-resume = notify-send "Welcome back!"; # command to run when user is back
+            on-timeout = "notify-send 'You are idle!'"; # command to run when timeout has passed
+            on-resume = "notify-send 'Welcome back!'"; # command to run when user is back
 
           }
           {
