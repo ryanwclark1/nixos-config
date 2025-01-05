35e37f4115030c048d6b369c599dcab7f755a73e
ryanwclark1 <36689148+ryanwclark1@users.noreply.github.com>
2025-01-05 16:30:26 -0600

    Clean up flake imports

--- flake.nix
+++ flake.nix
@@ -43,9 +43,6 @@
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
     };
-    # hyprland-qtutils = {
-    #   url = "github:hyprwm/hyprland-qtutils";
-    # };
     stylix = {
       url = "github:danth/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
