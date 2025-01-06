d80805382fd75b8f2a9ef59abc881506dbe0c7b5
Ryan Clark <ryanc@accentservices.com>
2025-01-06 10:59:39 -0600

    Comment out unused extraSetFlags in tailscale.nix and update extraUpFlags

--- hosts/frametop/services/tailscale.nix
+++ hosts/frametop/services/tailscale.nix
@@ -10,26 +10,33 @@
     useRoutingFeatures = lib.mkDefault "client";
     openFirewall = true;
     port = 41641;
-    extraSetFlags = [
-      "--accept-dns=false"
-      "--accept-risk=all"
-      "--accept-routes=true"
-      "--advertise-connector=false"
-      "--advertise-exit-node=false"
+    # extraSetFlags = [
+    #   "--accept-dns=false"
+    #   "--accept-risk=all"
+    #   "--accept-routes=true"
+    #   "--advertise-connector=false"
+    #   "--advertise-exit-node=false"
+    #   "--exit-node=homeassistant"
+    #   "--exit-node-allow-lan-access=true"
+    #   "--force-reauth=false"
+    #   "--json=false"
+    #   "--login-server=https://controlplane.tailscale.com"
+    #   "--netfilter-mode=on"
+    #   "--operator=administrator"
+    #   "--qr=false"
+    #   "--reset=true"
+    #   "--shields-up=false"
+    #   "--snat-subnet-routes=false"
+    #   "--ssh=false"
+    #   "--stateful-filtering=false"
+    #   "--timeout=0s"
+    # ];
+    extraUpFlags = [
       "--exit-node=homeassistant"
-      "--exit-node-allow-lan-access=true"
-      "--force-reauth=false"
-      "--json=false"
-      "--login-server=https://controlplane.tailscale.com"
-      "--netfilter-mode=on"
+      "--exit-node-allow-lan-access"
       "--operator=administrator"
-      "--qr=false"
-      "--reset=true"
-      "--shields-up=false"
-      "--snat-subnet-routes=false"
-      "--ssh=false"
-      "--stateful-filtering=false"
-      "--timeout=0s"
+      "--advertise-tags=tag:client,tag:desktop,tag:no-exit"
+      "--accept-routes=true"
     ];
   };
 
