dd1a68ace18c97dfea63566e5efd88bf035c5e44
Ryan Clark <ryanc@accentservices.com>
2025-01-06 11:49:49 -0600

    Update nix-env command to nix-shell for package installation in frametop_setup.sh

--- frametop_setup.sh
+++ frametop_setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 
-nix-env --install git gnumake
-git clone https://github.com/ryanwclark1/nixos-configs.git ~/nixos-configs
+nix-shell -p git gnumake
+git clone https://github.com/ryanwclark1/nixos-configs ~/nixos-configs
 cp /etc/nixos/hardware-configuration.nix ~/nixos-configs/host/frametop/hardware-configuration.nix
 
 # scp administrator@10.10.100.58:~/.ssh/id_host_rsa.pub ~/.ssh/id_host_rsa.pub
