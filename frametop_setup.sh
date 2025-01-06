63480075328daa915ef01b92b36310d6807b38bb
Ryan Clark <ryanc@accentservices.com>
2025-01-06 11:59:59 -0600

    Update nix-env command syntax and adjust SCP commands in frametop_setup.sh

--- frametop_setup.sh
+++ frametop_setup.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 
-nix-env --install git gnumake
+nix-env -iA git gnumake
 git clone https://github.com/ryanwclark1/nixos-config ~/nixos-config
 cp /etc/nixos/hardware-configuration.nix ~/nixos-config/host/frametop/hardware-configuration.nix
 
-# scp administrator@10.10.100.58:~/.ssh/id_host_rsa.pub ~/.ssh/id_host_rsa.pub
-# scp administrator@10.10.100.58:~/.ssh/id_host_rsa ~/.ssh/id_host_rsa
 scp administrator@10.10.100.58:~/.ssh/ssh_host_ed25519_key.pub ~/.ssh/ssh_host_ed25519_key.pub
 scp administrator@10.10.100.58:~/.ssh/ssh_host_ed25519_key ~/.ssh/ssh_host_ed25519_key
 
