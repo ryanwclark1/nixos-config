0f2279b36736f74861be7890e26e681b22b6fc97
Ryan Clark <ryanc@accentservices.com>
2025-01-06 11:38:33 -0600

    Comment out old SCP commands and update to use new SSH host keys in frametop_setup.sh

--- frametop_setup.sh
+++ frametop_setup.sh
@@ -1,14 +1,14 @@
-!/usr/bin/env bash
+#!/usr/bin/env bash
 
 
 nix-env --install git gnumake
 git clone https://github.com/ryanwclark1/nixos-configs.git ~/nixos-configs
 cp /etc/nixos/hardware-configuration.nix ~/nixos-configs/host/frametop/hardware-configuration.nix
 
-scp administrator@10.10.100.58:~/.ssh/id_host_rsa.pub ~/.ssh/id_host_rsa.pub
-scp administrator@10.10.100.58:~/.ssh/id_host_rsa ~/.ssh/id_host_rsa
-scp administrator@10.10.100.58:~/.ssh/id_host_ed25519.pub ~/.ssh/id_host_ed25519.pub
-scp administrator@10.10.100.58:~/.ssh/id_host_ed25519 ~/.ssh/id_host_ed25519
+# scp administrator@10.10.100.58:~/.ssh/id_host_rsa.pub ~/.ssh/id_host_rsa.pub
+# scp administrator@10.10.100.58:~/.ssh/id_host_rsa ~/.ssh/id_host_rsa
+scp administrator@10.10.100.58:~/.ssh/ssh_host_ed25519_key.pub ~/.ssh/ssh_host_ed25519_key.pub
+scp administrator@10.10.100.58:~/.ssh/ssh_host_ed25519_key ~/.ssh/ssh_host_ed25519_key
 
 systemctl stop efi.automount
 systemctl disable efi.automount
