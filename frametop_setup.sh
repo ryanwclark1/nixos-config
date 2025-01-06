603e9b2f9fa31d94859ca6615d856a62ff19f2f7
Ryan Clark <ryanc@accentservices.com>
2025-01-06 10:59:44 -0600

    Update SCP source IP addresses in frametop_setup.sh

--- frametop_setup.sh
+++ frametop_setup.sh
@@ -5,10 +5,10 @@ nix-env --install git gnumake
 git clone https://github.com/ryanwclark1/nixos-configs.git ~/nixos-configs
 cp /etc/nixos/hardware-configuration.nix ~/nixos-configs/host/frametop/hardware-configuration.nix
 
-scp administrator@10.10.100.147:~/.ssh/id_host_rsa.pub ~/.ssh/id_host_rsa.pub
-scp administrator@10.10.100.147:~/.ssh/id_host_rsa ~/.ssh/id_host_rsa
-scp administrator@10.10.100.147:~/.ssh/id_host_ed25519.pub ~/.ssh/id_host_ed25519.pub
-scp administrator@10.10.100.147:~/.ssh/id_host_ed25519 ~/.ssh/id_host_ed25519
+scp administrator@10.10.100.58:~/.ssh/id_host_rsa.pub ~/.ssh/id_host_rsa.pub
+scp administrator@10.10.100.58:~/.ssh/id_host_rsa ~/.ssh/id_host_rsa
+scp administrator@10.10.100.58:~/.ssh/id_host_ed25519.pub ~/.ssh/id_host_ed25519.pub
+scp administrator@10.10.100.58:~/.ssh/id_host_ed25519 ~/.ssh/id_host_ed25519
 
 systemctl stop efi.automount
 systemctl disable efi.automount
