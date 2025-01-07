b2b7a063c8f4a3e1210a944c28ff2d35f2865561
Ryan Clark <ryanc@accentservices.com>
2025-01-06 18:18:55 -0600

    Uncomment ghostty, qutebrowser, remmina, and virtualisation features in frametop.nix

--- home/frametop.nix
+++ home/frametop.nix
@@ -25,26 +25,24 @@
     ./features/fzf
     ./features/games
     ./features/git
+    ./features/ghostty
     ./features/imv
-    # ./features/helix
-    # ./features/kdeconnect
     ./features/kitty
     ./features/kubernetes
     ./features/lazygit
     ./features/media
     ./features/networking-utils
     ./features/nixvim
-    # ./features/pistol
     ./features/productivity
-    # ./features/qutebrowser
-    # ./features/remmina
+    ./features/qutebrowser
+    ./features/remmina
     ./features/ripgrep
     ./features/slack
     ./features/shell
     ./features/starship
     ./features/sys-stats
     ./features/tmux
-    # ./features/virtviewer
+    ./features/virtualisation
     ./features/vscode
     ./features/yazi
     ./features/zed
