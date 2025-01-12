b359907cfbe9f4263a638bc76699b2323364fc27
Ryan Clark <ryanc@accentservices.com>
2025-01-11 21:10:58 -0600

    Uncomment fprintd and tailscale service imports in default.nix

--- hosts/frametop/services/default.nix
+++ hosts/frametop/services/default.nix
@@ -4,15 +4,14 @@
 
 {
   imports = [
-    # ./auto-cpufreq.nix
-    # ./fprintd.nix
+    ./auto-cpufreq.nix
+    ./fprintd.nix
     ./fwupd.nix
     ./libinput.nix
     ./logind.nix
     ./prometheus-node-exporter.nix
     ./upower.nix
     ./vscode-server.nix
-    # ./tailscale.nix
+    ./tailscale.nix
   ];
-
 }
