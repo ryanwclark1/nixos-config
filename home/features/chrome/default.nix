184d5aff10876efa4507980bf155b11c41128d92
Ryan Clark <ryanc@accentservices.com>
2025-01-21 14:11:03 -0600

    Create a google chrome alias that can be used universally

--- home/features/chrome/default.nix
+++ home/features/chrome/default.nix
@@ -8,9 +8,9 @@
     (pkgs.google-chrome.override {
       commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland --force-dark-mode";
     })
+    (pkgs.writeShellScriptBin "google-chrome" ''
+       google-chrome-stable "$@"
+    '')
   ];
 
-  home.shellAliases = {
-    google-chrome-stable = "google-chrome";
-  };
 }
