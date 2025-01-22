b058656e0ef2f6448d146f27a7b4947f09534e3d
Ryan Clark <ryanc@accentservices.com>
2025-01-22 08:37:29 -0600

    Add default configurations for navi and tealdeer programs

---
+++ home/features/navi/default.nix
@@ -0,0 +1,37 @@
+# An interactive cheatsheet tool for the command-line
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  programs.navi = {
+    enable = true;
+    package = pkgs.navi;
+    # conflicts with Control G -> GH
+    enableBashIntegration = lib.mkIf config.programs.bash.enable false;
+    enableFishIntegration = lib.mkIf config.programs.fish.enable false;
+    enableZshIntegration = lib.mkIf config.programs.zsh.enable false;
+    settings = {
+      finder = {
+        command = "fzf";
+      #   # overrides: --tac # equivalent to the --fzf-overrides option
+      #   # overrides_var: --tac # equivalent to the --fzf-overrides-var option
+      };
+      # cheats = {
+      #   paths = [];
+      # };
+      # search:
+      #   tags: git,!checkout # equivalent to the --tag-rules option
+      client = {
+        tealdeer = true;
+      };
+      # shell = {
+      #   command = "bash";
+      #   finder_command = "bash";
+      # };
+    };
+  };
+}
