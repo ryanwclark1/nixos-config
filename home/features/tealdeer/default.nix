03dabbf36bc126bc34c27f5b02be427f8a5e12fd
Ryan Clark <ryanc@accentservices.com>
2025-01-22 08:37:29 -0600

    Add default configurations for navi and tealdeer programs

---
+++ home/features/tealdeer/default.nix
@@ -0,0 +1,20 @@
+# Tealdeer is a very fast implementation of tldr
+{
+  ...
+}:
+
+{
+  programs.tealdeer = {
+    enable = true;
+    settings = {
+      display = {
+        compact = false;
+        use_pager = true;
+      };
+      updates = {
+        auto_update = true;
+        auto_update_interval_hours = 24;
+      };
+    };
+  };
+}
