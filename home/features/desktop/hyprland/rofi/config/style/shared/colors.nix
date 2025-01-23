26a324c0caf3bdb450dfef2fea47783042e69d54
Ryan Clark <ryanc@accentservices.com>
2025-01-23 14:05:22 -0600

    Update Rofi color scheme with new color values for improved aesthetics

--- home/features/desktop/hyprland/rofi/config/style/shared/colors.nix
+++ home/features/desktop/hyprland/rofi/config/style/shared/colors.nix
@@ -6,12 +6,28 @@
   home.file.".config/rofi/style/shared/colors.rasi" = {
     text = ''
       * {
-        background:     #1E1D2FFF;
-        background-alt: #282839FF;
-        foreground:     #D9E0EEFF;
-        selected:       #7AA2F7FF;
-        active:         #ABE9B3FF;
-        urgent:         #F28FADFF;
+        background:     #303446;
+        background-alt: #303446;
+        foreground:     #c6d0f5;
+        selected:       #8caaee;
+        active:         #a6d189;
+        urgent:         #e78284;
+        base00:         #303446;
+        base01:         #292c3c;
+        base02:         #414559;
+        base03:         #51576d;
+        base04:         #626880;
+        base05:         #c6d0f5;
+        base06:         #f2d5cf;
+        base07:         #babbf1;
+        base08:         #e78284;
+        base09:         #ef9f76;
+        base0A:         #e5c890;
+        base0B:         #a6d189;
+        base0C:         #81c8be;
+        base0D:         #8caaee;
+        base0E:         #ca9ee6;
+        base0F:         #eebebe;
       }
     '';
     executable = false;
