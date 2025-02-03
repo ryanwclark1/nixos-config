cc26ffbd8c19d71dfedb1a7cdbc419754850fc3b
Ryan Clark <ryanc@accentservices.com>
2025-02-02 22:37:04 -0600

    Remove unused font packages from default.nix for cleaner configuration

--- home/features/fonts/default.nix
+++ home/features/fonts/default.nix
@@ -6,31 +6,19 @@
 {
   home.packages = with pkgs; [
     # Fonts
-    nerd-fonts._0xproto
-    nerd-fonts._3270
-    nerd-fonts.agave
-    nerd-fonts.anonymice
     nerd-fonts.arimo
-    nerd-fonts.aurulent-sans-mono
     nerd-fonts.bigblue-terminal
-    nerd-fonts.bitstream-vera-sans-mono
-    nerd-fonts.blex-mono
-    nerd-fonts.caskaydia-cove
-    nerd-fonts.caskaydia-mono
     nerd-fonts.code-new-roman
     nerd-fonts.comic-shanns-mono
     nerd-fonts.commit-mono
     nerd-fonts.cousine
     nerd-fonts.d2coding
-    nerd-fonts.daddy-time-mono
     nerd-fonts.dejavu-sans-mono
     nerd-fonts.departure-mono
     nerd-fonts.droid-sans-mono
     nerd-fonts.envy-code-r
-    nerd-fonts.fantasque-sans-mono
     nerd-fonts.fira-code
     nerd-fonts.fira-mono
-    nerd-fonts.monaspace
     nerd-fonts.mplus
     nerd-fonts.noto
     nerd-fonts.ubuntu
