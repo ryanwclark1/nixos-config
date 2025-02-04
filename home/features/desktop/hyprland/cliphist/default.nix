bc959a9aec0035decae37f2a3ba2faeb2737a06b
Ryan Clark <ryanc@accentservices.com>
2025-02-03 18:35:10 -0600

    Remove unnecessary extraOptions from cliphist configuration

--- home/features/desktop/hyprland/cliphist/default.nix
+++ home/features/desktop/hyprland/cliphist/default.nix
@@ -8,11 +8,5 @@
     enable = true;
     package = pkgs.cliphist;
     allowImages = true;
-    extraOptions = [
-      "-max-dedupe-search"
-      "10"
-      "-max-items"
-      "500"
-    ];
   };
 }
