6e810fdbe76458f6d831625a42bc95de08c4b324
Ryan Clark <ryanc@accentservices.com>
2025-02-09 15:06:34 -0600

    Enhance networking-utils by adding sngrep and updating comments; conditionally include ethtool for x86_64-linux

--- home/features/networking-utils/default.nix
+++ home/features/networking-utils/default.nix
@@ -6,17 +6,21 @@
 {
   home.packages = with pkgs; [
     dnsutils # `dig` + `nslookup`
-    ngrep
-    nload
+    ngrep # Network packet analyzer.
+    sngrep # CLI tool for visualizing SIP messages in a terminal UI
+    nload # Monitors network traffic and bandwidth usage with ncurses graphs.
     nmap # A utility for network discovery and security auditing
     openresolv # a resolv.conf management framework
     rathole # similar to ngrok - NAT traversal and reverse proxy
-    tcpdump
-    wireguard-tools
+    tcpdump # Network sniffer
+    wireguard-tools # Supplies the main userspace tooling for using and configuring WireGuard tunnels, including the wg(8) and wg-quick(8) utilities.
     netscanner # A simple network scanner
     mtr # A network diagnostic tool
-    iperf
-    ethtool # A utility for controlling network drivers and hardware
+    iperf # Tool to measure IP bandwidth using UDP or TCP.
     socat # replacement of openbsd-netcat
-  ];
+  ] ++ (if pkgs.stdenv.hostPlatform.system == "x86_64-linux" then [
+    ethtool # A utility for controlling network drivers and hardware
+  ] else []);
+
+
 }
