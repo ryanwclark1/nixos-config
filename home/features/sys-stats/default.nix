57b0c210f97643a9b907ade76f79671142bbea38
Ryan Clark <ryanc@accentservices.com>
2025-02-09 15:06:44 -0600

    Update sys-stats configuration to conditionally include sysstat and dmidecode for x86_64-linux

--- home/features/sys-stats/default.nix
+++ home/features/sys-stats/default.nix
@@ -6,10 +6,12 @@
 # inxi -Fxxxmprz <--- all info with filters
 {
   home.packages = with pkgs; [
-    sysstat
-    inxi
-    dmidecode
-    glxinfo
-    libgtop
-  ];
+    inxi # Full featured CLI system information tool.
+    glxinfo # Collection of demos and test programs for OpenGL and Mesa.
+    libgtop # Library that reads information about processes and the running system.
+  ] ++ (if pkgs.stdenv.hostPlatform.system == "x86_64-linux" then [
+    sysstat # Collection of performance monitoring tools for Linux (such as sar, iostat and pidstat)
+    dmidecode # Tool that reads information about your system's hardware from the BIOS according to the SMBIOS/DMI standard.
+  ] else []);
 }
+
