4d8939d948bc97820240c589c378c7404bbefaed
Ryan Clark <ryanc@accentservices.com>
2025-02-10 16:51:11 -0600

    Refactor wlogout configuration to use power management script for actions and add style.css for improved UI

--- home/features/desktop/hyprland/wlogout/default.nix
+++ home/features/desktop/hyprland/wlogout/default.nix
@@ -1,7 +1,5 @@
 {
   pkgs,
-  config,
-
   ...
 }:
 
@@ -11,104 +9,103 @@
     recursive = true;
   };
 
+  home.file."..config/wlogout/style.css" = {
+    source = ./style.css;
+  };
+
   programs.wlogout = {
     enable = true;
     package = pkgs.wlogout;
     layout = [
       {
         label = "lock";
-        action = "sleep 1; hyprlock";
+        action = "~/.config/hypr/scripts/power.sh lock";
         text = "Lock";
         keybind = "l";
       }
       {
-        label = "hibernate";
-        action = "systemctl hibernate  ";
-        text = "Hibernate";
-        keybind = "h";
-      }
-      {
         label = "logout";
-        action = "sleep 1; hyprctl dispatch exit";
+        action = "~/.config/hypr/scripts/power.sh logout";
         text = "Exit";
         keybind = "e";
       }
       {
-        label = "shutdown";
-        action = "sleep 1; systemctl poweroff";
-        text = "Shutdown";
-        keybind = "s";
-      }
-      {
         label = "suspend";
-        action = "sleep 1; systemctl suspend ";
+        action = "~/.config/hypr/scripts/power.sh suspend";
         text = "Suspend";
         keybind = "u";
       }
       {
         label = "reboot";
-        action = "sleep 1; systemctl reboot";
+        action = "~/.config/hypr/scripts/power.sh reboot";
         text = "Reboot";
         keybind = "r";
       }
+      {
+        label = "shutdown";
+        action = "~/.config/hypr/scripts/power.sh shutdown";
+        text = "Shutdown";
+        keybind = "s";
+      }
+
     ];
-    style = ''
-      * {
-        font-family: "UbuntuMono Nerd Font", FontAwesome, sans-serif;
-      	background-image: none;
-      	transition: 20ms;
-      }
-      window {
-      	background-color: rgba(12, 12, 12, 0.1);
-      }
-      button {
-      	color: #${config.lib.stylix.colors.base05};
-        font-size:20px;
-        background-repeat: no-repeat;
-      	background-position: center;
-      	background-size: 25%;
-      	border-style: solid;
-      	background-color: rgba(12, 12, 12, 0.3);
-      	border: 3px solid #${config.lib.stylix.colors.base05};
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-      }
-      /* button:hover */
-      button:focus,
-      button:active {
-        color: #${config.lib.stylix.colors.base0E};
-        background-color: rgba(12, 12, 12, 0.5);
-        border: 3px solid #${config.lib.stylix.colors.base0E};
-      }
-      #logout {
-      	margin: 10px;
-      	border-radius: 20px;
-      	background-image: image(url("icons/logout.png"));
-      }
-      #suspend {
-      	margin: 10px;
-      	border-radius: 20px;
-      	background-image: image(url("icons/suspend.png"));
-      }
-      #shutdown {
-      	margin: 10px;
-      	border-radius: 20px;
-      	background-image: image(url("icons/shutdown.png"));
-      }
-      #reboot {
-      	margin: 10px;
-      	border-radius: 20px;
-      	background-image: image(url("icons/reboot.png"));
-      }
-      #lock {
-      	margin: 10px;
-      	border-radius: 20px;
-      	background-image: image(url("icons/lock.png"));
-      }
-      #hibernate {
-      	margin: 10px;
-      	border-radius: 20px;
-      	background-image: image(url("icons/hibernate.png"));
-      }
-    '';
+    # style = ''
+    #   * {
+    #     font-family: "UbuntuMono Nerd Font", FontAwesome, sans-serif;
+    #   	background-image: none;
+    #   	transition: 20ms;
+    #   }
+    #   window {
+    #   	background-color: rgba(12, 12, 12, 0.1);
+    #   }
+    #   button {
+    #   	color: #${config.lib.stylix.colors.base05};
+    #     font-size:20px;
+    #     background-repeat: no-repeat;
+    #   	background-position: center;
+    #   	background-size: 25%;
+    #   	border-style: solid;
+    #   	background-color: rgba(12, 12, 12, 0.3);
+    #   	border: 3px solid #${config.lib.stylix.colors.base05};
+    #     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    #   }
+    #   /* button:hover */
+    #   button:focus,
+    #   button:active {
+    #     color: #${config.lib.stylix.colors.base0E};
+    #     background-color: rgba(12, 12, 12, 0.5);
+    #     border: 3px solid #${config.lib.stylix.colors.base0E};
+    #   }
+    #   #logout {
+    #   	margin: 10px;
+    #   	border-radius: 20px;
+    #   	background-image: image(url("icons/logout.png"));
+    #   }
+    #   #suspend {
+    #   	margin: 10px;
+    #   	border-radius: 20px;
+    #   	background-image: image(url("icons/suspend.png"));
+    #   }
+    #   #shutdown {
+    #   	margin: 10px;
+    #   	border-radius: 20px;
+    #   	background-image: image(url("icons/shutdown.png"));
+    #   }
+    #   #reboot {
+    #   	margin: 10px;
+    #   	border-radius: 20px;
+    #   	background-image: image(url("icons/reboot.png"));
+    #   }
+    #   #lock {
+    #   	margin: 10px;
+    #   	border-radius: 20px;
+    #   	background-image: image(url("icons/lock.png"));
+    #   }
+    #   #hibernate {
+    #   	margin: 10px;
+    #   	border-radius: 20px;
+    #   	background-image: image(url("icons/hibernate.png"));
+    #   }
+    # '';
   };
 }
