cc3463f70330233f61710e7fc61342f6583905e9
Ryan Clark <ryanc@accentservices.com>
2025-02-10 10:57:33 -0600

    Enable SPICE service and adjust configuration for guest agent and WebDAV support

--- hosts/woody/services/default.nix
+++ hosts/woody/services/default.nix
@@ -7,7 +7,7 @@
     ./fwupd.nix
     ./gvfs.nix
     ./prometheus-node-exporter.nix
-    # ./spice.nix
+    ./spice.nix
     ./vscode-server.nix
     ./xserver.nix
     ./tailscale.nix
--- hosts/woody/services/spice.nix
+++ hosts/woody/services/spice.nix
@@ -6,18 +6,17 @@
 {
   services = {
     # Enable SPICE guest agent functionalities (clipboard sharing, resolution resizing, etc.)
-    spice-vdagentd.enable = true;
+    # spice-vdagentd.enable = true;
 
     # Enable WebDAV support over SPICE for file sharing between host and guest
     spice-webdavd = {
       enable = true;
-      package = pkgs.pkgs.phodav;
     };
 
     # Enable automatic display configuration for SPICE sessions
     spice-autorandr = {
       enable = true;
-      package = pkgs.spice-autorandr;
+
     };
   };
 }
