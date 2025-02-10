d066cb1b870b079e9db27aeb10c54566e4566b7b
Ryan Clark <ryanc@accentservices.com>
2025-02-09 21:54:08 -0600

    Update Rofi powermenu script to enhance icon representation and improve command handling

--- home/features/desktop/hyprland/rofi/powermenu/type-4/powermenu.sh
+++ home/features/desktop/hyprland/rofi/powermenu/type-4/powermenu.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-## Author : Aditya Shakya (adi1090x)
-## Github : @adi1090x
-#
-## Rofi   : Power Menu
-#
-## Available Styles
-#
-## style-1   style-2   style-3   style-4   style-5
-
 # Current Theme
 dir="$HOME/.config/rofi/powermenu/type-4"
 theme='style-3'
@@ -18,28 +9,27 @@ uptime="`uptime -p | sed -e 's/up //g'`"
 host=`hostname`
 
 # Options
-shutdown=''
-reboot=''
-lock=''
-suspend=''
-logout=''
-yes=''
-no=''
+shutdown=' '
+reboot=''
+lock=''
+suspend='󰏦'
+logout='󰍃'
+yes=' '
+no=' '
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -dmenu \
-		-p "Goodbye ${USER}" \
-		-mesg "Uptime: $uptime" \
-		-theme ${dir}/${theme}.rasi
+  rofi -dmenu \
+    -p "Goodbye $USER" \
+    -mesg "Uptime: $uptime" \
+    -theme "$dir/$theme.rasi"
 }
-
 # Confirmation CMD
 confirm_cmd() {
 	rofi -dmenu \
 		-p 'Confirmation' \
 		-mesg 'Are you Sure?' \
-		-theme ${dir}/shared/confirm.rasi
+		-theme $dir/shared/confirm.rasi
 }
 
 # Ask for confirmation
@@ -82,24 +72,24 @@ run_cmd() {
 
 # Actions
 chosen="$(run_rofi)"
-case ${chosen} in
-    $shutdown)
-		run_cmd --shutdown
-        ;;
-    $reboot)
-		run_cmd --reboot
-        ;;
-    $lock)
-		if [[ -x '/usr/bin/betterlockscreen' ]]; then
-			betterlockscreen -l
-		elif [[ -x '/usr/bin/i3lock' ]]; then
-			i3lock
-		fi
-        ;;
-    $suspend)
-		run_cmd --suspend
-        ;;
-    $logout)
-		run_cmd --logout
-        ;;
+case $chosen in
+  $shutdown)
+    run_cmd --shutdown
+    ;;
+  $reboot)
+    run_cmd --reboot
+    ;;
+  $lock)
+    if command -v hyprlock &> /dev/null; then
+      hyprlock
+    else
+      echo "hyprlock does not exist."
+    fi
+    ;;
+  $suspend)
+    run_cmd --suspend
+    ;;
+  $logout)
+    run_cmd --logout
+    ;;
 esac
