2229913d8eeef83d13af389082354dfa6b5d769e
Ryan Clark <ryanc@accentservices.com>
2025-02-10 20:39:56 -0600

    Add hyprlock configuration file and update settings for improved functionality

--- home/features/desktop/hyprland/hyprlock/default.nix
+++ home/features/desktop/hyprland/hyprlock/default.nix
@@ -1,91 +1,99 @@
 {
-  config,
+  # config,
   pkgs,
   ...
 }:
 
+
 {
+  home.file."hyprlock.conf" = {
+    source = ./hyprlock.conf;
+  };
+
   programs = {
     hyprlock = {
       enable = true;
       package = pkgs.hyprlock;
-      settings = {
-        general = {
-          disable_loading_bar = true;
-          hide_cursor = false;
-          grace = 0;
-          no_fade_in = false;
-          no_fade_out = true;
-          text_trim = true;
-          fractional_scaling = 2;
-        };
+      # settings = {
+      #   general = {
+      #     ignore_empty_input = true;
+      #     # disable_loading_bar = true;
+      #     # hide_cursor = false;
+      #     # grace = 0;
+      #     # no_fade_in = false;
+      #     # no_fade_out = true;
+      #     # text_trim = true;
+      #     # fractional_scaling = 2;
+      #   };
 
-        auth = {
-          # fingerprint = lib.mkIf config.services.fprintd.enable true;
-          fingerprint = {
-            enabled = true;
-          };
-        };
+      #   auth = {
+      #     # fingerprint = lib.mkIf config.services.fprintd.enable true;
+      #     fingerprint = {
+      #       enabled = true;
+      #     };
+      #   };
 
-        # BACKGROUND
-        background = {
-          # monitor =
-          path = "${config.stylix.image}";
-          color = "rgb(48,52,70)";
-          blur_passes = 1; # 0 disables blurring
-          blur_size = 7;
-          noise = 0.0117;
-          contrast = 0.8916;
-          brightness = 0.8172;
-          vibrancy = 0.1696;
-          vibrancy_darkness = 0.05;
-          reload_time = -1;
-          crossfade_time = -1.0;
-          zindex = -1;
-        };
+      #   # BACKGROUND
+      #   background = {
+      #     # monitor =
+      #     path = "$HOME/.config/ml4w/cache/blurred_wallpaper.png";
+      #     # color = "rgb(48,52,70)";
+      #     # blur_passes = 1; # 0 disables blurring
+      #     # blur_size = 7;
+      #     # noise = 0.0117;
+      #     # contrast = 0.8916;
+      #     # brightness = 0.8172;
+      #     # vibrancy = 0.1696;
+      #     # vibrancy_darkness = 0.05;
+      #     # reload_time = -1;
+      #     # crossfade_time = -1.0;
+      #     # zindex = -1;
+      #   };
 
-        # TIME
-        label = {
-          # monitor =
-          text = "$TIME";
-          color = "rgb(48,52,70)";
-          font_size = 72;
-          font_family = "DejaVu Sans";
-          shadow_passes = 1;
-          shadow_size = 1;
-          rotate = 0;
-          position = "0, -200";
-          halign = "center";
-          valign = "top";
-        };
 
-        # INPUT FIELD
-        input-field = {
-          # monitor =
-          size = "400, 60";
-          outline_thickness = 1;
-          dots_size = 0.33; # Scale of input-field height, 0.2 - 0.8
-          dots_spacing = 0.15; # Scale of dots' absolute size, 0.0 - 1.0
-          dots_center = false;
-          dots_rounding = -1; # -1 default circle, -2 follow input-field rounding
-          # outer_color = $accent;
-          inner_color = "rgb(198,208,245)";
-          font_color = "rgb(48,52,70)";
-          fade_on_empty = false;
-          placeholder_text = "<span><i>󰌾  Logged in as </i><span>$USER</span></span>";
-          hide_input = false;
-          # check_color = $accent;
-          fail_color = "rgb(231,130,132)";
-          fail_text = ''<i>$FAIL <b>($ATTEMPTS)</b></i>'';
-          capslock_color = "rgb(229,200,144)";
-          numlock_color = "rgb(229,200,144)";
-          bothlock_color = "rgb(229,200,144)";
-          position = "0, -100";
-          halign = "center";
-          valign = "center";
-          zindex = 0;
-        };
-      };
+
+      #   # INPUT FIELD
+      #   input-field = {
+      #     # monitor =
+      #     size = "200, 50";
+      #     outline_thickness = 3;
+      #     dots_size = 0.33; # Scale of input-field height, 0.2 - 0.8
+      #     dots_spacing = 0.15; # Scale of dots' absolute size, 0.0 - 1.0
+      #     dots_center = false;
+      #     dots_rounding = -1; # -1 default circle, -2 follow input-field rounding
+      #     # outer_color = $accent;
+      #     inner_color = "rgb(198,208,245)";
+      #     font_color = "rgb(48,52,70)";
+      #     fade_on_empty = false;
+      #     placeholder_text = "<span><i>󰌾  Logged in as </i><span>$USER</span></span>";
+      #     hide_input = false;
+      #     # check_color = $accent;
+      #     fail_color = "rgb(231,130,132)";
+      #     fail_text = ''<i>$FAIL <b>($ATTEMPTS)</b></i>'';
+      #     capslock_color = "rgb(229,200,144)";
+      #     numlock_color = "rgb(229,200,144)";
+      #     bothlock_color = "rgb(229,200,144)";
+      #     position = "0, -100";
+      #     halign = "center";
+      #     valign = "center";
+      #     zindex = 0;
+      #   };
+
+      #   # TIME
+      #   label = {
+      #     # monitor =
+      #     text = "$TIME";
+      #     color = "rgb(48,52,70)";
+      #     font_size = 72;
+      #     font_family = "DejaVu Sans";
+      #     shadow_passes = 1;
+      #     shadow_size = 1;
+      #     rotate = 0;
+      #     position = "0, -200";
+      #     halign = "center";
+      #     valign = "top";
+      #   };
+      # };
     };
   };
 }
