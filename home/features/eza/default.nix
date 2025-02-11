f9487c21acf032ac96d83c9f81e642117910ce58
Ryan Clark <ryanc@accentservices.com>
2025-02-10 21:24:10 -0600

    Fix bash integration condition in eza configuration

--- home/features/eza/default.nix
+++ home/features/eza/default.nix
@@ -18,7 +18,7 @@
       "--octal-permissions"
       "--hyperlink"
     ];
-    enableBashIntegration = lib.mkIf config.programs.eza.enable true;
+    enableBashIntegration = lib.mkIf config.programs.bash.enable true;
     enableFishIntegration = lib.mkIf config.programs.fish.enable true;
     enableZshIntegration = lib.mkIf config.programs.zsh.enable true;
     enableNushellIntegration = lib.mkIf config.programs.nushell.enable true;
