09e8b913722710c13e68e090073897612c8b4e36
Ryan Clark <ryanc@accentservices.com>
2025-02-11 13:37:50 -0600

    Add new color templates for wal and update existing configurations

--- home/features/desktop/hyprland/wal/templates/colors-wal-dmenu.h
+++ home/features/desktop/hyprland/wal/templates/colors-wal-dmenu.h
@@ -1,6 +1,6 @@
-static const char *colors[SchemeLast][2] = {{
+static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = {{ "{color15}", "{color0}" }},
-	[SchemeSel] = {{ "{color15}", "{color1}" }},
-	[SchemeOut] = {{ "{color15}", "{color14}" }},
-}};
+	[SchemeNorm] = { "{{ color15 }}", "{{ color0 }}" },
+	[SchemeSel] = { "{{ color15 }}", "{{ color1 }}" },
+	[SchemeOut] = { "{{ color15 }}", "{{ color14 }}" },
+};
--- home/features/desktop/hyprland/wal/templates/colors-wal-st.h
+++ home/features/desktop/hyprland/wal/templates/colors-wal-st.h
@@ -1,30 +1,30 @@
-const char *colorname[] = {{
+const char *colorname[] = {
 
   /* 8 normal colors */
-  [0] = "{color0}", /* black   */
-  [1] = "{color1}", /* red     */
-  [2] = "{color2}", /* green   */
-  [3] = "{color3}", /* yellow  */
-  [4] = "{color4}", /* blue    */
-  [5] = "{color5}", /* magenta */
-  [6] = "{color6}", /* cyan    */
-  [7] = "{color7}", /* white   */
+  [0] = "{{ color0 }}", /* black   */
+  [1] = "{{ color1 }}", /* red     */
+  [2] = "{{ color2 }}", /* green   */
+  [3] = "{{ color3 }}", /* yellow  */
+  [4] = "{{ color4 }}", /* blue    */
+  [5] = "{{ color5 }}", /* magenta */
+  [6] = "{{ color6 }}", /* cyan    */
+  [7] = "{{ color7 }}", /* white   */
 
   /* 8 bright colors */
-  [8]  = "{color8}",  /* black   */
-  [9]  = "{color9}",  /* red     */
-  [10] = "{color10}", /* green   */
-  [11] = "{color11}", /* yellow  */
-  [12] = "{color12}", /* blue    */
-  [13] = "{color13}", /* magenta */
-  [14] = "{color14}", /* cyan    */
-  [15] = "{color15}", /* white   */
+  [8]  = "{{ color8 }}",  /* black   */
+  [9]  = "{{ color9 }}",  /* red     */
+  [10] = "{{ color10 }}", /* green   */
+  [11] = "{{ color11 }}", /* yellow  */
+  [12] = "{{ color12 }}", /* blue    */
+  [13] = "{{ color13 }}", /* magenta */
+  [14] = "{{ color14 }}", /* cyan    */
+  [15] = "{{ color15 }}", /* white   */
 
   /* special colors */
-  [256] = "{background}", /* background */
-  [257] = "{foreground}", /* foreground */
-  [258] = "{cursor}",     /* cursor */
-}};
+  [256] = "{{ background }}", /* background */
+  [257] = "{{ foreground }}", /* foreground */
+  [258] = "{{cursor}}",     /* cursor */
+};
 
 /* Default colors (colorname index)
  * foreground, background, cursor */
