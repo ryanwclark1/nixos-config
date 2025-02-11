9339ed6bcec5728deea23ab95afc1b9c577fa9fd
Ryan Clark <ryanc@accentservices.com>
2025-02-11 13:39:34 -0600

    Update cursor navigation commands in yazi configuration Deprecated command: `arrow -99999999` and `arrow 99999999` have been deprecated, please use `arrow top` and `arrow bot` instead, in your `keymap.toml`.

--- home/features/yazi/default.nix
+++ home/features/yazi/default.nix
@@ -235,12 +235,12 @@
         }
         {
           on = [ "g" "g" ];
-          run = "arrow -99999999";
+          run = "arrow top";
           desc = "Move cursor to the top";
         }
         {
           on = "G";
-          run = "arrow 99999999";
+          run = "arrow bottom";
           desc = "Move cursor to the bottom";
         }
         # Navigation
