48145723dfeb946c28dcbaba54e8c1abfd7b706b
Ryan Clark <ryanc@accentservices.com>
2025-02-12 08:52:40 -0600

    Add audio configuration files for Cava, MPD, MPRIS, ncmpcpp, playerctl, and Spotify

---
+++ home/features/audio/default.nix
@@ -0,0 +1,18 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  imports = [
+    ./cava.nix
+    ./mpris.nix
+    ./ncmp.nix
+    ./playerctl.nix
+    ./spotify.nix
+  ];
+
+  home.packages = with pkgs; [
+    termusic
+  ];
+}
