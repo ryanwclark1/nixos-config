83c9cd7b47b067a0d8907c3bd1d1f8ecb17fe248
Ryan Clark <ryanc@accentservices.com>
2025-02-12 08:50:36 -0600

    Update MPD service configuration to disable startWhenNeeded and add PipeWire audio output

--- hosts/woody/services/mpd.nix
+++ hosts/woody/services/mpd.nix
@@ -1,19 +1,29 @@
 {
+  pkgs,
   ...
 }:
 
 {
   services.mpd = {
     enable = true;
-    startWhenNeeded = true;
+    startWhenNeeded = false;
     musicDirectory = "/mnt/share/music"; # Mounts to local nfs directory nfs:// did not work
     playlistDirectory = "/mnt/share/music/playlists";
     dbFile = "/var/lib/mpd/tag_cache";
     user = "mpd";
     group = "audio";
+    extraConfig = ''
+      audio_output {
+        type "pipewire"
+        name "PipeWire Sound Server"
+      }
+    '';
     network = {
       listenAddress = "127.0.0.1"; # "any" if you want to allow non-localhost connections
       port = 6600;
     };
   };
+
+
+
 }
