c607acc5f4745ace7830f6883524299d31766c1a
Ryan Clark <ryanc@accentservices.com>
2025-02-12 09:45:04 -0600

    Enhance cliphist configuration with additional options and systemd target

--- home/features/desktop/hyprland/cliphist/default.nix
+++ home/features/desktop/hyprland/cliphist/default.nix
@@ -1,12 +1,17 @@
 {
-  pkgs,
   ...
 }:
 
 {
   services.cliphist = {
     enable = true;
-    package = pkgs.cliphist;
     allowImages = true;
+    extraOptions = [
+      "-max-dedupe-search"
+      "10"
+      "-max-items"
+      "500"
+    ];
+    systemdTarget = "hyprland-session.target";
   };
 }
