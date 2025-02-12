0551f4183ca5d79a82c2108f3079247be1e8066c
Ryan Clark <ryanc@accentservices.com>
2025-02-12 09:00:28 -0600

    Add mpc to audio packages and update mpris configuration for MPD

--- home/features/audio/mpris.nix
+++ home/features/audio/mpris.nix
@@ -1,19 +1,22 @@
 {
-  config,
   pkgs,
   ...
 }:
 
 {
   services.mpdris2 = {
-    enabled = true;
+    enable = true;
     package = pkgs.mpdris2;
     multimediaKeys = true;
     notifications = true;
     mpd = {
-      host = config.services.mpd.network.listenAddress;
-      musicDirectory = config.services.mpd.musicDirectory;
-      port = config.services.mpd.network.port;
-    }
+      host = "127.0.0.1"; # "any" if you want to allow non-localhost connections
+      port = 6600;
+      musicDirectory = "/mnt/share/music";
+      # config doesn't work as service is not home-manager service
+      # host = config.services.mpd.network.listenAddress;
+      # musicDirectory = config.services.mpd.musicDirectory;
+      # port = config.services.mpd.network.port;
+    };
   };
 }
