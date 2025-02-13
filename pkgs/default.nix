c9c4fcaebf929f378167102328e5e0e41350c561
Ryan Clark <ryanc@accentservices.com>
2025-02-12 21:16:23 -0600

    Remove commented-out colorscheme generation code in default.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -12,11 +12,11 @@
   allWallpapers = pkgs.linkFarmFromDrvs "wallpapers" (pkgs.lib.attrValues wallpapers);
 
     # And colorschemes based on it
-  generateColorscheme = import ./colorschemes/generator.nix {inherit pkgs;};
-  colorschemes = import ./colorschemes {inherit pkgs wallpapers generateColorscheme;};
-  allColorschemes = let
-    # This is here to help us keep IFD cached (hopefully)
-    combined = pkgs.writeText "colorschemes.json" (builtins.toJSON (pkgs.lib.mapAttrs (_: drv: drv.imported) colorschemes));
-  in
-    pkgs.linkFarmFromDrvs "colorschemes" (pkgs.lib.attrValues colorschemes ++ [combined]);
+  # generateColorscheme = import ./colorschemes/generator.nix {inherit pkgs;};
+  # colorschemes = import ./colorschemes {inherit pkgs wallpapers generateColorscheme;};
+  # allColorschemes = let
+  #   # This is here to help us keep IFD cached (hopefully)
+  #   combined = pkgs.writeText "colorschemes.json" (builtins.toJSON (pkgs.lib.mapAttrs (_: drv: drv.imported) colorschemes));
+  # in
+  #   pkgs.linkFarmFromDrvs "colorschemes" (pkgs.lib.attrValues colorschemes ++ [combined]);
 }
