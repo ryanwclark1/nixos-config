0842250b212245b29fbf548ad76b74b7b455dba0
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-02-16 17:06:38 -0600

    Refactor lazygit theme configuration by introducing color variables for improved readability and maintainability

--- home/features/lazygit/default.nix
+++ home/features/lazygit/default.nix
@@ -3,7 +3,32 @@
   pkgs,
   ...
 }:
-
+let
+  base00 = "303446"; # base
+  base01 = "292c3c"; # mantle
+  base02 = "414559"; # surface0
+  base03 = "51576d"; # surface1
+  base04 = "626880"; # surface2
+  base05 = "c6d0f5"; # text
+  base06 = "f2d5cf"; # rosewater
+  base07 = "babbf1"; # lavender
+  base08 = "e78284"; # red
+  base09 = "ef9f76"; # peach
+  base0A = "e5c890"; # yellow
+  base0B = "a6d189"; # green
+  base0C = "81c8be"; # teal
+  base0D = "8caaee"; # blue
+  base0E = "ca9ee6"; # mauve
+  base0F = "eebebe"; # flamingo
+  base10 = "292c3c"; # mantle - darker background
+  base11 = "232634"; # crust - darkest background
+  base12 = "ea999c"; # maroon - bright red
+  base13 = "f2d5cf"; # rosewater - bright yellow
+  base14 = "a6d189"; # green - bright green
+  base15 = "99d1db"; # sky - bright cyan
+  base16 = "85c1dc"; # sapphire - bright blue
+  base17 = "f4b8e4"; # pink - bright purple
+in
 {
   programs = {
     lazygit = {
@@ -16,36 +41,36 @@
         };
         theme = {
           activeBorderColor = [
-            "#ca9ee6"
+            "#${base0E}"
             "bold"
           ];
           inactiveBorderColor = [
-            "#a5adce"
+            "#${base05}"
           ];
           optionsTextColor = [
-            "#8caaee"
+            "#${base0D}"
           ];
           selectedLineBgColor = [
-            "#414559"
+            "#${base02}"
           ];
           cherryPickedCommitBgColor = [
-            "#51576d"
+            "#${base03}"
           ];
           cherryPickedCommitFgColor = [
-            "#ca9ee6"
+            "#${base0E}"
           ];
           unstagedChangesColor = [
-            "#e78284}"
+            "#${base08}"
           ];
           defaultFgColor = [
-            "#c6d0f5"
+            "#${base05}"
           ];
           searchingActiveBorderColor = [
-            "#e5c890"
+            "#${base0A}"
           ];
         };
         authorColors = {
-          "*" = "#babbf1";
+          "*" = "#${base07}";
         };
       };
     };
