191593ef21a05185d2f713bbedd72583e7bcc416
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-02-16 17:05:39 -0600

    Add Cava configuration file with color settings and parameters

---
+++ home/features/cava/default.nix
@@ -0,0 +1,54 @@
+{
+  pkgs,
+  ...
+}:
+let
+  base00 = "303446"; # base
+  base01 = "292c3c"; # mantle
+  base02 = "414559"; # surface0
+  base03 = "51576d"; # surface1
+  base04 = "626880"; # surface2
+  base05 = "c6d0f5"; # text
+  base06 = "f2d5cf"; # rosewater
+  base07 = "babbf1"; # lavender
+  base08 = "e78284"; # red
+  base09 = "ef9f76"; # peach
+  base0A = "e5c890"; # yellow
+  base0B = "a6d189"; # green
+  base0C = "81c8be"; # teal
+  base0D = "8caaee"; # blue
+  base0E = "ca9ee6"; # mauve
+  base0F = "eebebe"; # flamingo
+  base10 = "292c3c"; # mantle - darker background
+  base11 = "232634"; # crust - darkest background
+  base12 = "ea999c"; # maroon - bright red
+  base13 = "f2d5cf"; # rosewater - bright yellow
+  base14 = "a6d189"; # green - bright green
+  base15 = "99d1db"; # sky - bright cyan
+  base16 = "85c1dc"; # sapphire - bright blue
+  base17 = "f4b8e4"; # pink - bright purple
+in
+{
+  programs.cava = {
+    enable = true;
+    package = pkgs.cava;
+    settings = {
+     general.framerate = 60;
+      input.method = "pipewire";
+      smoothing.noise_reduction = 88;
+      color = {
+        # background = "'#303446'";
+
+        gradient = "1";
+        gradient_color_1 = "'#${base0E}'";
+        gradient_color_2 = "'#${base0D}'";
+        gradient_color_3 = "'#${base0C}'";
+        gradient_color_4 = "'#${base0B}'";
+        gradient_color_5 = "'#${base0A}'";
+        gradient_color_6 = "'#${base09}'";
+        gradient_color_7 = "'#${base08}'";
+        gradient_count = "7";
+      };
+    };
+  };
+}
