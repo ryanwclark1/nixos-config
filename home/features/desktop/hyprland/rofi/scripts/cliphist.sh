22bd1f317c572d943da8a1ad636eabc9754ce1a6
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-02-17 10:37:17 -0600

    Update Rofi scripts to use new configuration paths and clean up default.nix

--- home/features/desktop/hyprland/rofi/scripts/cliphist.sh
+++ home/features/desktop/hyprland/rofi/scripts/cliphist.sh
@@ -9,16 +9,16 @@
 
 case $1 in
     d)
-        cliphist list | rofi -dmenu -replace -config ~/.config/rofi/config-cliphist.rasi | cliphist delete
+        cliphist list | rofi -dmenu -replace -config ~/.config/rofi/style/cliphist.rasi | cliphist delete
         ;;
 
     w)
-        if [ $(echo -e "Clear\nCancel" | rofi -dmenu -config ~/.config/rofi/config-short.rasi) == "Clear" ]; then
+        if [ $(echo -e "Clear\nCancel" | rofi -dmenu -config ~/.config/rofi/style/config-short.rasi) == "Clear" ]; then
             cliphist wipe
         fi
         ;;
 
     *)
-        cliphist list | rofi -dmenu -replace -config ~/.config/rofi/config-cliphist.rasi | cliphist decode | wl-copy
+        cliphist list | rofi -dmenu -replace -config ~/.config/rofi/style/cliphist.rasi | cliphist decode | wl-copy
         ;;
 esac
