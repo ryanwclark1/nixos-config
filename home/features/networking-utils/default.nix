2ef3ad4fea00ab0a7ed2aa91aa18491037003c4f
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-03-01 13:39:30 -0600

    Comment out rathole in networking-utils for clarity

--- home/features/networking-utils/default.nix
+++ home/features/networking-utils/default.nix
@@ -11,7 +11,7 @@
     nload # Monitors network traffic and bandwidth usage with ncurses graphs.
     nmap # A utility for network discovery and security auditing
     openresolv # a resolv.conf management framework
-    rathole # similar to ngrok - NAT traversal and reverse proxy
+    # rathole # similar to ngrok - NAT traversal and reverse proxy
     tcpdump # Network sniffer
     wireguard-tools # Supplies the main userspace tooling for using and configuring WireGuard tunnels, including the wg(8) and wg-quick(8) utilities.
     netscanner # A simple network scanner
