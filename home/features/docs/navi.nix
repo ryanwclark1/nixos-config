b0a8021844ba1a146ee5e5512ed0565b037e04b9
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-03-24 16:39:29 -0500

    cleanup tldr and navi

--- home/features/docs/navi.nix
+++ home/features/docs/navi.nix
@@ -10,27 +10,18 @@
   programs.navi = {
     enable = true;
     package = pkgs.navi;
-    # conflicts with Control G -> GH
     enableBashIntegration = lib.mkIf config.programs.bash.enable false;
     enableFishIntegration = lib.mkIf config.programs.fish.enable false;
     enableZshIntegration = lib.mkIf config.programs.zsh.enable false;
     settings = {
       finder = {
         command = "fzf";
-      #   # overrides: --tac # equivalent to the --fzf-overrides option
-      #   # overrides_var: --tac # equivalent to the --fzf-overrides-var option
       };
-      # cheats = {
-      #   paths = [];
-      # };
-      # search:
-      #   tags: git,!checkout # equivalent to the --tag-rules option
       client = {
         tealdeer = true;
       };
       shell = {
         command = "bash";
-        finder_command = "bash";
       };
     };
   };
