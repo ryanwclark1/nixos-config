b900f3683e7b76f6bb3b1f0b33a33f2bfd09622a
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-03-27 09:42:57 -0500

    Add Windsurf package and update scripts for version management

--- home/features/windsurf/default.nix
+++ home/features/windsurf/default.nix
@@ -1,11 +1,11 @@
-{
-  pkgs,
-
-  ...
-}:
+{ pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    windsurf
+  home.packages = [
+    (import ../../../pkgs/windsurf {
+      inherit (pkgs) lib stdenv callPackage fetchurl;
+      inherit (pkgs.nixos-testers) nixosTests;
+      vscode-generic = pkgs.vscode-utils.vscode-generic;
+    })
   ];
 }
