a89f6b100022c3b67e0b5a1c834f9493ca6bf0b9
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-03-31 21:29:55 -0500

    Refactor default.nix: comment out useGlobalPkgs for clarity

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -18,7 +18,7 @@
     ./sops.nix
   ]; # ++ (builtins.attrValues outputs.nixosModules);
 
-  home-manager.useGlobalPkgs = true;
+  # home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.backupFileExtension = "bak";
   home-manager.extraSpecialArgs = {
