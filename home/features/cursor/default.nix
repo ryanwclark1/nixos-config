bcc52833c34b04f3c6678994a4d9f6ed169e8877
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-05-06 11:09:09 -0500

    Using nixos defauilt cursor

--- home/features/cursor/default.nix
+++ home/features/cursor/default.nix
@@ -2,11 +2,12 @@
 
 {
   home.packages = [
+    pkgs.code-cursor
     # pkgs.xorg.libxkbfile
-    (import ../../../pkgs/code-cursor {
-      inherit (pkgs) lib stdenvNoCC fetchurl appimageTools alsa-lib at-spi2-atk autoPatchelfHook cairo cups curlWithGnuTls egl-wayland expat fontconfig freetype ffmpeg glib glibc glibcLocales gtk3 libappindicator-gtk3 libdrm libgbm libGL libnotify libva-minimal libxkbcommon makeWrapper nspr nss pango pciutils pulseaudio vivaldi-ffmpeg-codecs vulkan-loader wayland rsync undmg;
-      inherit (pkgs.xorg) libxkbfile;
-    })
+    # (import ../../../pkgs/code-cursor {
+    #   inherit (pkgs) lib stdenvNoCC fetchurl appimageTools alsa-lib at-spi2-atk autoPatchelfHook cairo cups curlWithGnuTls egl-wayland expat fontconfig freetype ffmpeg glib glibc glibcLocales gtk3 libappindicator-gtk3 libdrm libgbm libGL libnotify libva-minimal libxkbcommon makeWrapper nspr nss pango pciutils pulseaudio vivaldi-ffmpeg-codecs vulkan-loader wayland rsync undmg;
+    #   inherit (pkgs.xorg) libxkbfile;
+    # })
   ];
 }
 # libxkbfile
