44be384a8780d1ca7a30cd59a206eec5c7a56442
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-05-09 09:52:58 -0500

    Added dysk and moved docker tools to docker

--- home/features/cli/default.nix
+++ home/features/cli/default.nix
@@ -11,16 +11,16 @@
     ./tf.nix
   ];
   home.packages = with pkgs; [
+    dysk # DF with flair
     duf # Disk Usage/Free Utility
     ncdu # TUI disk usage
-    lazydocker # A simple terminal UI for both docker and docker-compose
-    dive # A tool for exploring each layer in a docker image.
+
+    
     gum # shell scripts
     viu # Terminal image viewer with native support for iTerm and Kitty
     yq-go #jq for yaml, command-line YAML processor https://github.com/mikefarah/yq
     fx # Terminal JSON viewer
     aichat # Use GPT-4(V), Gemini, LocalAI, Ollama and other LLMs in the terminal.
-    lazysql # SQL Tui
     mprocs # multiple commands in parallel
     vultr-cli # vultr cli
     vhs # A tool for generating terminal GIFs with code
