a20750f255db3914da5b4271c0e11cd55fcb425c
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-05-15 11:35:55 -0500

    Updated sddm

--- hosts/common/optional/displaymanager/sddm/default.nix
+++ hosts/common/optional/displaymanager/sddm/default.nix
@@ -9,23 +9,38 @@ let
 in
 {
 
+  environment.systemPackages = with pkgs; [
+    sddm-astronaut
+    (catppuccin-sddm.override {
+      flavor = "frappe";
+      font = "Fira Sans";
+      fontSize = "10";
+      # background = "/path/to/your/wallpaper.jpg";
+      loginBackground = false;
+    })
+  ];
 
   services = {
+    xserver.enable = lib.mkDefault true;
     displayManager = {
       defaultSession = lib.mkDefault "hyprland-uwsm";
       sddm = {
         enable = true;
-        wayland.enable = true;
+        wayland = {
+          enable = true;
+          compositor = "weston";
+        };
         autoNumlock = true;
         enableHidpi = true;
-        extraPackages = [
-          sddmTheme
-          # sddm
-          # sddm-greeter
-          # sddm-theme
-        ];
         # theme "= "${sddmTheme}";
-        theme = "sddm-astronaut-theme";
+        package = pkgs.kdePackages.sddm;
+        theme = "catppuccin-frappe";
+        settings.Theme.CursorTheme = "Bibata-Modern-Classic";
+        extraPackages = with pkgs; [
+          kdePackages.qtmultimedia
+          kdePackages.qtsvg
+          kdePackages.qtvirtualkeyboard
+        ];
       };
     };
   };
