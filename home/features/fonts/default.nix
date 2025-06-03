b715514b27280c4eb6f02874cd141f249cb55304
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-06-03 10:21:25 -0500

    Remove font mplus

--- home/features/fonts/default.nix
+++ home/features/fonts/default.nix
@@ -67,7 +67,6 @@
     nerd-fonts.monofur
     nerd-fonts.monoid
     nerd-fonts.mononoki
-    nerd-fonts.mplus
     nerd-fonts.noto
     nerd-fonts.open-dyslexic
     nerd-fonts.overpass
@@ -96,7 +95,7 @@
     # noto-fonts-extra
     powerline-fonts
     powerline-symbols
-    
+
     # Font Manager
     font-manager
   ];
--- hosts/common/optional/fonts.nix
+++ hosts/common/optional/fonts.nix
@@ -69,7 +69,6 @@
     nerd-fonts.monofur
     nerd-fonts.monoid
     nerd-fonts.mononoki
-    nerd-fonts.mplus
     nerd-fonts.noto
     nerd-fonts.open-dyslexic
     nerd-fonts.overpass
