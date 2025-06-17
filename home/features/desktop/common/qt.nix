906d9e62d19f1f32ed8c0a04513a5e0ca5756fee
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-06-17 10:39:49 -0500

    Enhance qt.nix configuration: Add platform theme and include adwaita-qt packages for improved styling support.

--- home/features/desktop/common/qt.nix
+++ home/features/desktop/common/qt.nix
@@ -6,9 +6,15 @@
 {
   qt = {
     enable = true;
+    platformTheme = "qtct";
     style = {
       name = "adwaita-dark";
       package = pkgs.adwaita-qt;
     };
   };
+
+  home.packages = with pkgs; [
+    adwaita-qt
+    adwaita-qt6
+  ];
 }
