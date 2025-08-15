d1a64cb9018bfdca73012c352cdd6412a682bdd1
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-08-14 23:20:36 -0500

    ai

--- home/features/ai/default.nix
+++ home/features/ai/default.nix
@@ -10,19 +10,6 @@
     # claude-code
     # aider-chat
   ];
-  # services.open-webui = {
-  #   enable = true;
-  #   port = 8180;
-  #   host = "127.0.0.1";
-  #   openFirewall = true;
-  #   package = pkgs.open-webui;
-  #   environment = {
-  #     ANONYMIZED_TELEMETRY = "False";
-  #     DO_NOT_TRACK = "True";
-  #     SCARF_NO_ANALYTICS = "True";
-  #     OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
-  #     WEBUI_AUTH = "False";
-  #   };
-  # };
+
 }
 
