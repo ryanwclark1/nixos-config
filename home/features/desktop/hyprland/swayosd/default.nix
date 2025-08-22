b6cf88549608da2d8c87d5260b99105ecc8a1381
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-08-22 12:44:00 -0500

    Updated swayosd to default

--- home/features/desktop/hyprland/swayosd/default.nix
+++ home/features/desktop/hyprland/swayosd/default.nix
@@ -2,13 +2,13 @@
   ...
 }:
 
-
-
 {
-  # imports = [ ./style.css.nix ];
-
+  # SwayOSD configuration
+  # Note: SwayOSD automatically detects displays, no --display option needed
+  
   services.swayosd = {
     enable = true;
-    display = "eDP-1";
+    # stylePath can be set if you want custom CSS styling
+    # stylePath = "${config.home.homeDirectory}/.config/swayosd/style.css";
   };
 }
