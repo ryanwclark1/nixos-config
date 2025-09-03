468a26c5f8ac8407779e7c13d9c628200950f0f9
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-09-03 16:52:33 -0500

    fix: update comment for devpod-desktop to indicate Tauri version mismatch

--- home/features/gui-tools/development.nix
+++ home/features/gui-tools/development.nix
@@ -6,7 +6,7 @@
 {
   home.packages = with pkgs; [
     dbeaver-bin # Universal Database Tool
-    devpod-desktop # Codespaces but open-source, client-only and unopinionated: Works with any IDE
+    # devpod-desktop # Codespaces but open-source, client-only and unopinionated: Works with any IDE - BROKEN: Tauri version mismatch
     insomnia # API Client for REST, GraphQL, GRPC and OpenAPI design tool for developers.
     postman # API Development Environment
     sqlitebrowser # Visual tool to create, design, and edit database files compatible with SQLite
