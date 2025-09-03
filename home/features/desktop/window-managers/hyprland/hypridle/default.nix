63128d67d73ff782953b403d3b498cf01e3a753e
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-09-03 16:52:29 -0500

    feat: reorganize Hyprland configuration and add utility scripts

--- home/features/desktop/window-managers/hyprland/hypridle/default.nix
+++ home/features/desktop/window-managers/hyprland/hypridle/default.nix
@@ -4,38 +4,9 @@
 }:
 
 {
-  services = {
-    hypridle = {
-      enable = true;
-      package = pkgs.hypridle;
-      settings =
-      {
-        general = {
-          lock_cmd = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances.
-          before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
-          after_sleep_cmd = "hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
-          ignore_dbus_inhibit = false;
-        };
-        listener = [
-          {
-            timeout = 1000;
-            on-timeout = "loginctl lock-session";
-          }
-          # dpms
-          {
-            timeout = 1800;
-            on-timeout = "hyprctl dispatch dpms off";
-            on-resume = "hyprctl dispatch dpms on";
-          }
-          # Suspend
-          # {
-          #   # SUSPEND TIMEOUT
-          #   timeout = 20000;
-          #   #SUSPEND ONTIMEOUT
-          #   on-timeout = "systemctl suspend";
-          # }
-        ];
-      };
-    };
-  };
+  # Install hypridle package
+  home.packages = [ pkgs.hypridle ];
+  
+  # Copy hypridle configuration file
+  home.file.".config/hypr/hypridle.conf".source = ./hypridle.conf;
 }
