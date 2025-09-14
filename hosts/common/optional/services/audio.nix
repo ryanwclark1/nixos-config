4aa58fc545ce84822d2265715a3f86a49a395bb7
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-09-14 13:49:46 -0500

    feat: add Bluetooth audio support configuration for WirePlumber

--- hosts/common/optional/services/audio.nix
+++ hosts/common/optional/services/audio.nix
@@ -21,6 +21,17 @@
     };
     pulse.enable = true;
     jack.enable = true;
+    # Enable Bluetooth audio support (requires bluetooth.nix to be imported)
+    wireplumber.configPackages = [
+      (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
+        bluez_monitor.properties = {
+          ["bluez5.enable-sbc-xq"] = true,
+          ["bluez5.enable-msbc"] = true,
+          ["bluez5.enable-hw-volume"] = true,
+          ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+        }
+      '')
+    ];
     extraConfig.pipewire."92-low-latency" = {
       "context.properties" = {
         "default.clock.rate" = 44100;
@@ -68,7 +79,9 @@
   environment.systemPackages = with pkgs; [
     pwvucontrol
     pulsemixer
+    pulseaudio  # Provides pactl command
     alsa-lib
     alsa-utils
+    pavucontrol  # GUI audio control
   ];
 }
