36c30f857e8ec248dd142b8925a14011f8fadec6
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-09-17 16:58:52 -0500

    refactor(nixtools): remove outdated nix-index database sync script and related configurations

--- home/features/cli/nixtools.nix
+++ home/features/cli/nixtools.nix
@@ -4,18 +4,7 @@
   pkgs,
   ...
 }:
-let
-  update-script = pkgs.writeShellApplication {
-    name = "fetch-nix-index-database";
-    runtimeInputs = with pkgs; [ wget coreutils ];
-    text = ''
-      filename="index-$(uname -m | sed 's/^arm64$/aarch64/')-$(uname | tr '[:upper:]' '[:lower:]')"
-      mkdir -p ~/.cache/nix-index && cd ~/.cache/nix-index
-      wget -q -N "https://github.com/nix-community/nix-index-database/releases/latest/download/$filename"
-      ln -f "$filename" files
-    '';
-  };
-in
+
 {
   home.packages = with pkgs; [
     comma
@@ -28,32 +17,5 @@ in
     patchelf
     sops
     nix-prefetch-git # nix development
-    update-script
   ];
-
-  programs.nix-index = {
-    enable = true;
-    package = pkgs.nix-index;
-    enableBashIntegration = lib.mkIf config.programs.bash.enable true;
-    enableFishIntegration = lib.mkIf config.programs.fish.enable true;
-    enableZshIntegration = lib.mkIf config.programs.zsh.enable true;
-  };
-
-  systemd.user.services.nix-index-database-sync = {
-    Unit = { Description = "fetch mic92/nix-index-database"; };
-    Service = {
-      Type = "oneshot";
-      ExecStart = "${update-script}/bin/fetch-nix-index-database";
-      Restart = "on-failure";
-      RestartSec = "5m";
-    };
-  };
-  systemd.user.timers.nix-index-database-sync = {
-    Unit = { Description = "Automatic github:mic92/nix-index-database fetching"; };
-    Timer = {
-      OnBootSec = "10m";
-      OnUnitActiveSec = "24h";
-    };
-    Install = { WantedBy = [ "timers.target" ]; };
-  };
 }
