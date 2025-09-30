0d43121447a5471aae91906c6c49036d017a7d12
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-09-30 09:39:40 -0500

    Refactor waybar style configuration and rename hexToRgba to hexToRGBA; remove unused colorscheme files and related configurations

--- hosts/common/global/core/environment.nix
+++ hosts/common/global/core/environment.nix
@@ -8,9 +8,6 @@
   # Environment variables
   environment = {
     variables = {
-      # Personal domain variables
-      # Note: ACCENT_EMAIL is intentionally not set here as it's environment-specific
-      # You can set it manually or use a different approach for sensitive data
       PERSONAL_DOMAIN = "techcasa.io";
 
       # System-wide environment variables
@@ -30,15 +27,6 @@
 
     # System-wide shell aliases
     shellAliases = {
-      # Common commands
-      ll = "ls -la";
-      la = "ls -A";
-      l = "ls -CF";
-
-      # NixOS specific
-      rebuild = "sudo nixos-rebuild switch";
-      rebuild-test = "sudo nixos-rebuild test";
-      rebuild-boot = "sudo nixos-rebuild boot";
 
       # Git shortcuts
       gs = "git status";
@@ -54,7 +42,6 @@
     };
   };
 
-  # System-wide session variables
   environment.sessionVariables = {
     # Application-specific
     GTK_THEME = "Adwaita:dark";
