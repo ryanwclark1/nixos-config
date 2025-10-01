c7c776c043efdc6ca413ef208d808cae4f09569d
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-09-30 20:26:15 -0500

    Refactor productivity configurations: consolidate calendar and contact management into dedicated files, remove deprecated khal and khard setups, and enhance email account settings for improved organization and functionality.

--- home/features/productivity/calendar.nix
+++ home/features/productivity/calendar.nix
@@ -6,68 +6,91 @@
 }:
 
 {
-  accounts.calendar.accounts.accent = {
-    name = "accent";
-    local = {
-      fileExt = ".ics";
-      type = "filesystem";
-    };
-    primary = true;
+  home.packages = with pkgs; [
+    khal
+  ];
 
-    remote = {
-      url = "http://localhost:1080/users/ryanc@accentvoice.com/calendar/";
-      type = "caldav";
-      userName = "ryanc@accentvoice.com";
-      passwordCommand = [
-        "cat"
-        config.sops.secrets."accent-email/accent-email-password".path
-      ];
-    };
+  accounts.calendar = {
+    basePath = "${config.home.homeDirectory}/Calendars";
+    accounts.accent = {
+      name = "accent";
+      local = {
+        fileExt = ".ics";
+        type = "filesystem";
+      };
+      primary = true;
 
-    vdirsyncer = {
-      enable = true;
-      collections = [ "calendar" ];
-      itemTypes = [ "VEVENT" ];
-      timeRange = {
-        start = "datetime.now() - timedelta(days=7)";
-        end = "datetime.now() + timedelta(days=30)";
+      remote = {
+        url = "http://localhost:1080/users/ryanc@accentvoice.com/calendar/";
+        type = "caldav";
+        userName = "ryanc@accentvoice.com";
+        passwordCommand = [
+          "cat"
+          config.sops.secrets."accent-email/accent-email-password".path
+        ];
+      };
+
+      vdirsyncer = {
+        enable = true;
+        collections = [ "calendar" ];
+        itemTypes = [ "VEVENT" ];
+        timeRange = {
+          start = "datetime.now() - timedelta(days=7)";
+          end = "datetime.now() + timedelta(days=30)";
+        };
       };
     };
+
+    # systemd.user = {
+    #   services = {
+    #     calsync = {
+    #       Unit = {
+    #         Description = "calsync";
+    #       };
+
+    #       Service = {
+    #         Type = "oneshot";
+    #         ExecStart = "${lib.getExe pkgs.calsync} --noninteractive";
+    #         ExecStopPost = "${lib.getExe pkgs.service-status} calsync";
+    #       };
+    #     };
+    #   };
+
+    #   timers = {
+    #     calsync = {
+    #       Unit = {
+    #         Description = "regular calsync";
+    #         After = "network-online.target";
+    #       };
+
+    #       Timer = {
+    #         Unit = "calsync.service";
+    #         OnCalendar = "0/2:00:00";
+    #         AccuracySec = "10min";
+    #       };
+
+    #       Install = {
+    #         WantedBy = [ "timers.target" ];
+    #       };
+    #     };
+    #   };
+    # };
   };
 
-  systemd.user = {
-    services = {
-      calsync = {
-        Unit = {
-          Description = "calsync";
-        };
+  # home.file.".config/khal/config".text =
+  #   /*
+  #   toml
+  #   */
+  #   ''
+  #     [calendars]
 
-        Service = {
-          Type = "oneshot";
-          ExecStart = "${lib.getExe pkgs.calsync} --noninteractive";
-          ExecStopPost = "${lib.getExe pkgs.service-status} calsync";
-        };
-      };
-    };
+  #     [[calendars]]
+  #     path = ~/Calendars/*
+  #     type = discover
 
-    timers = {
-      calsync = {
-        Unit = {
-          Description = "regular calsync";
-          After = "network-online.target";
-        };
-
-        Timer = {
-          Unit = "calsync.service";
-          OnCalendar = "0/2:00:00";
-          AccuracySec = "10min";
-        };
-
-        Install = {
-          WantedBy = [ "timers.target" ];
-        };
-      };
-    };
-  };
+  #     [locale]
+  #     timeformat = %H:%M
+  #     dateformat = %d/%m/%Y
+  #   '';
 
 }
--- home/features/productivity/contact.nix
+++ home/features/productivity/contact.nix
@@ -5,31 +5,76 @@
 }:
 
 {
-  accounts.contact.accounts.accent = {
-    name = "accent";
-    local = {
-      type = "filesystem";
-      fileExt = ".vcf";
+  home.packages = with pkgs; [
+    khard
+  ];
+
+  accounts.contact = {
+    basePath = "${config.home.homeDirectory}/Contacts";
+    accounts.accent = {
+      name = "accent";
+      local = {
+        type = "filesystem";
+        fileExt = ".vcf";
+      };
+
+      # remote = {
+      #   url = "http://localhost:1080/users/ryanc@accentvoice.com/calendar/";
+      #   type = "caldav";
+      #   userName = "ryanc@accentvoice.com";
+      #   passwordCommand = [
+      #     "cat"
+      #     config.sops.secrets."accent-email/accent-email-password".path
+      #   ];
+      # };
+
+      # vdirsyncer = {
+      #   enable = true;
+      #   collections = [ "calendar" ];
+      #   itemTypes = [ "VEVENT" ];
+      #   timeRange = {
+      #     start = "datetime.now() - timedelta(days=7)";
+      #     end = "datetime.now() + timedelta(days=30)";
+      #   };
+      # };
     };
-
-    # remote = {
-    #   url = "http://localhost:1080/users/ryanc@accentvoice.com/calendar/";
-    #   type = "caldav";
-    #   userName = "ryanc@accentvoice.com";
-    #   passwordCommand = [
-    #     "cat"
-    #     config.sops.secrets."accent-email/accent-email-password".path
-    #   ];
-    # };
-
-    # vdirsyncer = {
-    #   enable = true;
-    #   collections = [ "calendar" ];
-    #   itemTypes = [ "VEVENT" ];
-    #   timeRange = {
-    #     start = "datetime.now() - timedelta(days=7)";
-    #     end = "datetime.now() + timedelta(days=30)";
-    #   };
-    # };
   };
+
+  home.file.".config/khard/khard.conf".text =
+    /*
+    toml
+    */
+    ''
+      [addressbooks]
+      [[contacts]]
+      path = ~/Contacts
+
+      [general]
+      debug = no
+      default_action = list
+      editor = $EDITOR
+      merge_editor = vimdiff
+
+      [contact table]
+      # display names by first or last name: first_name / last_name / formatted_name
+      display = first_name
+      # group by address book: yes / no
+      group_by_addressbook = no
+      # reverse table ordering: yes / no
+      reverse = no
+      # append nicknames to name column: yes / no
+      show_nicknames = no
+      # show uid table column: yes / no
+      show_uids = no
+      # sort by first or last name: first_name / last_name / formatted_name
+      sort = last_name
+      # localize dates: yes / no
+      localize_dates = yes
+      # set a comma separated list of preferred phone number types in descending priority
+      # or nothing for alphabetical order
+      preferred_phone_number_type = pref, cell, home
+      # set a comma separated list of preferred email types in descending priority
+      # or nothing for alphabetical order
+      preferred_email_type = pref, work, home
+    '';
 }
