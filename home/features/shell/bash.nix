ad4db56b8d426763098820a3a17040f1f75f299c
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2026-03-09 08:57:30 -0500

    feat(shell): add history and LESS terminal capabilities to bash and zsh
    - Add HISTSIZE and SAVEHIST exports to bash and zsh
    - Add LESS_TERMCAP color variables for better man page display
    - Source common shell functions in bash configuration

--- home/features/shell/bash.nix
+++ home/features/shell/bash.nix
@@ -12,9 +12,25 @@
     enableCompletion = true;
 
     bashrcExtra = ''
+      export HISTSIZE=100000
+      export SAVEHIST=100000
+
+      export LESS_TERMCAP_mb=$'\e[1;32m'
+      export LESS_TERMCAP_md=$'\e[1;32m'
+      export LESS_TERMCAP_me=$'\e[0m'
+      export LESS_TERMCAP_se=$'\e[0m'
+      export LESS_TERMCAP_so=$'\e[01;33m'
+      export LESS_TERMCAP_ue=$'\e[0m'
+      export LESS_TERMCAP_us=$'\e[1;4;31m'
+
       # Initialize fnm (Fast Node Manager)
       if command -v fnm &> /dev/null; then
         eval "$(fnm env --use-on-cd --shell bash)"
+      fi
+
+      # Source common shell functions
+      if [ -f "$HOME/.config/shell/functions.sh" ]; then
+        source "$HOME/.config/shell/functions.sh"
       fi
     '';
   };
--- home/features/shell/zsh.nix
+++ home/features/shell/zsh.nix
@@ -153,6 +153,17 @@
       # Disable # as glob character for flake references (allows .#hostname without quotes)
       disable -p '#'
 
+      export HISTSIZE=100000
+      export SAVEHIST=100000
+
+      export LESS_TERMCAP_mb=$'\e[1;32m'
+      export LESS_TERMCAP_md=$'\e[1;32m'
+      export LESS_TERMCAP_me=$'\e[0m'
+      export LESS_TERMCAP_se=$'\e[0m'
+      export LESS_TERMCAP_so=$'\e[01;33m'
+      export LESS_TERMCAP_ue=$'\e[0m'
+      export LESS_TERMCAP_us=$'\e[1;4;31m'
+
       # Enable vim mode
       bindkey -v
 
