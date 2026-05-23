f7b216ae17d7e85a1ea55d6065ce450a6c13e8b3
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2026-05-23 13:20:29 -0500

    fix(vscode-generic): remove file existence checks in installPhase

--- pkgs/vscode-generic/generic.nix
+++ pkgs/vscode-generic/generic.nix
@@ -383,28 +383,26 @@ stdenv.mkDerivation (
       lib.optionalString stdenv.hostPlatform.isLinux (
         # disable update checks
         ''
-          if [ -f resources/app/product.json ]; then
-            tmpProductJson="$(mktemp)"
-            jq 'del(.updateUrl, .backupUpdateUrl)' resources/app/product.json > "$tmpProductJson"
-            mv "$tmpProductJson" resources/app/product.json
-          else
-            echo "Warning: resources/app/product.json not found, skipping updateUrl removal"
-          fi
+          tmpProductJson="$(mktemp)"
+          jq 'del(.updateUrl, .backupUpdateUrl)' resources/app/product.json > "$tmpProductJson"
+          mv "$tmpProductJson" resources/app/product.json
         ''
         # this is a fix for "save as root" functionality
         + ''
           packed="resources/app/node_modules.asar"
           unpacked="resources/app/node_modules"
-          if [ -f "$packed" ]; then
-            asar extract "$packed" "$unpacked"
-            substituteInPlace $unpacked/@vscode/sudo-prompt/index.js \
-              --replace-fail "/usr/bin/pkexec" "/run/wrappers/bin/pkexec" \
-              --replace-fail "/bin/bash" "${bash}/bin/bash"
-            rm -rf "$packed"
-            ln -rs "$unpacked" "$packed"
-          else
-            echo "Warning: $packed not found, skipping save as root fix"
-          fi
+          asar extract "$packed" "$unpacked"
+          substituteInPlace $unpacked/@vscode/sudo-prompt/index.js \
+            --replace-fail "/usr/bin/pkexec" "/run/wrappers/bin/pkexec" \
+            --replace-fail "/bin/bash" "${bash}/bin/bash"
+          rm -rf "$packed"
+        ''
+        # without this symlink loading JsChardet, the library that is used for auto encoding detection when files.autoGuessEncoding is true,
+        # fails to load with: electron/js2c/renderer_init: Error: Cannot find module 'jschardet'
+        # and the window immediately closes which renders VSCode unusable
+        # see https://github.com/NixOS/nixpkgs/issues/152939 for full log
+        + ''
+          ln -rs "$unpacked" "$packed"
         ''
       )
       + (
